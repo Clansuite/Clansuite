ece2ebe0031345860005484513d05efb77a425e1
Florian Wolf <xsign@gna.org>
2006-06-24 14:41:59 +0000

    added fix_magic_quotes()

--- core/input.class.php
+++ core/input.class.php
@@ -277,5 +277,67 @@ class input
         }
 		return $r_bool;
 	}		
+
+	function fix_magic_quotes( $var = NULL, $sybase = NULL )
+	{
+		// if sybase style quoting isn't specified, use ini setting
+		if ( !isset ($sybase) )
+		{
+			$sybase = ini_get ('magic_quotes_sybase');
+		}
+
+		// if no var is specified, fix all affected superglobals
+		if ( !isset ($var) )
+		{
+			// if magic quotes is enabled
+			if ( get_magic_quotes_gpc () )
+			{
+				// workaround because magic_quotes does not change $_SERVER['argv']
+				$argv = isset($_SERVER['argv']) ? $_SERVER['argv'] : NULL; 
+
+				// fix all affected arrays
+				foreach ( array ('_ENV', '_REQUEST', '_GET', '_POST', '_COOKIE', '_SERVER') as $var )
+				{
+					$GLOBALS[$var] = fix_magic_quotes ($GLOBALS[$var], $sybase);
+				}
+
+				$_SERVER['argv'] = $argv;
+
+				// turn off magic quotes, this is so scripts which
+				// are sensitive to the setting will work correctly
+				ini_set ('magic_quotes_gpc', 0);
+			}
+
+			// disable magic_quotes_sybase
+			if ( $sybase )
+			{
+				ini_set ('magic_quotes_sybase', 0);
+			}
+
+			// disable magic_quotes_runtime
+			set_magic_quotes_runtime (0);
+			return TRUE;
+		}
+
+		// if var is an array, fix each element
+		if ( is_array ($var) )
+		{
+			foreach ( $var as $key => $val )
+			{
+				$var[$key] = fix_magic_quotes ($val, $sybase);
+			}
+
+			return $var;
+		}
+
+		// if var is a string, strip slashes
+		if ( is_string ($var) )
+		{
+			return $sybase ? str_replace ('\'\'', '\'', $var) : stripslashes ($var);
+		}
+
+		// otherwise ignore
+		return $var;
+	}
 }	
 ?>
