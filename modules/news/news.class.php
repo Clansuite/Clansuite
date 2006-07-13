c7b7aea02c875246d7864dd6a01d55ed86f272a4
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2006-07-13 01:49:25 +0000

    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@149 34db09b4-2314-0410-a940-aec02a7c051d

--- modules/news/news.class.php
+++ modules/news/news.class.php
@@ -83,6 +83,43 @@ class module_news
     {
         global $cfg, $db, $tpl, $error, $lang, $functions, $security;
         
+        // $newslist = newseinträge mit usernick und categorie
+        
+        $stmt = $db->prepare('SELECT n.*, u.nick, c.cat_name, c.cat_image_url 
+                                FROM ' . DB_PREFIX .'news n
+                                LEFT JOIN ' . DB_PREFIX .'users u USING(user_id)
+                                LEFT JOIN ' . DB_PREFIX .'category c ON ( n.news_category = c.cat_id AND c.cat_modulname = 'news')
+                                WHERE n.news_hidden='0' ORDER BY news_id DESC' );
+        
+        $stmt->execute( );
+        if ($result = $stmt->fetch() )
+        {
+            $newslist = $result;
+            return $newslist;
+        }
+        else
+        {
+            return false;
+        }
+        
+        
+     
+        /** in pdo umwandeln
+        / $newslist arrayergänzung um die anzahl der newscommentare
+        foreach ($newslist as $k => $v) 
+        		{ $newslist[$k]['nr_news_comments'] = $Db->getOne("SELECT COUNT(*) FROM ".DB_PREFIX."news_comments WHERE news_id = ?", $v['news_id']); }
+
+        // falls es comments gibt, den usernick des letzten comments holen
+        foreach ($newslist as $k => $v) 
+        		{ if ($newslist[$k]['nr_news_comments'] > 0 ) {
+        		$newslist[$k]['lastcomment_by'] = $Db->getOne("SELECT IFNULL(u.nick, c.pseudo) FROM ".DB_PREFIX."news_comments c 
+        										  			   LEFT JOIN ".DB_PREFIX."users u USING(user_id) 
+        													   WHERE c.news_id = ? ORDER BY c.comment_id DESC", $v['news_id']); } }
+        */
+        // $newslist an Smarty übergeben und Template ausgeben
+        $tpl->assign('news', $newslist);
+        
+        $this->output = $tpl->fetch('/news/show.tpl');
         $this->output .= 'You have created a new module, that currently handles this message';
     }
 }
