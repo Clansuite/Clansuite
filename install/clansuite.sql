4221bebf48746bcc8e9baa5572b538887625b9ca
xsign <xsign@34db09b4-2314-0410-a940-aec02a7c051d>
2006-07-20 02:50:11 +0000

    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@221 34db09b4-2314-0410-a940-aec02a7c051d

--- install/clansuite.sql
+++ install/clansuite.sql
@@ -1,9 +1,9 @@
--- phpMyAdmin SQL Dump
+﻿-- phpMyAdmin SQL Dump
 -- version 2.8.1
 -- http://www.phpmyadmin.net
 -- 
 -- Host: localhost
--- Erstellungszeit: 20. Juli 2006 um 04:43
+-- Erstellungszeit: 20. Juli 2006 um 04:48
 -- Server Version: 5.0.21
 -- PHP-Version: 5.1.4
 -- 
@@ -117,6 +117,12 @@ CREATE TABLE `cs_modules` (
 -- Daten für Tabelle `cs_modules`
 -- 
 
+INSERT INTO `cs_modules` VALUES (1, 'index', 'Index Module', 'This is the main site.', 'module_index', 'index.class.php', 'index', 1, 'module_index.jpg', 0.1, 0.1);
+INSERT INTO `cs_modules` VALUES (2, 'admin', 'Admin Interface', 'This is the Admin Control Panel', 'module_admin', 'admin.class.php', 'admin', 1, 'module_admin.jpg', 0.1, 0.1);
+INSERT INTO `cs_modules` VALUES (3, 'account', 'Account Administration', 'This module handles all necessary account stuff - like login/logout etc.', 'module_account', 'account.class.php', 'account', 1, 'module_account.jpg', 0.1, 0.1);
+INSERT INTO `cs_modules` VALUES (4, 'captcha', 'Captcha Module', 'The captcha module presents a image only humanoids can read.', 'module_captcha', 'captcha.class.php', 'captcha', 1, 'module_captcha.jpg', 0.1, 0.1);
+INSERT INTO `cs_modules` VALUES (5, 'asdfffffffffffffffffffffff', 'asd34535', 'asd', 'module_asdfffffffffffffffffffffff', 'asdfffffffffffffffffffffff.class.php', 'asdfffffffffffffffffffffff', 0, 'module_asdfffffffffffffffffffffff.jpg', 0, 0);
+INSERT INTO `cs_modules` VALUES (8, 'news', 'The website news', 'The website news', 'module_news', 'news.class.php', 'news', 0, 'module_news.jpg', 0, 0);
 
 -- --------------------------------------------------------
 
