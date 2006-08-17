aebcab3c47cd752b9d0de7fd3c5cbcaf998fe697
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2006-08-17 18:02:20 +0000

    clip.js und gruppenimages
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@384 34db09b4-2314-0410-a940-aec02a7c051d

---
+++ templates/core/javascript/clip.js
@@ -0,0 +1,29 @@
+/*
+ * DO NOT REMOVE THIS NOTICE
+ *
+ * PROJECT:   clansuite
+ * VERSION:   0.1
+ * COPYRIGHT: (c) 2005,2006 Jens-Andre Koch, Florian Wolf
+ * LINK:      http://www.clansuite.com
+ * LICENSE:   GPL2
+ *
+ *
+ * Example Use:
+ *
+ * <a href="javascript:clip_span('1')">Clip it</a>
+ *
+ * <span id="span_1" style="display: none;">Stuff</span>
+ *
+ */
+
+function clip_span(id)
+{
+    if(document.getElementById("span_" + id).style.display == 'none')
+    {
+        document.getElementById("span_" + id).style.display = "block";
+    }
+    else
+    {
+        document.getElementById("span_" + id).style.display = "none";
+    }
+}
