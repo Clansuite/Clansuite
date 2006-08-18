0d314945d13506a9d984e30806881cd719a15d5d
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2006-08-18 23:37:41 +0000

    reverted to old class; an altered version can be found in #388 commited by freq77
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@390 34db09b4-2314-0410-a940-aec02a7c051d

--- core/language.class.php
+++ core/language.class.php
@@ -46,12 +46,11 @@ class language
     * @desc $loaded array for storing all loaded XML language files
     */
 
-    public  $loaded  = array();	 	// Contains the filenames of the loaded language files
-	private	$lang    = array();    	// Contains all translation of a file in the following form:
-									// array(%id% => %message%);
+    public $xml     = array();
+    public $loaded     = array();
     
     /**
-    * @desc Conrtuctor to register {translate} in SMARTY Template Engine
+    * @desc Register new Smarty Block {translate} 
     */
 
     
@@ -80,16 +79,30 @@ class language
     
     /**
     * @desc Translate a string into the given language
+    *
+    * Usage: $lang->t('Hello' );
     */
 
     function t($string, $args = array() )
-    {        
-		global $lang;
-		
-        if(isset($lang->lang[ $string ])) 
-		{
-			return strtr($this->lang[ $string ], $args);
-		}
+    {
+        global $lang;
+        
+        foreach ($lang->xml as $parent)
+        {
+            foreach ($parent[0]['children'] as $tag)
+            {
+                if ($tag['tag'] == "MESSAGE")
+                {
+                    if ($tag['children'][0]['value'] == $string)
+                    {
+                        if ($tag['children'][1]['value'] != "")
+                        {
+                            return strtr($tag['children'][1]['value'], $args);
+                        }
+                    }
+                }
+            }
+        }
         
         return strtr($string, $args);
     }
@@ -103,24 +116,87 @@ class language
         global $cfg;
         
         $file = LANG_ROOT . '/' . $cfg->language . '/' . $xml_file_name . '.xml';
-        			
-		/**
-		 * Parse translations via Simple XML
-		 * and write to array
-		 */
-		 if (file_exists($file))
+        
+        if (file_exists($file))
         {
-            if (!in_array($xml_file_name, $this->loaded) )
+            if (!in_array($xml_file_name, $this->loaded ) )
             {
-				array_push($this->loaded, $xml_file_name );
-				
-				$xml = new SimpleXMLElement(file_get_contents($file));
-				
-				foreach($xml->message as $msg) {
-					$this->lang[ $msg['id'] ] = $msg['string'];
-				}                
+                array_push($this->xml, $this->get_xml_tree($file) );
+                array_push($this->loaded, $xml_file_name );
             }
         }
-    }    
+    }
+    
+    /**
+    * @desc Handle all children attributes from XML file
+    */
+
+    function get_children($vals, &$i)
+    {
+        global $lang;
+        
+        $children = array();
+        
+        if (!isset($vals[$i]['attributes']))
+        {
+            $vals[$i]['attributes'] = "";
+        }
+        
+        while (++$i < count($vals))
+        {
+            if (!isset($vals[$i]['attributes']))
+            {
+                $vals[$i]['attributes'] = "";
+            }
+            
+            if (!isset($vals[$i]['value']))
+            {
+                $vals[$i]['value'] = "";
+            }
+            
+            switch ($vals[$i]['type'])
+            {
+                case 'complete':
+                    array_push($children, array('tag' => $vals[$i]['tag'], 'attributes' => $vals[$i]['attributes'], 'value' => $vals[$i]['value']));
+                    break;
+                case 'open':
+                    array_push($children, array('tag' => $vals[$i]['tag'], 'attributes' => $vals[$i]['attributes'], 'children' => $lang->get_children($vals, $i)));
+                    break;
+                case 'close':
+                    return $children;
+                    break;
+            }
+        }
+        
+        return $children;
+    }
+    
+    /**
+    * @desc XML file parsing
+    * @desc Return Treestructure
+    */
+
+    function get_xml_tree($file)
+    {
+        global $lang;
+        
+        $data = implode("", file($file));
+        $xml  = xml_parser_create();
+        xml_parser_set_option($xml, XML_OPTION_SKIP_WHITE, 1);
+        xml_parse_into_struct($xml, $data, &$vals, &$index);
+        xml_parser_free($xml);
+        
+        $tree = array();
+        $i = 0;
+        array_push($tree, array(
+								'tag' => $vals[$i]['tag'], 
+								'attributes' => $vals[$i]['attributes'], 
+								'children' => $lang->get_children($vals, $i)
+								)
+				
+					);
+        
+        return $tree;
+    }
 }
 ?>
