ab50db8bb1862be50a2441f4eb5e2db05eb382f2
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2006-10-08 01:53:50 +0000

    timestamp added to backupfiles
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@631 34db09b4-2314-0410-a940-aec02a7c051d

--- install/insert.bat
+++ install/insert.bat
@@ -14,10 +14,14 @@ SET mysqlpassword=toop
 SET mysqluser=clansuite
 SET dbname=clansuite
 
+@REM Zeitstempel ermitteln
+FOR /F "tokens=1,2,3,4 delims=/. " %%a in ('date/T') do set CDATE=%%a-%%b-%%c
+FOR /F "tokens=1,2 delims=/: " %%a in ('time/T') do set CTIME=%%a-%%b
+
 @REM Clansuite DB insert
 
 @ECHO Creating backup of %dbname% as %dbname%_backup.sql...
-%mysqldir%/mysqldump -u %mysqluser% -p%mysqlpassword% --skip-add-locks --add-drop-table %dbname% > %basedir%\%dbname%_backup.sql
+%mysqldir%/mysqldump -u %mysqluser% -p%mysqlpassword% --skip-add-locks --add-drop-table %dbname% > %basedir%\%dbname%_%CDATE%_%CTIME%_backup.sql
 @ECHO Beginning insert of %dbname%...
 %mysqldir%/mysql -u %mysqluser% -p%mysqlpassword% %dbname% < %basedir%\%dbname%.sql
 
