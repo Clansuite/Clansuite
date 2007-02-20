ee268df07f3ada4e959266612f33f77fda5d727b
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2007-02-20 04:04:16 +0000

    doctype xhtml trans commented out shoutbox, till xhtml safe included serverstats into index.tpl added serverstats functions as direct smarty functions {functionname} {gzipcheck} {serverload} {memusage} memusage is not avaiable in a nice way on windows machines (cmd window is opened on every call), so it's commented out
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@965 34db09b4-2314-0410-a940-aec02a7c051d

---
+++ core/smarty/plugins/function.gzipcheck.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Smarty plugin
+ * @package Smarty
+ * @subpackage plugins
+ */
+
+/**
+ * smarty_function_gzipcheck
+ */
+function smarty_function_gzipcheck($params, &$smarty)
+{
+    if (ini_get('zlib.output_compression') == 1) 
+    {
+		echo 'Enabled';
+    } 
+    else 
+    {
+		echo 'Disabled';
+    }
+}
+
+/* vim: set expandtab: */
+
+?>
---
+++ core/smarty/plugins/function.memusage.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Smarty plugin
+ * @package Smarty
+ * @subpackage plugins
+ */
+
+/**
+ * smarty_function_memusage
+ */
+function smarty_function_memusage($params, &$smarty)
+{
+   /*
+   if( !function_exists('memory_get_usage') )
+   {
+        function memory_get_usage()
+       {
+           //If its Windows
+           //Tested on Win XP Pro SP2. Should work on Win 2003 Server too
+           //Doesn't work for 2000
+           //If you need it to work for 2000 look at http://us2.php.net/manual/en/function.memory-get-usage.php#54642
+           if ( substr(PHP_OS,0,3) == 'WIN')
+           {
+                   if ( substr( PHP_OS, 0, 3 ) == 'WIN' )
+                   {
+                       $output = array();
+                       exec( 'tasklist /FI "PID eq ' . getmypid() . '" /FO LIST', $output );
+          
+                       return preg_replace( '/[\D]/', '', $output[5] ) * 1024;
+                   }
+           }else
+           {
+               //We now assume the OS is UNIX
+               //Tested on Mac OS X 10.4.6 and Linux Red Hat Enterprise 4
+               //This should work on most UNIX systems
+               $pid = getmypid();
+               exec("ps -eo%mem,rss,pid | grep $pid", $output);
+               $output = explode("  ", $output[0]);
+               //rss is given in 1024 byte units
+               return $output[1] * 1024;
+           }
+       }
+    }
+    $memusage=memory_get_usage();
+    */
+    
+    if (function_exists('memory_get_usage')) {
+      $memusage = memory_get_usage();
+    } else {
+      return $memusage = 'n/a';      
+    }
+    
+    
+    if ($memusage>0) {
+      $memunit="B";
+      if ($memusage>1024) {
+        $memusage=$memusage/1024;
+        $memunit="kB";
+      }
+      if ($memusage>1024) {
+        $memusage=$memusage/1024;
+        $memunit="MB";
+      }
+      if ($memusage>1024) {
+        $memusage=$memusage/1024;
+        $memunit="GB";
+      }
+      print(number_format($memusage,2).$memunit);
+    }
+}
+
+/* vim: set expandtab: */
+
+?>
