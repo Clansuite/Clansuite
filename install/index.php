813b4e9c29c8221fb97e1723a13fb0ef20076a4a
vyper <vyper@34db09b4-2314-0410-a940-aec02a7c051d>
2007-03-28 19:21:31 +0000

    Fixed some redirects
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@1070 34db09b4-2314-0410-a940-aec02a7c051d

--- install/index.php
+++ install/index.php
@@ -78,7 +78,7 @@ if( $_GET['step'] == 2 )
         empty($_SESSION['admin_nick']) ||
         empty($_SESSION['admin_pass']) )
     {
-        header("Location: /" . $_SERVER['PHP_SELF'] . "?step=1&error=fill_form");
+        header("Location: " . $_SERVER['PHP_SELF'] . "?step=1&error=fill_form");
         die();
     }
 
@@ -105,7 +105,7 @@ if( $_GET['step'] == 3 )
             empty($_SESSION['ftp_username']) ||
             empty($_SESSION['ftp_folder']) )
         {
-            header("Location: /" . $_SERVER['PHP_SELF'] . "?step=2&error=fill_form");
+            header("Location: " . $_SERVER['PHP_SELF'] . "?step=2&error=fill_form");
             die();
         }
 
@@ -115,7 +115,7 @@ if( $_GET['step'] == 3 )
         $ftp_con = ftp_connect($_SESSION['ftp_hostname'], $_SESSION['ftp_port']);
         if( !ftp_con )
         {
-            header("Location: /" . $_SERVER['PHP_SELF'] . "?step=2&error=no_connection");
+            header("Location: " . $_SERVER['PHP_SELF'] . "?step=2&error=no_connection");
             die();
         }
         else
@@ -123,7 +123,7 @@ if( $_GET['step'] == 3 )
             $ftp_login = ftp_login($ftp_con, $_SESSION['ftp_username'], $_SESSION['ftp_pass']);
             if( !$ftp_login )
             {
-                header("Location: /" . $_SERVER['PHP_SELF'] . "?step=2&error=no_connection");
+                header("Location: " . $_SERVER['PHP_SELF'] . "?step=2&error=no_connection");
                 die();
             }
             else
