1ac4f7d590047d5c1b88fd930e784c8d383ba187
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2007-04-25 19:30:11 +0000

    create_board added; advanced recursion plugin for smarty /plugins/compiler.defun.php added
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@1217 34db09b4-2314-0410-a940-aec02a7c051d

--- modules/board/board.admin.php
+++ modules/board/board.admin.php
@@ -66,7 +66,7 @@ class module_board_admin
 
         // Set Pagetitle and Breadcrumbs
         $trail->addStep($lang->t('Admin'), '/index.php?mod=admin');
-        $trail->addStep($lang->t('board'), '/index.php?mod=board&sub=admin');
+        $trail->addStep($lang->t('Board'), '/index.php?mod=board&sub=admin');
 
         switch ($_REQUEST['action'])
         {
@@ -76,6 +76,11 @@ class module_board_admin
                 $trail->addStep($lang->t('Show'), '/index.php?mod=board&sub=admin&action=show');
                 $this->show();
                 break;
+            
+            case 'create_board':
+                $trail->addStep($lang->t('Create a new board'), '/index.php?mod=board&amp;sub=admin&amp;action=create_board');
+                $this->create_board();
+                break;  
 
             case 'instant_show':
                 $this->output .= call_user_func_array( array( $this, 'instant_show' ), $params );
@@ -98,11 +103,138 @@ class module_board_admin
     function show()
     {
         global $cfg, $db, $tpl, $error, $lang, $functions, $security, $input, $perms, $users;
-
-        // Add $lang-t() translated text to the output.
-        $this->output .= $lang->t('This is the admin submodule: module_board_admin. Place Administration of the Module here.');
+        
+                
+        // Assign and output
+        $tpl->assign('boards', $this->build_boardtree());
+        $this->output = $tpl->fetch('board/admin_board.tpl');
     }
 
+    /**
+     * We love elegant recursive functions until our heads explode.
+     * Lets build up the boardtree with all it's nestinlevels/ childelements.
+     * This function is similar to build_menueditor 
+     */    
+    function build_boardtree ( &$result = '', $parent = 0, $level = 0 )
+    {
+        global $db;
+
+        /**
+         * Fetch every Forum around there, if result is empty
+         * Means: this is the starting point
+         */
+        if ( empty($result) )
+        {         
+            $stmt = $db->prepare('SELECT * FROM '. DB_PREFIX .'board_forums ORDER BY displayorder, forumparent ASC');
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+        
+        $output = array();        
+        $rows = count($result);     
+      
+        for ($i = 0; $i < $rows; $i++)
+        {   
+            if ($result[$i]['forumparent'] == $parent )
+            {
+                
+                //new index
+                $output[$result[$i]['forumid']] = array(
+                                                            'forumid'       => $result[$i]['forumid'],
+                                                            'forumparent'   => $result[$i]['forumparent'],
+                                                            'name'          => $result[$i]['name'],
+                                                            'description'   => $result[$i]['description'],
+                                                            
+                                                            'nestinglevel'  => $level,
+                                                            'type'          => $result[$i]['type'],
+                                                            
+                                                            'moderator'     => $result[$i]['moderator'],
+                                                            'displayorder'  => $result[$i]['displayorder'],
+                                                            'posts'         => $result[$i]['posts'],
+                                                            'threads'       => $result[$i]['threads'],
+                                                            'type'          => $result[$i]['type'],
+                                                            'status'        => $result[$i]['status'],
+                                                        );
+                
+                
+                // recursion callback with next level
+                $output[$result[$i]['forumid']]['children'] = $this->build_boardtree($result, $result[$i]['forumid'], $level + 1);
+                
+                if( count($output[$result[$i]['forumid']]['children']) == 0)
+                {
+                    unset($output[$result[$i]['forumid']]['children']);
+                }
+             }        
+        }        
+        return array_values($output);
+    }
+    
+    function create_board()
+    {    
+        global $cfg, $db, $tpl, $error, $lang, $functions, $security, $input;
+
+        /**
+         * Init
+         */
+        $submit     = $_POST['submit'];
+        $info       = $_POST['newboard'];
+   
+        if ( !empty( $submit ) )
+        {
+            // Forum already stored?
+            $stmt = $db->prepare( 'SELECT * FROM ' . DB_PREFIX . 'board_forums WHERE name = ?' );
+            $stmt->execute( array( $info['name'] ) );
+            $forums = $stmt->fetch(PDO::FETCH_ASSOC);
+            if ( is_array( $forums ) )
+            {
+                $err['name_already'] = 1;
+            }
+
+            // Form filled?
+            if( empty($info['name']) OR
+                empty($info['description']))
+            {
+                $err['fill_form'] = 1;
+            }
+        }
+    
+        /**
+         * No errors - procceed to insert
+         */
+        if ( !empty($submit) AND count($err) == 0 )
+        {
+            /**
+             * setting of "before" "after" means that "displayorder" has to be dealt with
+             * setting of "child" means that "parentid" and "displayorder" have to be dealt with
+             * @todo before, after, child handling 
+             */
+            if (($info['positiontype'] == before) OR ($info['positiontype'] == after))
+            {}
+                echo "insert";
+            // Insert Forum to Db
+            $sets =  'name = ?, description = ?, permissions = ?, forumparent = ?';
+            $stmt = $db->prepare( 'INSERT ' . DB_PREFIX . 'board_forums SET ' . $sets );
+            $stmt->execute( array ( $info['name'],
+                                    $info['description'],
+                                    $info['permissiontype'],
+                                    $info['parentid']                                   
+                                     ) );
+            
+    
+        /**
+         *  Redirect...
+         */
+        $functions->redirect( 'index.php?mod=board&sub=admin', 'metatag|newsite', 3, $lang->t( 'Board was created.' ), 'admin' );
+        } 
+        
+         /**
+        * @desc Assign & Show template
+        */
+        $tpl->assign( 'modules'     , $modules );
+        $tpl->assign( 'err'         , $err );
+        $this->output .= $tpl->fetch('board/admin_board.tpl');
+    }
+    
     /**
     * Instant Show
     *
