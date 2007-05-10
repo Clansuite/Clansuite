6db5a0e958b38e1a86225f8f181df2001b1a3ead
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2007-05-10 19:44:30 +0000

    seperated the array for the db timermarkers in the benchmark class integrated benchmarking for db actions
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@1259 34db09b4-2314-0410-a940-aec02a7c051d

--- core/benchmark.class.php
+++ core/benchmark.class.php
@@ -39,95 +39,169 @@
     */
 
 /**
- * Class Benchmark 
+ * Class Benchmark
  * @author Boots @ Smarty Forum
  * @copyright Boots, 2003
  *
- * Benchmark implements the Timemarker Functionality 
+ * Benchmark implements the Timemarker Functionality
  *
  * Example Usage:
  *
- * php 
+ * php
  *       begin of php
  *       include_once 'benchmark.class.php';
  *       benchmark::timemarker('begin', 'Clansuite Initialization');
  *       assign function to smarty
- *       $tpl->register_modifier('timemarker',  array('benchmark', 'timemarker'));  
+ *       $tpl->register_modifier('timemarker',  array('benchmark', 'timemarker'));
  *
  *       end of php
  *       benchmark::timemarker('list'); echos a history of timemarkers
  *       DEBUG ? benchmark::timemarker('list') : ''; # returns a history of timemarkers in case of debug
- *       
+ *
  * tpl
- *       
- *       start a new timemarker with name "template block" 
+ *
+ *       start a new timemarker with name "template block"
  *       {"begin"|timemarker:"template block"}
- *       
- *       will print the timemarker name and formatted time of it     
+ *
+ *       will print the timemarker name and formatted time of it
  *       {"end"|timemarker:true}
- *       
- *       start two timemarkers 
+ *
+ *       start two timemarkers
  *       {"begin"|timemarker:"level2"}
  *       {"begin"|timemarker:"level3"}
- *       
+ *
  *       stop all your timemarkers at once using the 'stop' action. this will stop also php-started timers
  *       {"stop"|timemarker:true|nl2br}
  *
  */
 class benchmark
-{   
+{
     function timemarker($mode='begin', $msg='')
     {
-        global $_timer_blocks, $_timer_history; 
-        
-        switch ($mode) 
+        global $_timer_blocks, $_timer_history, $db_timer_blocks, $db_timer_history, $_db_timer;
+
+        switch ($mode)
         {
-            case 'begin':
-                            $_timer_blocks[] =array(microtime(), $msg);
+            /**
+             *  Db - db_begin, db_end, db_list
+             */
+             
+            case 'db_begin':
+                            $db_timer_blocks[] = array( microtime() , $msg);
                             break;
-    
-            case 'end':
+
+            case 'db_end':
+                            $last = array_pop($db_timer_blocks);
+                            $_start = $last[0];
+                            $_msg = $last[1];
+                            list($a_micro, $a_int) = explode(' ', $_start);
+                            list($b_micro, $b_int) = explode(' ', microtime());
+                            /**
+                             * $elapsed contains the calculated difference of Start to Endtime
+                             */
+                            $elapsed = ($b_int - $a_int) + ($b_micro - $a_micro);
+
+                            $db_timer_history[0][1] += $elapsed;
+                            $db_timer_history[] = array( $elapsed, "$_msg [". round($elapsed, 4) ."s]");
+
+                            // add elapsed time to db overall counter
+
+
+                            if ($msg)
+                            {
+                                return "$_msg: {$elapsed}s \n";
+
+                            }
+                            return $elapsed;
+                            break;
+
+             case 'db_list':
+
+                            $o = '<strong>Database Timemarkers History:</strong> <br />';
+                            if(isset($db_timer_history) && !empty($db_timer_history))
+                            {
+
+                                foreach ($db_timer_history as $mark)
+                                {
+                                    $o .= $mark[1] . " \n <br />";
+
+                                }
+                                echo $o;
+                            }
+
+                            var_dump($db_timer_history);
+
+                            break;
+
+            /**
+             *  Standard - begin, end, list, stop
+             */
+
+            case 'begin':
+                            $_timer_blocks[] = array(microtime(), $msg);
+                            break;
+
+            case 'end':     var_dump($_msg);
+                            
                             $last = array_pop($_timer_blocks);
                             $_start = $last[0];
                             $_msg = $last[1];
                             list($a_micro, $a_int) = explode(' ', $_start);
                             list($b_micro, $b_int) = explode(' ', microtime());
-                            $elapsed = ($b_int - $a_int) + ($b_micro - $a_micro);
+                            /**
+                             * $elapsed contains the calculated difference of Start to Endtime
+                             * notice @ - to get rid of division by zero
+                             */
+                            $elapsed = @($b_int - $a_int) + ($b_micro - $a_micro);
+
+                            /**
+                             * Now this is entered into the $_timmer_history array.
+                             * And displayed in the following manner: [0.0022s 454.55/s]
+                             *
+                             * First: the elapsed time in seconds (known as the period)
+                             * Second: how many times that First element can occur
+                             *         sequentially in a single second (known as the frequency)
+                             * Means with example above: 0.0022 Seconds * 454.55 Times = 1 Second
+                             *
+                             * :) - Strange but true - So... What's the Frequency, Kenneth?
+                             */
+
                             $_timer_history[] = array($elapsed, $_msg, "$_msg [".round($elapsed, 4)."s ".(round(1/$elapsed, 2))."/s]");
-                            
-                            if ($msg) 
+
+
+                            if ($msg)
                             {
                                 return "$_msg: {$elapsed}s \n";
                             }
-                            
+
                             return $elapsed;
                             break;
-    
+
             case 'list':
-            
-                            $o = 'Timemarkers History: ';
+                            $o = '<strong>Timemarkers History:</strong> <br />';
                             if(isset($_timer_history) && !empty($_timer_history))
                             {
-                                foreach ($_timer_history as $mark) 
+                                foreach ($_timer_history as $mark)
                                 {
-                                    $o .= $mark[2] . " \n";
+                                    $o .= $mark[2] . " \n <br />";
                                 }
                                 echo $o;
                             }
+
+                            var_dump($_timer_history);
                             break;
-    
+
             case 'stop':
-                        
                             $result = '';
-                            while(!empty($_timer_blocks)) 
+                            while(!empty($_timer_blocks))
                             {
                                 $result .= benchmark::timemarker('end', $msg);
                             }
-                
+
                             return $result;
                             break;
         }
-    
+
     }
 }
 ?>
