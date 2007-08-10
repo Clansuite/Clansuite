274aea7f6e6e206e290e61c3eaa51eb8e4a61b55
Jens-André Koch <jakoch@web.de>
2007-08-10 14:58:26 +0000

    - installer update: version 0.2 DEV

--- installation/install-step2.php
+++ installation/install-step2.php
@@ -1,70 +1,50 @@
-<?php ?>
-<html>
-<head>
-<title>ClanSuite.com - just an eSport CMS - Installer</title>
-<link rel="stylesheet" type="text/css" href="installation.css" />
-</head>
-<body>
-<center>
-<h1>Installer</h1>
-<br />
-This Installer will guide you in 3 small steps through the hole installation of the <b>C</b>ontent <b>M</b>anagement <b>S</b>ystem (CMS) ClanSuite.
-<p>&nbsp;</p>
-<form autocomplete="off" action="index.php?step=3" method="POST">
-<table style="border: 1px solid black" width="400">
-    <tr>
-        <td height="50" colspan="2" align="center"><b>Step 2:</b> Database data</td>
-    </tr>
-            <?php
-                if( $_GET['error'] == 'fill_form' )
-                {
-                    echo '<tr><td height="30" align="center" colspan="2"><div class="error">Please fill all fields in the form!</div></td></tr>';
-                }
-                if( $_GET['error'] == 'no_connection' )
-                {
-                    echo '<tr><td height="30" align="center" colspan="2"><div class="error">The connection could not be established with the given data!</div></td></tr>';
-                }
-            ?>
-    <tr>
-        <td width="150" align="right">DB type:</td>
-        <td align="left">
-        <select name="config[db_type]" class="inputs">
-            <option value="mysql">MySQL</option>
-        </select>
-        </td>
-    </tr>
-    <tr>
-        <td width="100" align="right">Host:</td>
-        <td align="left"><input class="inputs" type="text" name="config[db_host]" value="<?=$_SESSION['config']['db_host'] ?>" /></td>
-    </tr>
-    <tr>
-        <td width="100" align="right">DB name:</td>
-        <td align="left"><input class="inputs" type="text" name="config[db_name]" value="<?=$_SESSION['config']['db_name'] ?>" /></td>
-    </tr>
-    <tr>
-        <td width="100" align="right" class="desc">Prefix:</td>
-        <td align="left"><input class="inputs" type="text" name="config[db_prefix]" value="<?=$_SESSION['config']['db_prefix'] ?>" /></td>
-    </tr>
-    <tr>
-        <td width="100" align="right" class="desc">Username:</td>
-        <td align="left"><input class="inputs" type="text" name="config[db_username]" value="<?=$_SESSION['config']['db_username'] ?>" /></td>
-    </tr>
-    <tr>
-        <td width="100" align="right" class="desc">Password:</td>
-        <td align="left"><input class="inputs" type="password" name="config[db_password]" value="<?=$_SESSION['config']['db_password'] ?>" /></td>
-    </tr>
-    <tr>
-        <td height="70" colspan="2" align="center">
-            <input type="submit" style="border: 1px solid black" value="Generate Tables..." name="submit" />
-            <br /><br />
-            <a href="/install/index.php?step=1"><<< Back to Step 1</a>
-            <br />
-            <small>By clicking the button above you accept the <a href="/COPYING.txt">GPL Licensing</a>.</small>
-        </td>
-    </tr>
-</table>
-</form>
-</center>
+    <div id="sidebar" id="leftsidebar">
+        <div id="stepbar">
+            Installationsschritte
+            <div class="step-pass"> [1] Sprachauswahl</div>
+            <div class="step-on">   [2] Systemcheck</div>
+            <div class="step-off">  [3] GNU/GPL Lizenz</div>
+            <div class="step-off">  [4] Datenbank</div>
+            <div class="step-off">  [5] Konfiguration</div>
+            <div class="step-off">  [6] Settings</div>
+            <div class="step-off">  [7] Abschluss</div>
+        </div>
+    </div>
 
-</body>
-</html>
+    <div id="content" class="narrowcolumn">
+    
+        <div id="content_footer">
+                    
+            <div class="accordion">
+        	   <h2 class="headerstyle">
+        	       <img src="images/64px-Utilities-system-monitor.svg.png" border="0" align="absmiddle"> 
+        	       <?=$language['STEP2_SYSTEMCHECK'] ?>
+        	   </h2>
+        			
+        	   <p>Systemcheck...</p>
+
+            <div class="navigation">
+                        
+            			<div class="alignleft">
+            			 <form action="index.php" name="lang" method="post">
+                            <input type="submit" value="<?=$language['BACKSTEP'] ?>" class="button" name="Button2"/>
+                            <input type="hidden" name="lang" value="<?=$_SESSION['lang'] ?>">
+                            <input type="hidden" name="step" value="<?=$_SESSION['step']-1; ?>">
+                         </form>
+                        </div>
+                        
+            			<div class="alignright">
+            			 <form action="index.php" name="lang" method="post">
+                            <input type="submit" value="<?=$language['NEXTSTEP'] ?>" class="button" name="Button2"/>
+                            <input type="hidden" name="lang" value="<?=$_SESSION['lang'] ?>">
+                            <input type="hidden" name="step" value="<?=$_SESSION['step']+1; ?>"> 
+            			 </form>
+            			</div>
+            </div><!-- div navigation end -->
+            
+           
+        	</div> <!-- div accordion end -->   
+    
+        </div> <!-- div content_footer end -->
+
+    </div> <!-- div content end -->
---
+++ installation/readme.txt
@@ -0,0 +1,8 @@
+Clansuite Installer
+Version 0.2
+
+Icons by Tango Project  (http://tango-project.org/)
+via Wikimedia Commons   (http://commons.wikimedia.org/wiki/Category:Tango_project)
+
+How to get resized SVG-Icons from Wikimedia Commons?
+Browse Archiv -> Klick Icon -> Graphic Show -> URL Bar -> change px -> SAVE AS
