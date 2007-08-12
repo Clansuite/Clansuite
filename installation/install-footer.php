95575c2ee431513f0197fbce2f84816abde5dd28
vyper <vyper@34db09b4-2314-0410-a940-aec02a7c051d>
2007-08-12 23:12:52 +0000

    this solves all the HTML errors
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@1285 34db09b4-2314-0410-a940-aec02a7c051d

--- installation/install-footer.php
+++ installation/install-footer.php
@@ -5,24 +5,24 @@
     		    <li><h2><?=$language['INSTALL_PROGRESS']?></h2></li>
     		    <li><?=$language['COMPLETED']?> <b><?=$_SESSION['progress']?>%</b>
     		      <div id="progressbar">
-    		            <?php 
+    		            <?php
     		            #note by vain: this fixes a 2pixel problem while displaying the progress bar at 100percent:P
-    		            if ($_SESSION['step'] == 7 ) { $_SESSION['progress'] = $_SESSION['progress'] - 2; } 
+    		            if ($_SESSION['step'] == 7 ) { $_SESSION['progress'] = $_SESSION['progress'] - 2; }
     		            ?>
                         <div style="border: 1px solid white; height: 5px ! important; width: <?=$_SESSION['progress']?>px; background-color: rgb(181, 0, 22);"/>
                     </div>
-                </li> 
-    		    
+                </li>
+
     		    <!-- Clansuite Shortcuts -->
     		    <li><h2><?=$language['SHORTCUTS']?></h2></li>
     		  	<li><strong><a href="http://www.clansuite.com/">Website</a></strong></li>
     		  	<li><strong><a href="http://www.clansuite.com/smf/">Forum</a></strong></li>
     		  	<li><strong><a href="http://www.clansuite.com/smf/">Installsupport</a></strong></li>
     		  	<!--<li><strong><a href="http://www.clansuite.com/wiki/">Help</a></strong></li>-->
-    		 
+
     		    <!-- Change Language -->
     		  	<li><h2><?=$language['CHANGE_LANGUAGE']?></h2></li>
-    		    <li>    		    
+    		    <li>
         		    <?php # pruefen ob es die aktuelle sprache ist, die reloaded werden soll
                           # nur reloaden, wenn neue sprache ausgewaehlt                ?>
                     <select name="lang" style="width: 100px"
@@ -30,30 +30,30 @@
                     <?php
                     echo '<option value="">- Select Language -</option>';
                     foreach (new DirectoryIterator('./languages/') as $file) {
-                       // get each file not starting with dots ('.','..') 
+                       // get each file not starting with dots ('.','..')
                        // or containing ".install.php"
-                       if ((!$file->isDot()) && preg_match("/.gif$/",$file->getFilename())) 
+                       if ((!$file->isDot()) && preg_match("/.gif$/",$file->getFilename()))
                        {
                           // the shortest way to show a selected item by vain :D
-                          echo '<option style="padding-left: 30px; background-image: url(./languages/' . $file .'); background-repeat: no-repeat;"'; 
+                          echo '<option style="padding-left: 30px; background-image: url(./languages/' . $file .'); background-repeat: no-repeat;"';
                           $file = substr($file->getFilename(), 0, -4);
-                          if ($lang == $file) { echo ' selected'; }
+                          if ($lang == $file) { echo ' selected="selected"'; }
                           echo '>';
                           echo $file;
-                          echo '</option>\n';
+                          echo "</option>\n";
                        }
                     }
                     echo "</select>\n";
-                    ?>     
-    		    </li>   
+                    ?>
+    		    </li>
     		 </ul>
     </div>
-<hr />                
+<hr />
 <!-- Fusszeile -->
     <div id="footer">
         <p style="filter:alpha(opacity=65); -moz-opacity:0.65;">
        	    <br />
-            &copy; 2005-<?=date("Y"); ?> by Jens-Andr&#x00E9; Koch, Florian Wolf & Clansuite Development Team<br />
+            &copy; 2005-<?=date("Y"); ?> by Jens-Andr&#x00E9; Koch, Florian Wolf &amp; Clansuite Development Team<br />
          </p>
     </div><!-- Fusszeile ENDE -->
 </div><!-- PAGE ENDE -->
--- installation/install-step1.php
+++ installation/install-step1.php
@@ -10,61 +10,51 @@
             <div class="step-off"><?=$language['MENUSTEP7']?></div>
         </div>
     </div>
-
     <div id="content" class="narrowcolumn">
-    
         <div id="content_footer">
-                 
             <div class="accordion">
-        	   <h2 class="headerstyle">
-        	       <img src="images/64px-Tango_Globe_of_Letters.svg.png" border="0" style="vertical-align:middle" alt="installstep image" />
-        	       <?=$language['STEP1_LANGUAGE_SELECTION']?>
-        	   </h2>
-        	 <p><strong><?=$language['STEP1_WELCOME']?></strong></p>
-        	 <p><?=$language['STEP1_APPINSTALL_STEPWISE']?></p>
-        	 <p><?=$language['STEP1_CHOOSELANGUAGE']?></p>
-        	 <p>
-        	    <form action="index.php" name="lang" method="post" /> 
-        	    <input type="hidden" name="lang" value="<?=$_SESSION['lang']?>" />   
-        	    
-                <?php # @todo: nur reloaden, wenn eine neue sprache ausgewaehlt ?>      
-                <select name="lang" style="width: 160px"
-                        onchange="window.location.href='<?php echo $_SERVER['PHP_SELF']; ?>?lang='+this.options[this.selectedIndex].value;" >
+                <h2 class="headerstyle">
+        	        <img src="images/64px-Tango_Globe_of_Letters.svg.png" border="0" style="vertical-align:middle" alt="installstep image" />
+        	        <?=$language['STEP1_LANGUAGE_SELECTION']?>
+        	    </h2>
+        	    <p><strong><?=$language['STEP1_WELCOME']?></strong></p>
+        	    <p><?=$language['STEP1_APPINSTALL_STEPWISE']?></p>
+        	    <p><?=$language['STEP1_CHOOSELANGUAGE']?></p>
+        	    <form action="index.php" name="lang" method="post">
+        	        <p>
+        	            <input type="hidden" name="lang" value="<?=$_SESSION['lang']?>" />
+
+                <?php # @todo: nur reloaden, wenn eine neue sprache ausgewaehlt ?>
+                        <select name="lang" style="width: 160px"
+                            onchange="window.location.href='<?php echo $_SERVER['PHP_SELF']; ?>?lang='+this.options[this.selectedIndex].value;" >
                 <?php
                 echo '<option value="">- Select Language -</option>';
                 foreach (new DirectoryIterator('./languages/') as $file) {
-                   // get each file not starting with dots ('.','..') 
+                   // get each file not starting with dots ('.','..')
                    // or containing ".install.php"
-                   if ((!$file->isDot()) && preg_match("/.install.php$/",$file->getFilename())) 
+                   if ((!$file->isDot()) && preg_match("/.install.php$/",$file->getFilename()))
                    {
                       $file = substr($file->getFilename(), 0, -12);
                       // the shortest way to show a selected item by vain :D
-                      echo '<option'; 
-                      if ($lang == $file) { echo ' selected'; }
+                      echo '<option';
+                      if ($lang == $file) { echo ' selected="selected"'; }
                       echo '>';
                       echo $file;
-                      echo '</option>\n';
+                      echo "</option>\n";
                    }
                 }
                 echo "</select>\n";
-                ?>              
-                  
-            </p>		 
-        			 
-        			 
-        	<div class="navigation">
-        	        <hr>
+                ?>
+                    </p>
+        	        <div class="navigation">
+        	            <hr />
         	            <span style="font-size:10px;"><?=$language['CLICK_NEXT_TO_PROCEED']?></span>
-            			
-            			<div class="alignright"> 
-            			    <input type="submit" value="<?=$language['NEXTSTEP']?>" class="button" name="ButtonNext"/>
-                            <input type="hidden" name="step" value="<?=$_SESSION['step']+1 ?>">            			 
-            			 </form>
-            			 </div>
-            </div><!-- div navigation end --> 
-
-        	</div> <!-- div accordion end -->   
-    
+                        <div class="alignright">
+                            <input type="submit" value="<?=$language['NEXTSTEP']?>" class="button" name="ButtonNext" />
+                            <input type="hidden" name="step" value="<?=$_SESSION['step']+1 ?>" />
+                        </div>
+                    </div><!-- div navigation end -->
+                </form>
+        	</div> <!-- div accordion end -->
         </div> <!-- div content_footer end -->
-
     </div> <!-- div content end -->
