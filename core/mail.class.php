d51752c41d9275862c4de72ce76c336db4530b32
Jens-André Koch <jakoch@web.de>
2007-08-17 16:06:50 +0000

    - security handler changed

--- core/mail.class.php
+++ core/mail.class.php
@@ -38,10 +38,8 @@
     * @version    SVN: $Id$
     */
 
-/**
- * Security Handler
- */
-if (!defined('IN_CS')){ die('You are not allowed to view this page.' );}<?php
+// Security Handler
+if (!defined('IN_CS')){ die('Clansuite not loaded. Direct Access forbidden.' );}<?php
 
 /**
  * This Clansuite Core Class for for Mail Handling (Wrapper for SwiftMailer)
