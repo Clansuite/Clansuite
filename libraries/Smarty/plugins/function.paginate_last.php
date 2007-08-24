b5377e9196dbb702e2dfb490de9946ae9cf424fc
Jens-André Koch <jakoch@web.de>
2007-08-24 21:51:08 +0000

    - updated SmartyPaginate to v1.6-dev

--- libraries/Smarty/plugins/function.paginate_last.php
+++ libraries/Smarty/plugins/function.paginate_last.php
@@ -23,58 +23,56 @@
  * @copyright 2001-2005 New Digital Group, Inc.
  * @author Monte Ohrt <monte at newdigitalgroup dot com>
  * @package SmartyPaginate
- * @version 1.5
+ * @version 1.6-dev
  */
 
-function smarty_function_paginate_last($params, &$smarty)
-{
-    global $SmartyPaginate;
+function smarty_function_paginate_last($params, &$smarty) {
 
     $_id = 'default';
     $_attrs = array();
-
+    
     if (!class_exists('SmartyPaginate')) {
         $smarty->trigger_error("paginate_last: missing SmartyPaginate class");
         return;
     }
     if (!isset($_SESSION['SmartyPaginate'])) {
         $smarty->trigger_error("paginate_last: SmartyPaginate is not initialized, use connect() first");
-        return;
+        return;        
     }
 
     foreach($params as $_key => $_val) {
         switch($_key) {
             case 'id':
-                if (!$SmartyPaginate->isConnected($_val)) {
+                if (!SmartyPaginate::isConnected($_val)) {
                     $smarty->trigger_error("paginate_last: unknown id '$_val'");
-                    return;
+                    return;        
                 }
                 $_id = $_val;
                 break;
             default:
                 $_attrs[] = $_key . '="' . $_val . '"';
-                break;
+                break;   
         }
     }
-
-    if (SmartyPaginate->getTotal($_id) === false) {
+    
+    if (SmartyPaginate::getTotal($_id) === false) {
         $smarty->trigger_error("paginate_last: total was not set");
-        return;
+        return;        
     }
-
-    $_url = $SmartyPaginate->getURL($_id);
-    $_total = $SmartyPaginate->getTotal($_id);
-    $_limit = $SmartyPaginate->getLimit($_id);
-
-    $_attrs = !empty($_attrs) ? ' ' . implode(' ', $_attrs) : '';
-
-    $_text = isset($params['text']) ? $params['text'] : $SmartyPaginate->getLastText($_id);
+    
+    $_url = SmartyPaginate::getURL($_id);
+    $_total = SmartyPaginate::getTotal($_id);
+    $_limit = SmartyPaginate::getLimit($_id);
+    
+    $_attrs = !empty($_attrs) ? ' ' . implode(' ', $_attrs) : '';    
+    
+    $_text = isset($params['text']) ? $params['text'] : SmartyPaginate::getLastText($_id);
     $_url .= (strpos($_url, '?') === false) ? '?' : '&';
-    $_url .= $SmartyPaginate->getUrlVar($_id) . '=';
+    $_url .= SmartyPaginate::getUrlVar($_id) . '=';
     $_url .= ($_total % $_limit > 0) ? $_total - ( $_total % $_limit ) + 1 : $_total - $_limit + 1;
-
-    return '<a href="' . preg_replace('/&([^a][^m][^p][^\;])/', "&amp;$1", $_url) . '"' . $_attrs . '>' . $_text . '</a>';
-
+    
+    return '<a href="' . str_replace('&','&amp;', $_url) . '"' . $_attrs . '>' . $_text . '</a>';
+    
 }
 
 ?>
--- libraries/Smarty/plugins/function.paginate_prev.php
+++ libraries/Smarty/plugins/function.paginate_prev.php
@@ -23,65 +23,57 @@
  * @copyright 2001-2005 New Digital Group, Inc.
  * @author Monte Ohrt <monte at newdigitalgroup dot com>
  * @package SmartyPaginate
- * @version 1.5
+ * @version 1.6-dev
  */
 
-function smarty_function_paginate_prev($params, &$smarty)
-{
-    global $SmartyPaginate;
+function smarty_function_paginate_prev($params, &$smarty) {
 
     $_id = 'default';
     $_attrs = array();
-
+    
     if (!class_exists('SmartyPaginate')) {
         $smarty->trigger_error("paginate_prev: missing SmartyPaginate class");
         return;
     }
     if (!isset($_SESSION['SmartyPaginate'])) {
         $smarty->trigger_error("paginate_prev: SmartyPaginate is not initialized, use connect() first");
-        return;
+        return;        
     }
 
     foreach($params as $_key => $_val) {
         switch($_key) {
             case 'id':
-                if (!$SmartyPaginate->isConnected($_val)) {
+                if (!SmartyPaginate::isConnected($_val)) {
                     $smarty->trigger_error("paginate_prev: unknown id '$_val'");
-                    return;
+                    return;        
                 }
                 $_id = $_val;
                 break;
-
-             case 'text':
-                 //nothin... just catch
-                 ;
-                 break;
-
             default:
                 $_attrs[] = $_key . '="' . $_val . '"';
-                break;
+                break;   
         }
     }
-
-    if ($SmartyPaginate->getTotal($_id) === false) {
+    
+    if (SmartyPaginate::getTotal($_id) === false) {
         $smarty->trigger_error("paginate_prev: total was not set");
-        return;
+        return;        
     }
-
-    $_url = $SmartyPaginate->getURL($_id);
-
-    $_attrs = !empty($_attrs) ? ' ' . implode(' ', $_attrs) : '';
-
-    if(($_item = $SmartyPaginate->_getPrevPageItem($_id)) !== false) {
+    
+    $_url = SmartyPaginate::getURL($_id);
+    
+    $_attrs = !empty($_attrs) ? ' ' . implode(' ', $_attrs) : '';    
+    
+    if(($_item = SmartyPaginate::_getPrevPageItem($_id)) !== false) {
         $_show = true;
-        $_text = isset($params['text']) ? $params['text'] : $SmartyPaginate->getPrevText($_id);
+        $_text = isset($params['text']) ? $params['text'] : SmartyPaginate::getPrevText($_id);
         $_url .= (strpos($_url, '?') === false) ? '?' : '&';
-        $_url .= $SmartyPaginate->getUrlVar($_id) . '=' . $_item;
+        $_url .= SmartyPaginate::getUrlVar($_id) . '=' . $_item;
     } else {
-        $_show = false;
+        $_show = false;   
     }
-
-    return $_show ? '<a href="' . preg_replace('/&([^a][^m][^p][^\;])/', "&amp;$1", $_url) . '"' . $_attrs . '>' . $_text . '</a>' : '';
+    
+    return $_show ? '<a href="' . str_replace('&','&amp;', $_url) . '"' . $_attrs . '>' . $_text . '</a>' : '';
 }
 
 ?>
