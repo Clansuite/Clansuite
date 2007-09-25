f442cc387c43fcbc9f03f85a3ba68cb4569e4a2b
Tino Goratsch <vyper@gna.org>
2007-09-25 17:35:37 +0000

    fixed some umlauts + some xhtml fixes

--- installation/install-step4.php
+++ installation/install-step4.php
@@ -36,15 +36,15 @@
         	   <dl>
 			   		<dd><input type="hidden" name="db_type" value="mysql" /></dd>
 			   		<dt><?=$language['DB_NAME']?></dt>
-			   		<dd><input type="name" name="db_name" value="<?=$values['db_name']?>" /></dd>
+			   		<dd><input type="text" name="db_name" value="<?=$values['db_name']?>" /></dd>
 			   		<dt><?=$language['DB_HOST']?></dt>
-			   		<dd><input type="name" name="db_host" value="<?=$values['db_host']?>" /></dd>
+			   		<dd><input type="text" name="db_host" value="<?=$values['db_host']?>" /></dd>
 			   		<dt><?=$language['DB_USER']?></dt>
-			   		<dd><input type="name" name="db_user" value="<?=$values['db_username']?>" /></dd>
+			   		<dd><input type="text" name="db_user" value="<?=$values['db_username']?>" /></dd>
 			   		<dt><?=$language['DB_PASS']?></dt>
-			   		<dd><input type="name" name="db_pass" value="<?=$values['db_password']?>" /></dd>
+			   		<dd><input type="text" name="db_pass" value="<?=$values['db_password']?>" /></dd>
 			   		<dt><?=$language['DB_PREFIX']?></dt>
-			   		<dd><input type="name" name="db_prefix" value="<?=$values['db_prefix']?>" /></dd>
+			   		<dd><input type="text" name="db_prefix" value="<?=$values['db_prefix']?>" /></dd>
 			   </dl>
 
 			<p><?=$language['STEP4_SENTENCE2']; ?></p>
@@ -67,8 +67,8 @@
                             <input type="submit" value="<?=$language['BACKSTEP']?>" class="ButtonRed" name="step_backward" />
                             <input type="hidden" name="lang" value="<?=$_SESSION['lang']?>" />
                         </div>
-			</form>
-            </div><!-- div navigation end -->
+                    </div><!-- div navigation end -->
+			    </form>
 
         	</div> <!-- div accordion end -->
 
--- installation/install-step6.php
+++ installation/install-step6.php
@@ -22,17 +22,17 @@
         	   </h2>
 
         	   <p><?=$language['STEP6_SENTENCE1']?></p>
-				
+
 				<form action="index.php" method="post">
 				<dl>
 					<dt><?=$language['STEP6_ADMIN_NAME']?></dt>
-			   		<dd><input type="name" name="admin_name" value="<?=$values['admin_name']?>" /></dd>
+			   		<dd><input type="text" name="admin_name" value="<?=$values['admin_name']?>" /></dd>
 			   		<dt><?=$language['STEP6_ADMIN_PASSWORD']?></dt>
-			   		<dd><input type="name" name="admin_password" value="<?=$values['admin_password']?>" /></dd>
+			   		<dd><input type="text" name="admin_password" value="<?=$values['admin_password']?>" /></dd>
 			   		<dt><?=$language['STEP6_ADMIN_EMAIL']?></dt>
-			   		<dd><input type="name" name="admin_email" value="<?=$values['admin_email']?>" /></dd>
+			   		<dd><input type="text" name="admin_email" value="<?=$values['admin_email']?>" /></dd>
 			   		<dt><?=$language['STEP6_ADMIN_LANGUAGE']?></dt>
-			   		<dd><input type="name" name="admin_language" value="<?=$values['admin_language']?>" /></dd>
+			   		<dd><input type="text" name="admin_language" value="<?=$values['admin_language']?>" /></dd>
 			   	</dl>
 
 
@@ -47,13 +47,13 @@
 							<div class="alignright">
 	                            <input type="submit" value="<?=$language['NEXTSTEP']?>" class="ButtonGreen" name="step_forward" />
 	            			</div>
-	            			
+
 	            			<div class="alignleft">
 	                            <input type="submit" value="<?=$language['BACKSTEP']?>" class="ButtonRed" name="step_backward" />
 	                            <input type="hidden" name="lang" value="<?=$_SESSION['lang']?>" />
 	                        </div>
-							</form>
-                 </div><!-- div navigation end -->
+                        </div><!-- div navigation end -->
+					</form>
 
         	</div> <!-- div accordion end -->
 
