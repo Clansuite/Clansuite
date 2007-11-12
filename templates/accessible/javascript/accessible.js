7b6a00df9c8636e76962024e4b7ea3c4dba00c01
Tino Goratsch <vyper@gna.org>
2007-11-12 17:55:02 +0000

    Refactored the Docblock

--- templates/accessible/javascript/accessible.js
+++ templates/accessible/javascript/accessible.js
@@ -1,31 +1,36 @@
 /**
-* @desc Initialize objects, create DB link, load templates, clean input
-*
-* PHP versions 5.1.4
-*
-* LICENSE:
-*
-*    This program is free software; you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation; either version 2 of the License, or
-*    (at your option) any later version.
-*
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
-*    You should have received a copy of the GNU General Public License
-*    along with this program; if not, write to the Free Software
-*    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*
-* @author     Florian Wolf <xsign.dll@clansuite.com>
-* @author     Jens-Andre Koch <vain@clansuite.com>
-* @copyright  2006 Clansuite Group
-* @license    see COPYING.txt
-* @version    SVN: $Id$
-* @link       http://gna.org/projects/clansuite
-* @since      File available since Release 0.1
-*/
+ * Clansuite - just an E-Sport CMS
+ * Jens-Andre Koch, Florian Wolf
+ * http://www.clansuite.com/
+ * All rights reserved
+ *
+ * LICENSE:
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @license    GNU/GPL, see COPYING.txt
+ *
+ * @author     Jens-Andre Koch <vain@clansuite.com>
+ * @copyright  Jens-Andre Koch (2005-2007)
+ *
+ * @link       http://www.clansuite.com
+ * @link       http://gna.org/projects/clansuite
+ * @since      File available since Release 0.2
+ *
+ * @version    SVN: $Id: accessible.js 1498 2007-11-12 18:53:20Z vyper $
+ */
 
 function passTest()
 {
