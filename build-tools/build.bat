104c92fedab3f8ad6d5e3dcade20e71e06c93188
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2008-01-05 16:25:59 +0000

    - svn id added
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@1618 34db09b4-2314-0410-a940-aec02a7c051d

--- build-tools/build.bat
+++ build-tools/build.bat
@@ -1,3 +1,11 @@
+@REM   ,------------------------------------.
+@REM   | Phing: Clansuite Build-Script  w32 |
+@REM   | by Jens-Andre Koch (jakoch@web.de) |
+@REM   | for clansuite.com                  |
+@REM   | LICENCE: BSD                    <3 |
+@REM   `------------------------------------'
+@REM SVN: $Id$
+
 @echo off
 c:\xampplite\php\phing.bat -f build-clansuite.xml
 pause
