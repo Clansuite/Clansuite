a9bbd80f9907005e78c83812a7ccca46bd1ba5a1
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2008-01-11 00:23:44 +0000

    - licence changed from bsd to gpl - added eof
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@1643 34db09b4-2314-0410-a940-aec02a7c051d

--- build-tools/build.bat
+++ build-tools/build.bat
@@ -2,10 +2,11 @@
 @REM   | Phing: Clansuite Build-Script  w32 |
 @REM   | by Jens-Andre Koch (jakoch@web.de) |
 @REM   | for clansuite.com                  |
-@REM   | LICENCE: BSD                    <3 |
+@REM   | LICENCE: GPL                    <3 |
 @REM   `------------------------------------'
 @REM SVN: $Id$
 
 @echo off
 c:\xampplite\php\phing.bat -f build-clansuite.xml
 pause
+@REM ======= EOF build.bat =======
