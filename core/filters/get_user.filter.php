0fe900fbd6d80b14d596eb5ccbcf5b2f3c99835a
Tino Goratsch <vyper@gna.org>
2008-02-17 13:34:11 +0000

    first pass at correcting/improving phpdoc

--- core/filters/get_user.filter.php
+++ core/filters/get_user.filter.php
@@ -1,7 +1,7 @@
 <?php
    /**
     * Clansuite - just an eSports CMS
-    * Jens-Andre Koch © 2005-2008
+    * Jens-Andre Koch Â© 2005-2008
     * http://www.clansuite.com/
     *
     * LICENSE:
@@ -35,25 +35,25 @@
 /**
  * get_user Filter Function
  *
- * Purpose: Set Theme via URL by appendix $_GET['theme'] 
+ * Purpose: Set Theme via URL by appendix $_GET['theme']
  * Example: index.php?theme=themename
  * When request parameter 'theme' is set, the user session value for theme will be updated
  *
  * @implements IFilter
  */
 class get_user implements FilterInterface
-{   
+{
     private $user    = null;
-    
+
     function __construct(user $user)
     {
         $this->user = $user;
-    }   
-    
+    }
+
     public function executeFilter(httprequest $request, httpresponse $response)
     {
        $this->user->create_user();		    # Create a user (empty)
-       $this->user->check_login_cookie();	# Check for login cookie - Guest/Member  
+       $this->user->check_login_cookie();	# Check for login cookie - Guest/Member
     }
 }
 
--- core/filters/theme_via_get.filter.php
+++ core/filters/theme_via_get.filter.php
@@ -1,7 +1,7 @@
 <?php
    /**
     * Clansuite - just an eSports CMS
-    * Jens-Andre Koch © 2005-2008
+    * Jens-Andre Koch Â© 2005-2008
     * http://www.clansuite.com/
     *
     * LICENSE:
@@ -48,32 +48,32 @@ class theme_via_get implements FilterInterface
     private $config     = null;
     private $input      = null;
     private $security   = null;
-    
+
     function __construct(configuration $config, input $input, security $security)
     {
        $this->config    = $config;
        $this->input     = $input;
        $this->security  = $security;
-    }   
-    
+    }
+
     public function executeFilter(httprequest $request, httpresponse $response)
-    { 
+    {
         // take the initiative, if themeswitching is enabled in CONFIG
         // or pass through (do nothing)
         if($this->config['themeswitch_via_url'] == 1)
-        {              
+        {
             if(isset($request['theme']) && !empty($request['theme']))
-            {   
-                #@todo debug traceing message           
+            {
+                #@todo debug traceing message
                 #echo 'processing themefilter';
-            	
+
             	// Security Handler for $_GET['theme']
             	if( !$this->input->check( $request['theme'], 'is_abc|is_custom', '_' ) )
-                {   
+                {
                     // @todo umstellen auf thrown Exception
                     $this->security->intruder_alert();
                 }
-                
+
                 // If $_GET['theme'] dir exists, set it as session-user-theme
                 if(is_dir(ROOT_TPL . '/' . $request['theme'] . '/'))
                 {
