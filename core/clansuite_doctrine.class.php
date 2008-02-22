897b1a85c54ec6dd2be70d130d7f45f09f195df2
Jens-André Koch <jakoch@web.de>
2008-02-22 04:43:17 +0000

    - i cleaned this mess of a class up by removing all testing examples. - the class is added to the DI container - bootstrap loads Doctrine & prepareDbConnection "sets us up the bomb" :)

--- core/clansuite_doctrine.class.php
+++ core/clansuite_doctrine.class.php
@@ -1,8 +1,44 @@
 <?php
+   /**
+    * Clansuite - just an eSports CMS
+    * Jens-Andre Koch © 2005-2008
+    * http://www.clansuite.com/
+    *
+    *
+    * LICENSE:
+    *
+    *    This program is free software; you can redistribute it and/or modify
+    *    it under the terms of the GNU General Public License as published by
+    *    the Free Software Foundation; either version 2 of the License, or
+    *    (at your option) any later version.
+    *
+    *    This program is distributed in the hope that it will be useful,
+    *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    *    GNU General Public License for more details.
+    *
+    *    You should have received a copy of the GNU General Public License
+    *    along with this program; if not, write to the Free Software
+    *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    *
+    * @license    GNU/GPL, see COPYING.txt
+    *
+    * @author     Jens-Andre Koch   <vain@clansuite.com>
+    * @copyright  Jens-Andre Koch (2005-$LastChangedDate: 2008-02-18 23:18:47 +0100 (Mo, 18 Feb 2008) $)
+    *
+    * @link       http://www.clansuite.com
+    * @link       http://gna.org/projects/clansuite
+    * @since      File available since Release 0.2
+    *
+    * @version    SVN: $Id$
+    */
+// Security Handler
+if (!defined('IN_CS')){die('Clansuite not loaded. Direct Access forbidden.');}
+
 /**
- * phpDoctrine - Clansuite Initialization Class
+ * Clansuite Core Class for the Initialization of phpDoctrine
  *
- * The library provides a database abstraction layer (DAL) 
+ * The phpDoctrine library provides a database abstraction layer (DAL)
  * as well as object-relational mapping (ORM)
  * with drivers for every PDO-supported database:
  *
@@ -15,13 +51,26 @@
  *    - PostgreSQL
  *    - Sqlite
  */
+
 class Clansuite_Doctrine
 {
+    public $db = null; # holds a db instance
+
+    function __construct(configuration $config)
+    {
+
+        $this->config = $config; # set config instance
+    }
+
     /**
      * Doctrine Bootstrap
+     *
+     * 1. Loads the compiled (if exists) or normal Doctrine Base Class
+     * 2. registers autoload
+     * 3. Prepares a connection to the Database
      */
-    public static function doctrine_bootstrap() 
-    {   
+    public function doctrine_bootstrap()
+    {
         // Require compiled or normal Library
         if (is_file( ROOT_LIBRARIES . '/doctrine/Doctrine.compiled.php'))
         {
@@ -31,12 +80,12 @@ class Clansuite_Doctrine
         {
             require_once ROOT_LIBRARIES .'/doctrine/Doctrine.php';
         }
-        // Register autoloader         
+        // Register autoloader
         spl_autoload_register(array('Doctrine', 'autoload'));
-        
-        self::prepareDbConnection();
+        // db connection
+        $this->prepareDbConnection();
     }
-    
+
     /**
      * prepareDbConnection
      *
@@ -46,152 +95,66 @@ class Clansuite_Doctrine
      *
      * @link DNS Types on Doctrine Chapter 4.1.
      */
-    public static function prepareDbConnection()
+    public function prepareDbConnection()
     {
+
         // construct the Data Source Name (DSN)
-        $dsn = 'mysql://clansuite:toop@localhost/clansuite';
-        
-        // check connection
-        /*
-        try
-        {
-            $pdo = new PDO($dsn);
-        } 
-        catch (PDOException $e) 
-        {
-            echo 'Connection failed: ' . $e->getMessage();
-        }
-        */
-    
+        #$dsn = 'mysql://clansuite:toop@localhost/clansuite';
+        $dsn = $this->config['db_type'].'://'.$this->config['db_username'].':'.$this->config['db_password'].'@'.$this->config['db_host'].'/'.$this->config['db_name'];
+
         // initalize a new Doctrine_Connection
         $db = Doctrine_Manager::connection($dsn);
+
         // !! no actual database connection yet !!
-    
+        # object(Doctrine_Connection_Mysql) -> 'isConnected' => false
+        # var_dump($db);
+
+        /**
+                    * Setup phpDoctrine Attributes for that later Connection
+                    */
+
         // Changing the database naming convention by adding DB_PREFIX
         $db->setAttribute(Doctrine::ATTR_DBNAME_FORMAT, DB_PREFIX.'_%s');
-        
+
         /**
-         * Aggressive - It finds all .php files in a given path recursively and
-           performs a require_once() on each file. Your files can be in subfolders, and
-           the files can include multiple models. It is very flexible but the downside
-           is that it will require_once() all files.
-           
-           Conservative - It finds all .php files in a given path recursively and
-           builds an array of className => /path/to/file. The className is parsed from
-           the name of the file, so each file must contain only one class and the file
-           must be named after the class inside of it. This array is then referenced in
-           Doctrine::autoload() and used to load models when they are asked for. 
-           Johnatan Wage on http://groups.google.com/group/doctrine-user
-         */
-        $db->setAttribute(Doctrine::ATTR_MODEL_LOADING, Doctrine::MODEL_LOADING_CONSERVATIVE); 
-    
-        // case-sensitive: 
-        // array('ModelName' => '/path/to/ModelName.php'); 
-        #$db->loadModels($this->get_path('model')); 
-    
-        # Set Attributes for that later Connection
-    
+                     * Aggressive - It finds all .php files in a given path recursively and
+                       performs a require_once() on each file. Your files can be in subfolders, and
+                       the files can include multiple models. It is very flexible but the downside
+                       is that it will require_once() all files.
+
+                       Conservative - It finds all .php files in a given path recursively and
+                       builds an array of className => /path/to/file. The className is parsed from
+                       the name of the file, so each file must contain only one class and the file
+                       must be named after the class inside of it. This array is then referenced in
+                       Doctrine::autoload() and used to load models when they are asked for.
+                       Johnatan Wage on http://groups.google.com/group/doctrine-user
+                     */
+        $db->setAttribute(Doctrine::ATTR_MODEL_LOADING, Doctrine::MODEL_LOADING_CONSERVATIVE);
+
+        // case-sensitive:
+        // array('ModelName' => '/path/to/ModelName.php');
+        #$db->loadModels($this->get_path('model'));
+
         // Validate All
         #$db->setAttribute(Doctrine::ATTR_VALIDATE, Doctrine::VALIDATE_ALL);
-    
+
         // Set portability for all rdbms = default
         #$db->setAttribute('portability', Doctrine::PORTABILITY_ALL);
-    
+
         // identifier quoting
         // disabled for now, because we have no reserved words as a field names
         #$db->setAttribute(Doctrine::ATTR_QUOTE_IDENTIFIER, true);
-        
+
         // Set Cache Driver
         /**
-         * Doctrine_Cache_Exception: The apc extension must be loaded for using this backend !
-        $cacheDriver = new Doctrine_Cache_Apc();
-        $db->setAttribute(Doctrine::ATTR_RESULT_CACHE, $cacheDriver);
-        // set the lifespan as one hour (60 seconds * 60 minutes = 1 hour = 3600 secs)
-        $db->setAttribute(Doctrine::ATTR_RESULT_CACHE_LIFESPAN, 3600);
-        */
-    
-        # $db->execute("SET CHARACTER SET utf8");
-        // ---- DO QUERY ---- //
-    
-        // connects database and performs a query
-        #$db->query('FROM User u');
-    
-       /**
-        *   $conn = Doctrine_Manager::getInstance()
-        *           ->openConnection(new PDO("dsn", "username", "pw"));
-        */
+                     * Doctrine_Cache_Exception: The apc extension must be loaded for using this backend !
+                    $cacheDriver = new Doctrine_Cache_Apc();
+                    $db->setAttribute(Doctrine::ATTR_RESULT_CACHE, $cacheDriver);
+                    // set the lifespan as one hour (60 seconds * 60 minutes = 1 hour = 3600 secs)
+                    $db->setAttribute(Doctrine::ATTR_RESULT_CACHE_LIFESPAN, 3600);
+                    */
+        # set character set
+        #$db->execute("SET CHARACTER SET utf8");
     }
 }
-
-/**
-
-try {
-    $conn->execute('SELECT * FROM unknowntable');
-} catch (Doctrine_Connection_Exception $e) {
-    print 'Code : ' . $e->getPortableCode();
-    print 'Message : ' . $e->getPortableMessage();
-}
-
-
-$q = new Doctrine_Query();
-
-// Doctrine uses 'connection 1' for fetching here
-$users = $q->from('User u')->where('u.id IN (1,2,3)')->execute();
-
-// Doctrine uses 'connection 2' for fetching here
-$groups = $q->from('Group g')->where('g.id IN (1,2,3)')->execute();
-
-# Delete
-
-$q->delete()->from($modelname)->execute(); 
-
-
-$users = Doctrine_Query::create()->select('u.name, u.password')->from('User u')->execute();
-
-###
-
-$r = Doctrine_Manager::getInstance()->getQueryRegistry();
-
-$r->add('User/all', 'FROM User u');
-$r->add('User/byName', 'FROM User u WHERE u.name = ?');
-
-$user = new User();
-
-// find the user named Jack Daniels
-$user = $user->findOne('byName', array('Jack Daniels'));
-
-// find all users
-$users = $user->find('all');
-
-
-findBy%s($value)
-$user = $userTable->findOneByUsername('jonwage');
-findOneBy%s($value)
-
-
-// Array fetching is the best choice whenever you need data read-only like passing it to the view for display. 
-$comments = $query->select("c.id, ...")->from("Comment c")
-        ->leftJoin("c.foo f")
-        ->leftJoin("f.bar b")
-        ->where("c.article_id = ?")
-        ->execute(array(1), Doctrine::FETCH_ARRAY);
-$view->comments = $comments;
-
-# ----- #
-
-$users = $query->select('u.name')->from('User u INDEXBY
-u.id')->execute(array(), Doctrine::HYDRATE_ARRAY);
-
-That gets you an array where the keys are the ids and the values is an
-assoc array with just the name name.
-
-foreach ($users as $key => $user) {
-  echo $user['name'];
-
-}
-
-The HYDRATE_ARRAY option reduces a lot the overheads because you are
-not instatiating new records. 
-
-*/
 ?>
