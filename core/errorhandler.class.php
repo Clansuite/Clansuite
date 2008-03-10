c77a473a0a47160b095ae1e7d9aef14d366bf352
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2008-03-10 14:26:42 +0000

    - errormessage layout changed
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@1776 34db09b4-2314-0410-a940-aec02a7c051d

--- core/errorhandler.class.php
+++ core/errorhandler.class.php
@@ -230,32 +230,39 @@ class errorhandler
     public function ysod( $ErrorObject, $error_head = 'Unknown Error', $string = '', $error_level = 3, $redirect = '' )
     {
         # Header
-        echo '<html><head>';
-        echo '<title>Clansuite Error - '. $error_head .' - Errortype '. $error_level .'</title>';
-        echo '<body>';
-        echo '<link rel="stylesheet" href="'. WWW_ROOT .'/templates/core/css/error.css" type="text/css" />';
-        echo '</head>';
+        $errormessage    = '<html><head>';
+        $errormessage   .= '<title>Clansuite Error - '. $error_head .' | Errorcode: '. $error_level .'</title>';
+        $errormessage   .= '<body>';
+        $errormessage   .= '<link rel="stylesheet" href="'. WWW_ROOT_THEMES_CORE .'css/error.css" type="text/css" />';
+        $errormessage   .= '</head>';
         # Body
-        echo '<body>';
-        # Fieldset with colours
+        $errormessage   .= '<body>';
+        # Fieldset with colours (error_red, error_orange, error_beige)
         if ($error_level == 1)     { echo '<fieldset class="error_red">'; }
         elseif ($error_level == 2) { echo '<fieldset class="error_orange">'; }
         elseif ($error_level == 3) { echo '<fieldset class="error_beige">'; }
+        # Errorlogo
+        $errormessage   .= '<div style="float: left; margin: 5px; margin-right: 25px; border:1px inset #bf0000; padding: 20px;">';
+        $errormessage   .= '<img src="'. WWW_ROOT_THEMES_CORE .'/images/Clansuite-Toolbar-Icon-64-error.png" style="border: 2px groove #000000;"/></div>';
         # Fieldset Legend
-        echo "<legend>Clansuite Error: $error_head</legend>";
+        $errormessage   .= '<legend>Clansuite Error: $error_head</legend>';
         # Error String (passed Error Description)
-        echo '<p><strong>'.$string.'</strong>';
+        $errormessage   .= '<p><strong>'.$ErrorObject->message.'</strong>';
         # Error Messages from the ErrorObject
-        echo '<hr><table>';
-        echo '<tr><td><strong>ErrorCode:</strong></td><td>'.$ErrorObject -> getCode().'</td></tr>';
-        echo '<tr><td><strong>Message:</strong></td><td>'.$ErrorObject -> getMessage().'</td></tr>';
-        echo '<tr><td><strong>Pfad :</strong></td><td>'. dirname($ErrorObject -> getFile()).'</td></tr>';
-        echo '<tr><td><strong>Datei :</strong></td><td>'. basename($ErrorObject -> getFile()).'</td></tr>';
-        echo '<tr><td><strong>Zeile :</strong></td><td>'.$ErrorObject -> getLine().'</td></tr>';
-        echo '</table>';
+        $errormessage   .= '<hr><table>';
+        $errormessage   .= '<tr><td><strong>Errorcode:</strong></td><td>'.$ErrorObject->getCode().'</td></tr>';
+        $errormessage   .= '<tr><td><strong>Message:</strong></td><td>'.$ErrorObject->getMessage().'</td></tr>';
+        $errormessage   .= '<tr><td><strong>Pfad :</strong></td><td>'. dirname($ErrorObject->getFile()).'</td></tr>';
+        $errormessage   .= '<tr><td><strong>Datei :</strong></td><td>'. basename($ErrorObject->getFile()).'</td></tr>';
+        $errormessage   .= '<tr><td><strong>Zeile :</strong></td><td>'.$ErrorObject->getLine().'</td></tr>';
+        $errormessage   .= '</table>';
+        $errormessage   .= '</table>';
         # @todo tracing here?
-        echo '</fieldset>';
-        echo '</body></html>';
+        $errormessage   .= '</fieldset>';
+        $errormessage   .= '</body></html>';
+
+        # Output the errormessage
+        echo $errormessage;
     }
 
     /**
