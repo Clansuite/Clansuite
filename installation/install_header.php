cf14d255784967ebeba2b38d208e97cfaf8d184c
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2008-03-14 01:28:36 +0000

    - removed nocache meta-tags, cleanup, wsr
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@1785 34db09b4-2314-0410-a940-aec02a7c051d

--- installation/install_header.php
+++ installation/install_header.php
@@ -1,38 +1,22 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
    	<meta name="robots" content="index,follow" />
-
-	<meta http-equiv="expires" content="Fri, Jan 01 1900 00:00:00 GMT" />
-	<meta http-equiv="pragma" content="no-cache" />
-	<meta http-equiv="cache-control" content="no-cache" />
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<meta http-equiv="content-language" content="en,de" />
 	<meta http-equiv="reply-to" content="system@clansuite.com" />
-
 	<link rel="shortcut icon" href="images/Clansuite-Favicon-16.ico" />
-	
     <link rel="stylesheet" type="text/css" href="installation.css" />
-
 	<title>Clansuite.com - just an eSport CMS :: Installation</title>
-
 </head>
 <body>
-
 <div id="page">
-
     <div id="header">
         <div id="headerimg">
-           <!--
-           <h1><a style="margin-left: 220px;" href=""></a></h1>
-           -->
-    	   <div style="font-size:20px; margin-left: 190px; padding-top: 100px;" class="description">Installation</div>
+          <div style="font-size:20px; margin-left: 190px; padding-top: 100px;" class="description">Installation</div>
     	   <span>
-    	       <!-- note by vain: when using h1 text in headerimg correct frist margin value !! -->
     	       <img style="margin: -86px 38px 0pt; position:relative;" src="images/clansuite-joker.gif"  alt="Clansuite Joker Logo" />
     	   </span>
     	</div>
     </div>
-
-    <hr />    
+<hr />
