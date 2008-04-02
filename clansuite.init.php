54cdb2e29d20a58c50c182df12093c0dffa6cbc9
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2008-04-02 23:25:30 +0000

    - added startup errors
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@1843 34db09b4-2314-0410-a940-aec02a7c051d

--- clansuite.init.php
+++ clansuite.init.php
@@ -64,7 +64,8 @@ define('DEBUG', $config['debug']);
 # If Debug is enabled, set FULL error_reporting, else DISABLE it completely
 if ( defined('DEBUG') && DEBUG===1 )
 {
-    ini_set('display_errors',   true);  # display errors in the browser
+    ini_set('display_startup_errors', true);
+    ini_set('display_errors', true);    # display errors in the browser    
     error_reporting(E_ALL | E_STRICT);  # all errors and strict standard optimizations
 }
 else
