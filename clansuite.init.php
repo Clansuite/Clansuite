be2cb2b5ed17a2922db15995d73a4c7a4bd3894f
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2008-04-04 03:21:11 +0000

    - version check fix
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@1882 34db09b4-2314-0410-a940-aec02a7c051d

--- clansuite.init.php
+++ clansuite.init.php
@@ -44,7 +44,7 @@ if (!defined('IN_CS')){ die('Clansuite not loaded. Direct Access forbidden.' );}
  */
 # PHP Version Check
 define('REQUIRED_PHP_VERSION', '5.2');
-if (version_compare(PHP_VERSION, '5.2', '<') == true) { die('Your PHP Version: <b>' . PHP_VERSION . '</b>! Clansuite requires PHP <b>' . REQUIRED_PHP_VERSION . '</b>'); }
+if (version_compare(PHP_VERSION, REQUIRED_PHP_VERSION, '<') == true) { die('Your PHP Version: <b>' . PHP_VERSION . '</b>! Clansuite requires PHP <b>' . REQUIRED_PHP_VERSION . '</b>'); }
 # PDO Check
 if (!class_exists('pdo')) { die('<i>php_pdo</i> not enabled!'); }
 # PDO mysql driver Check
