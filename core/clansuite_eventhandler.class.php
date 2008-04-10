9b85df6f884bc68fbc957c26dd81b0112367594b
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2008-04-10 21:23:05 +0000

    - eventhandling / hookmanagement added
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@1916 34db09b4-2314-0410-a940-aec02a7c051d

---
+++ core/clansuite_eventhandler.class.php
@@ -0,0 +1,224 @@
+<?php
+   /**
+    * Clansuite - just an eSports CMS
+    * Jens-Andre Koch © 2005-2008
+    * http://www.clansuite.com/
+    *
+    *
+    * LICENSE:
+    *
+    *    This program is free software; you can redistribute it and/or modify
+    *    it under the terms of the GNU General Public License as published by
+    *    the Free Software Foundation; either version 2 of the License, or
+    *    (at your option) any later version.
+    *
+    *    This program is distributed in the hope that it will be useful,
+    *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    *    GNU General Public License for more details.
+    *
+    *    You should have received a copy of the GNU General Public License
+    *    along with this program; if not, write to the Free Software
+    *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    *
+    * @license    GNU/GPL, see COPYING.txt
+    *
+    * @author     Jens-Andre Koch   <vain@clansuite.com>
+    * @copyright  Jens-Andre Koch (2005-$LastChangedDate$)
+    *
+    * @link       http://www.clansuite.com
+    * @link       http://gna.org/projects/clansuite
+    * @since      File available since Release 0.2
+    *
+    * @version    SVN: $Id$
+    */
+// Security Handler
+if (!defined('IN_CS')){die('Clansuite not loaded. Direct Access forbidden.');}
+
+/**
+ * Interface for Clansuite_EventHandler
+ *
+ * The Clansuite_EventHandler has to implement a method handle()
+ *
+ * @package     clansuite
+ * @subpackage  eventhandler
+ * @category    interfaces
+ */
+interface Clansuite_EventHandler
+{
+    # methods gets an Event Object
+    public function handle(Event $event);
+}
+
+/**
+ * Clansuite_EventManager
+ *
+ * Is a parent Class for all the EventHandlers.
+ * Performs an lookup of the eventname over all registered eventhandlers 
+ * and fires the event, if found.
+ *
+ * @package     clansuite
+ * @subpackage  eventhandler
+ * @category    core
+ */
+class Clansuite_EventManager
+{
+    # holds instance of this class (singleton)
+    static private $instance;
+
+    # holds eventhandlers
+    private $eventhandlers = array();
+
+    /**
+     * Clansuite_EventManager is a Singelton implementation
+     *
+     * @static
+     * @access public
+     */
+    static public function()
+    {
+        if (self::$instance === null)
+        {
+            self::instance = new Clansuite_EventManager();
+        }
+    }
+
+    /**
+     * add an EventHandler to the Eventhandlers Array
+     *
+     * @param $eventName Name of the Event
+     * @param $event Instance of Clansuite_EventHandler
+     */
+    public function addHandler($eventName, Clansuite_EventHandler $event)
+    {
+        #
+        if (!isset->eventhandlers[$eventName])
+        {
+            $this->eventhandlers[$eventName] = $array();
+        }
+
+        # add event to the eventhandler list
+        $this->eventhandlers[$eventName] = $event;
+    }
+
+    /**
+     * handle = getName
+     */
+    public function handle()
+    {
+        print $event->getName();
+    }
+
+    /**
+     * triggerEvent
+     *
+     * @param $name
+     * @param $context default null
+     * @param $info default null
+     */
+    public function triggerEvent($event, $context = null, $info = null)
+    {
+        # init Event Class with constructor settings
+        if(!$event instanceof Event)
+        {
+            $event = new Event($event, $context, $info);
+        }
+
+        # get the Name
+        $eventName = $event->getName();
+
+        if (!isset($this->eventhandlers[$eventName]))
+        {
+            return $event;
+        }
+
+        # loop over all eventhandlers and look for that eventname
+        foreach ($this->eventhandlers[$eventName] as $eventhandler)
+        {
+            # handle the event !!
+            $eventhandler->handle($event);
+
+            # break, on cancelled
+            if($event->isCancelled())
+            {
+                break;
+            }
+        }
+        return $event;
+    }
+
+    # no construct (singleton)
+    protected function __construct(){}
+    # no clone (singleton)
+    private function __clone(){}
+}
+
+/**
+ * Represents an Event
+ *
+ * @package     clansuite
+ * @subpackage  eventhandler
+ * @category    core
+ */
+class Event
+{
+    # name of the event
+    private $name;
+    # context of the event triggering, the object from where we are calling
+    private $context;
+    # some informations
+    private $info;
+    # cancel state of the event
+    private $cancelled = false;
+
+    /**
+     * Event constructor
+     *
+     * @param $name
+     * @param $context default null
+     * @param $info default null
+     */
+    public function __construct($name, $context = null, $info = null)
+    {
+
+    }
+
+    /**
+     * getName
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * getContext
+     */
+    public function getContext()
+    {
+        return $this->context;
+    }
+
+    /**
+     * getInfo
+     */
+    public function getInfo()
+    {
+        return $this->info;
+    }
+
+    /**
+     * getCancelled
+     */
+    public function getCancelled()
+    {
+        return $this->cancelled;
+    }
+
+    # action cancel, sets cancelled to true
+    public function cancel()
+    {
+        $this->cancelled = true;
+    }
+}
+?>
