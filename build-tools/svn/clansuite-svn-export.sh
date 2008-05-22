410e1ffbae9c19c6ed069c949c61d856ec06d66b
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2008-05-22 20:53:53 +0000

    - archives building (zip / tar.gz) from svn-export - gpg signaturing of webinstaller and both archiv files - rsync to download.gna.org
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@2055 34db09b4-2314-0410-a940-aec02a7c051d

--- build-tools/svn/clansuite-svn-export.sh
+++ build-tools/svn/clansuite-svn-export.sh
@@ -14,6 +14,15 @@
 # 2. If checkout exists, svn update will incrementally update the checkout
 # 3. Based on that checkout, perform an svn export
 #    This safes bandwidth, instead of exporting directly from gna.
+# 4. SVN EXPORT Directory is archived as ".zip" and ".tar.gz"
+#    into /home/clansuite/downloads/
+# -----
+# Create, signature and upload Archives
+# -----
+# 5. Archives are build
+# 6. Webinstaller is fetched into the /downloads dir
+# 7. Archives are GPG Signatured
+# 8. All files are rsynced to download.gna.org
 
 # Variables
 # SVN Directory to work on
@@ -42,3 +51,44 @@ rm -rf $EXPORT_DIR
 #echo "SVN EXPORT from GNA -- done --"
 svn export $CHECKOUT_DIR $EXPORT_DIR
 echo "SVN EXPORT from local SVN -- done --"
+
+#--------
+
+# ARCHIVE 
+# ZIP 
+# a new, mx=compresslevel7, t = zip, r recurse
+7z a -mx7 -tzip /home/clansuite/downloads/clansuite.zip -r /home/clansuite/svn-export
+echo "ZIP created!"
+
+# TAR.GZ
+# c=create new archive, v=verbose on, f=write to file, z=zip
+tar cvzf /home/clansuite/downloads/clansuite.tar.gz /home/clansuite/svn-export
+echo "TAR.GZ created!"
+
+#--------
+
+# Copy Webinstaller into /home/clansuite/downloads
+cp /home/clansuite/svn-export/installation/webinstaller/* /home/clansuite/downloads/
+
+#--------
+
+# CREATE GPG SIGNATURES
+gpg --detach /home/clansuite/downloads/webinstaller.php
+gpg --detach /home/clansuite/downloads/clansuite.zip
+gpg --detach /home/clansuite/downloads/clansuite.tar.gz
+echo "Signatures created!"
+
+#--------
+
+# SCP - COPY STUFF TO GNA Downloads
+#scp /home/clansuite/downloads/clansuite.zip* vain@download.gna.org:/upload/clansuite/
+#scp /home/clansuite/downloads/clansuite.tar.gz vain@download.gna.org:/upload/clansuite/
+
+# RSYNC - BETTER
+# 1) Get the content on downloag.gna.org
+#rsync -avr --rsh="ssh" vain@download.gna.org:/upload/clansuite/ /home/clansuite/downloads/
+#echo "Content Fetch from GNA!"
+# 2) Put the content on download.gna.org
+rsync --delete -avr --rsh="ssh" /home/clansuite/downloads/ vain@download.gna.org:/upload/clansuite
+echo "Content Uploaded to GNA!"
+echo "---> Mission Complete! <---"
