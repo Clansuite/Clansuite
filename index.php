e03c70f40535c10a74a724e38c8e1c6351fe4de0
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2008-06-28 18:31:34 +0000

    - copyright fun
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@2120 34db09b4-2314-0410-a940-aec02a7c051d

--- index.php
+++ index.php
@@ -1,27 +1,30 @@
-<?php /*
-             _\|/_
-             (o o)
-    +-----oOO-{_}-OOo------------------+
-    | Clansuite - just an eSports CMS  |
-    | Jens-Andre Koch © 2005 - onwards |
-    | http://www.clansuite.com/        |
-    +----------------------------------+
+<?php
+   /**
+    * Clansuite - just an eSports CMS
+    * Jens-Andre Koch © 2005 - onwards
+    * http://www.clansuite.com/
     *
-    * LICENSE:
-    *
-    *    This program is free software; you can redistribute it and/or modify
-    *    it under the terms of the GNU General Public License as published by
-    *    the Free Software Foundation; either version 2 of the License, or
-    *    (at your option) any later version.
-    *
-    *    This program is distributed in the hope that it will be useful,
-    *    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    *    GNU General Public License for more details.
-    *
-    *    You should have received a copy of the GNU General Public License
-    *    along with this program; if not, write to the Free Software
-    *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    *        _\|/_
+    *        (o o)
+    +-----oOO-{_}-OOo------------------------------------------------------------------+
+    |                                                                                  |
+    | LICENSE:                                                                         |
+    |                                                                                  |
+    |    This program is free software; you can redistribute it and/or modify          |
+    |    it under the terms of the GNU General Public License as published by          |
+    |    the Free Software Foundation; either version 2 of the License, or             |
+    |    (at your option) any later version.                                           |
+    |                                                                                  |
+    |    This program is distributed in the hope that it will be useful,               |
+    |    but WITHOUT ANY WARRANTY; without even the implied warranty of                |
+    |    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                 |
+    |    GNU General Public License for more details.                                  |
+    |                                                                                  |
+    |    You should have received a copy of the GNU General Public License             |
+    |    along with this program; if not, write to the Free Software                   |
+    |    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA    |
+    |                                                                                  |
+    +----------------------------------------------------------------------------------+
     *
     * @license    GNU/GPL, see COPYING.txt
     *
@@ -76,7 +79,7 @@ $injector = new Phemto();
 
 # core classes to load
 $core_classes = array(
-'Clansuite_Config', 'errorhandler', 'httprequest', 'httpresponse', 'filtermanager', 
+'Clansuite_Config', 'errorhandler', 'httprequest', 'httpresponse', 'filtermanager',
 'db', 'clansuite_doctrine','localization', 'security', 'input', 'functions', 'statistic'
 );
 foreach($core_classes as $class) { $injector->register(new Singleton($class)); }
