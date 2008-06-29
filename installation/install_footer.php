c7901429a2ddbdd410f8d3570e14db2db1311c30
Jens-André Koch <jakoch@web.de>
2008-06-29 23:51:11 +0000

    - css fixes - header& footer tabs to ws - footer corrected

--- installation/install_footer.php
+++ installation/install_footer.php
@@ -7,32 +7,24 @@ if (!defined('IN_CS')){ die( 'Clansuite not loaded. Direct Access forbidden.' );
 date_default_timezone_set('Europe/Berlin');
 ?>
    <div id="rightsidebar">
-    		<ul>
-    		    <!-- Installation Progress BAR -->
-    		    <li><h2><?=$language['INSTALL_PROGRESS']?></h2></li>
-    		    <li><?=$language['COMPLETED']?> <b><?=$_SESSION['progress']?>%</b>
-    		      <div id="progressbar">
-    		            <?php
-    		            #note by vain: this fixes a 2pixel problem while displaying the progress bar at 100percent:P
-    		            if ($_SESSION['step'] == 7 ) { $_SESSION['progress'] = $_SESSION['progress'] - 2; }
-    		            ?>
+            <ul>
+
+                <!-- Installation Progress BAR -->
+                <li><h2><?=$language['INSTALL_PROGRESS']?></h2></li>
+                <li><?=$language['COMPLETED']?> <b><?=$_SESSION['progress']?>%</b>
+                  <div id="progressbar">
+                        <?php
+                        #note by vain: this fixes a 2pixel problem while displaying the progress bar at 100percent:P
+                        if ($_SESSION['step'] == 7 ) { $_SESSION['progress'] = $_SESSION['progress'] - 2; }
+                        ?>
                         <div style="border: 1px solid white; height: 5px ! important; width: <?=$_SESSION['progress']?>px; background-color: rgb(181, 0, 22);"/>
                     </div>
                 </li>
 
-    		    <!-- Clansuite Shortcuts -->
-    		    <li><h2><?=$language['SHORTCUTS']?></h2></li>
-    		  	<li><strong><a href="http://www.clansuite.com/">Website</a></strong></li>
-    		  	<li><strong><a href="http://www.clansuite.com/smf/">Forum</a></strong></li>
-    		  	<li><strong><a href="http://www.clansuite.com/smf/index.php?board=4">Installsupport</a></strong></li>
-    		  	<li><strong><a href="http://www.clansuite.com/trac/">Bugtracker</a></strong></li>
-    		  	<li><strong><a href="teamspeak://clansuite.com:8000?channel=Clansuite%20Admins?subchannel=Clansuite%20Support">Teamspeak</a></strong></li>
-    		  	<!--<li><strong><a href="http://www.clansuite.com/wiki/">Help</a></strong></li>-->
-
-    		    <!-- Change Language -->
-    		  	<li><h2><?=$language['CHANGE_LANGUAGE']?></h2></li>
-    		    <li>
-        		    <?php # pruefen ob es die aktuelle sprache ist, die reloaded werden soll
+                <!-- Change Language -->
+                <li><h2><?=$language['CHANGE_LANGUAGE']?></h2></li>
+                <li>
+                    <?php # pruefen ob es die aktuelle sprache ist, die reloaded werden soll
                           # nur reloaden, wenn neue sprache ausgewaehlt                ?>
                     <select name="lang" style="width: 100px"
                             onchange="window.location.href='<?php echo $_SERVER['PHP_SELF']; ?>?lang='+this.options[this.selectedIndex].value;" >
@@ -54,17 +46,45 @@ date_default_timezone_set('Europe/Berlin');
                     }
                     echo "</select>\n";
                     ?>
-    		    </li>
-    		 </ul>
+                </li>
+
+                <!-- Clansuite Shortcuts -->
+                <li><h2><?=$language['SHORTCUTS']?></h2></li>
+                <li><strong><a href="http://www.clansuite.com/">Website</a></strong></li>
+                <li><strong><a href="http://www.clansuite.com/smf/">Forum</a></strong></li>
+                <li><strong><a href="http://www.clansuite.com/smf/index.php?board=4">Installsupport</a></strong></li>
+                <li><strong><a href="http://www.clansuite.com/trac/">Bugtracker</a></strong></li>
+                <li><strong><a href="teamspeak://clansuite.com:8000?channel=Clansuite%20Admins?subchannel=Clansuite%20Support">Teamspeak</a></strong></li>
+                <li><strong><a href="http://www.clansuite.com/toolbar/">Toolbar</a></strong></li>
+
+                <!-- Donate -->
+                <li><h2>Donate</h2></li>
+                <li>
+                <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                <input type="hidden" name="cmd" value="_s-xclick" />
+                <input type="image" src="https://www.paypal.com/de_DE/i/btn/x-click-but04.gif" name="submit" alt="Zahlen Sie mit PayPal - schnell, kostenlos und sicher!" />
+                <img alt="" border="0" src="https://www.paypal.com/de_DE/i/scr/pixel.gif" width="1" height="1" />
+                <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHTwYJKoZIhvcNAQcEoIIHQDCCBzwCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYB0LEEuVZOTu++bRevqW4bD4mdoGvWnTwCQ4urr8cax4ilsFehU4sl729m3S9QtPQv0B7CFhtWGxJ7pXhx3cQ35nTzobkxCYRYy01Aw0Gkmlxnc+6Rz7lIjAKOnL6U9Ftr7iCJH74c6ryJSlI8QB9dsqUi2YBsgfljyx5w/bunS9TELMAkGBSsOAwIaBQAwgcwGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIXabqVcNbyPOAgaiz4LIIs8323fnbtieAP3ump4WwZ7rItgWlTYEj4DnK3zhL8nj78XevGVKQ3PjAHGHPIqvqHeP8QEgUWtW4B7cnRGZyPGF6eXOPnNGAfDpALa4us2I38klL3HI207q5ob+2Rz/9gu5wLccfDcWfyi5aTBVzWcozcyIwyhaOgZP8z1JzVj26uYhqZwPOryQ6KmvUa//K9+6RyEyttVo51/EtejO1zX/KNsKgggOHMIIDgzCCAuygAwIBAgIBADANBgkqhkiG9w0BAQUFADCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wHhcNMDQwMjEzMTAxMzE1WhcNMzUwMjEzMTAxMzE1WjCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMFHTt38RMxLXJyO2SmS+Ndl72T7oKJ4u4uw+6awntALWh03PewmIJuzbALScsTS4sZoS1fKciBGoh11gIfHzylvkdNe/hJl66/RGqrj5rFb08sAABNTzDTiqqNpJeBsYs/c2aiGozptX2RlnBktH+SUNpAajW724Nv2Wvhif6sFAgMBAAGjge4wgeswHQYDVR0OBBYEFJaffLvGbxe9WT9S1wob7BDWZJRrMIG7BgNVHSMEgbMwgbCAFJaffLvGbxe9WT9S1wob7BDWZJRroYGUpIGRMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbYIBADAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4GBAIFfOlaagFrl71+jq6OKidbWFSE+Q4FqROvdgIONth+8kSK//Y/4ihuE4Ymvzn5ceE3S/iBSQQMjyvb+s2TWbQYDwcp129OPIbD9epdr4tJOUNiSojw7BHwYRiPh58S1xGlFgHFXwrEBb3dgNbMUa+u4qectsMAXpVHnD9wIyfmHMYIBmjCCAZYCAQEwgZQwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tAgEAMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0wNzExMjMxNzU2NDdaMCMGCSqGSIb3DQEJBDEWBBT0m+M8uOJiEOLXaidOMaMQ39p/HzANBgkqhkiG9w0BAQEFAASBgJeXD93po4s9fSwc9U10wtURG34U1WcaiTJFVUPGUTwY80/IgBDyC7swVhXdGMq6sNLaQwb9f0DLvVHyYIMVHSEN90imprm9A7TzohMWKE695ypas6sQI1NOGxxC/lGwJnfib+k7II053TIDAM2ezZtnqpaF/ub0F+7aXcuusPp5-----END PKCS7-----" />
+                </form>
+                </li>
+
+                <!-- Link Us -->
+                <li><h2>Link us</h2></li>
+                <li><a href="http://www.clansuite.com/banner/" target="_blank"><img src="http://www.clansuite.com/website/images/banners/clansuite-crown-banner-80x31.png" alt="Clansuite 80x31 LOGO" /></a></li>
+
+           </ul>
     </div>
-<hr />
-<!-- Fusszeile -->
+    <hr />
+    <!-- Fusszeile -->
     <div id="footer">
         <p style="filter:alpha(opacity=65); -moz-opacity:0.65;">
-       	    <br />
-       	    Installer Version :  0.3dev - SVN: $Rev$ $Author$
-       	     <br />
-            &copy; 2005-<?=date("Y"); ?> by Jens-Andr&#x00E9; Koch &amp; Clansuite Development Team
+            <br />
+            <?php $webinstaller_version = 'Version : 0.3 - '. date("l, jS F Y",filemtime($_SERVER['SCRIPT_FILENAME'])); ?>
+            Clansuite Installation <?php echo $webinstaller_version; ?>
+            <br />
+            SVN: $Rev$ $Author$
+            <br />
+            &copy; 2005-<?=date("Y"); ?> by <a href="http://www.jens-andre-koch.de" target="_blank" style="text-decoration=none">Jens-Andr&#x00E9; Koch</a> &amp; Clansuite Development Team
         </p>
     </div><!-- Fusszeile ENDE -->
 </div><!-- PAGE ENDE -->
--- installation/install_header.php
+++ installation/install_header.php
@@ -7,22 +7,22 @@ if (!defined('IN_CS')){ die( 'Clansuite not loaded. Direct Access forbidden.' );
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-   	<meta name="robots" content="index,follow" />
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<meta http-equiv="content-language" content="en,de" />
-	<meta http-equiv="reply-to" content="system@clansuite.com" />
-	<link rel="shortcut icon" href="images/Clansuite-Favicon-16.ico" />
+    <meta name="robots" content="index,follow" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta http-equiv="content-language" content="en,de" />
+    <meta http-equiv="reply-to" content="system@clansuite.com" />
+    <link rel="shortcut icon" href="images/Clansuite-Favicon-16.ico" />
     <link rel="stylesheet" type="text/css" href="installation.css" />
-	<title>Clansuite.com - just an eSport CMS :: Installation</title>
+    <title>Clansuite :: Installation</title>
 </head>
 <body>
 <div id="page">
     <div id="header">
         <div id="headerimg">
           <div style="font-size:20px; margin-left: 190px; padding-top: 100px;" class="description">Installation</div>
-    	   <span>
-    	       <img style="margin: -86px 38px 0pt; position:relative;" src="images/clansuite-joker.gif"  alt="Clansuite Joker Logo" />
-    	   </span>
-    	</div>
+           <span>
+               <img style="margin: -86px 38px 0pt; position:relative;" src="images/clansuite-joker.gif"  alt="Clansuite Joker Logo" />
+           </span>
+        </div>
     </div>
 <hr />
