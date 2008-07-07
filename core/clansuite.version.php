bcc76ee679eb33e2885d532fbb230c85b84dc562
Jens-André Koch <jakoch@web.de>
2008-07-07 14:47:04 +0000

    - version info update

--- core/clansuite.version.php
+++ core/clansuite.version.php
@@ -1,5 +1,11 @@
 <?php
+
+if (!defined('IN_CS')){ die('Clansuite Framework not loaded. Direct Access forbidden.' );}
+
+# This file contains version info only and is automatically updated. DO NOT EDIT.
+
 $clansuite_version       = '0.2';
 $clansuite_version_state = 'alpha-dev';
 $clansuite_version_name  = 'Trajan';
+$clansuite_revision      = '2100+';
 ?>
