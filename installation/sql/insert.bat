0e742ce18b42067a73566335abbcfbdb953c8aa8
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2008-07-09 12:56:44 +0000

    - insert.bat places backups now in directory /backup
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@2207 34db09b4-2314-0410-a940-aec02a7c051d

--- installation/sql/insert.bat
+++ installation/sql/insert.bat
@@ -8,8 +8,8 @@
 @ECHO OFF
 
 @REM Verzeichnisse 
-SET basedir=C:\xampplite\htdocs\work\clansuite\trunk\installation
-SET mysqldir=C:\xampplite\mysql\bin
+SET basedir=D:\xampplite\htdocs\work\clansuite\trunk\installation
+SET mysqldir=D:\xampplite\mysql\bin
 SET mysqluser=clansuite
 SET mysqlpassword=toop
 SET dbname=clansuite
@@ -21,7 +21,7 @@ FOR /F "tokens=1,2 delims=/: " %%a in ('time/T') do set CTIME=%%a-%%b
 @REM Clansuite DB insert
 
 @ECHO Creating backup of %dbname% as \sql\%dbname%_%CDATE%_%CTIME%_backup.sql ...
-%mysqldir%/mysqldump -u %mysqluser% -p%mysqlpassword% --skip-add-locks --add-drop-table %dbname% >  %basedir%\sql\%dbname%_%CDATE%_%CTIME%_backup.sql
+%mysqldir%/mysqldump -u %mysqluser% -p%mysqlpassword% --skip-add-locks --add-drop-table %dbname% >  %basedir%\sql\backup\%dbname%_%CDATE%_%CTIME%_backup.sql
 @ECHO Beginning insert of \sql\%dbname%.sql ...
 %mysqldir%/mysql -u %mysqluser% -p%mysqlpassword% %dbname% < %basedir%\sql\%dbname%.sql
 
