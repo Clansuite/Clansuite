299cb2d9c589054886a1b253fd7636c4db9b10e9
Jens-André Koch <jakoch@web.de>
2008-07-10 15:32:34 +0000

    - doctrine compile script

--- build-tools/doctrine/doctrine_compile.php
+++ build-tools/doctrine/doctrine_compile.php
@@ -1,11 +1,20 @@
 <?php
 /**
  * Compile phpDoctrine
+ * -------------------
  * 
  * This assembles all library files of phpDoctrine
  * to only ONE big library file, which can be included by:
  *
- * require_once 'path_to_doctrine/Doctrine.compiled.php';
+ * Usage : require_once 'path_to_doctrine/Doctrine.compiled.php';
+ *
+ * Whats going on?
+ * ---------------
+ *
+ *  Xdebug Output:
+ *  
+ *  Doctrine::compile( )	        ..\doctrine_compile.php:7
+ *  Doctrine_Compiler::compile( )	..\Doctrine.php:992
  */
 require 'libraries/doctrine/Doctrine.php';
 
@@ -13,15 +22,4 @@ spl_autoload_register(array('Doctrine', 'autoload'));
 #Doctrine_Manager::connection('mysql://clansuite:toop@localhost/clansuite');
 
 Doctrine::compile();
-
-/**
-Xdebug
-
-Doctrine::compile( )	        ..\doctrine_compile.php:7
-Doctrine_Compiler::compile( )	..\Doctrine.php:992
-array_merge ( )
-...
-*/
-
-
 ?>
