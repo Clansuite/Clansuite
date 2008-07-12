1eeaa369c3ee32e940c7e28f012a932a28362951
xsign <xsign@34db09b4-2314-0410-a940-aec02a7c051d>
2008-07-12 17:43:03 +0000

    - samrty fixes
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@2268 34db09b4-2314-0410-a940-aec02a7c051d

--- libraries/smarty/clansuite_plugins/resource.fetch_module_templates.php
+++ libraries/smarty/clansuite_plugins/resource.fetch_module_templates.php
@@ -11,7 +11,18 @@ function smarty_fetch_module_templates($resource_type, $resource_name, &$templat
     {
         #return false;
         #var_dump($template);
-        $smarty_obj->_current_tpl = $template;
+        #$smarty_obj->_current_tpl = $template;
+        #$smarty_obj->assign('_current_tpl', str_replace( ROOT, '', $template));
+        $smarty_obj->_current_tpl = str_replace(ROOT, '', $template);
+        $smarty_obj->_current_tpl = str_replace( '\\', '/',  $smarty_obj->_current_tpl );
+        # single slash correction
+        $smarty_obj->_current_tpl = str_replace("\\", "/",  $smarty_obj->_current_tpl);
+        # get rid of double slashes
+        $smarty_obj->_current_tpl = str_replace("//", "/",  $smarty_obj->_current_tpl);
+        $smarty_obj->_current_tpl = str_replace("\\\\", "\\",  $smarty_obj->_current_tpl);
+
+        $smarty_obj->assign('_current_tpl',  str_replace(ROOT, '', $smarty_obj->_current_tpl));
+        $smarty_obj->assign('_current_path', str_replace( '\\', '/', str_replace(ROOT, '', preg_replace('#^(.*)/([^/]+)$#',"\\1", $smarty_obj->_current_tpl ))));
         $template_source    = file_get_contents($template);
         $template_timestamp = filemtime($template);
         return true;
