3e4a0f8e3cda4908b72b56f107b90d9a2ed3b973
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2008-07-12 15:49:49 +0000

    - modules menu (former admin/adminmenu)
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@2260 34db09b4-2314-0410-a940-aec02a7c051d

---
+++ modules/menu/menu.module.php
@@ -0,0 +1,66 @@
+<?php
+   /**
+    * Clansuite - just an eSports CMS
+    * Jens-Andre Koch © 2005 - onwards
+    * http://www.clansuite.com/
+    *
+    * LICENSE:
+    *
+    *    This program is free software; you can redistribute it and/or modify
+    *    it under the terms of the GNU General Public License as published by
+    *    the Free Software Foundation; either version 2 of the License, or
+    *    (at your option) any later version.
+    *
+    *    This program is distributed in the hope that it will be useful,
+    *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    *    GNU General Public License for more details.
+    *
+    *    You should have received a copy of the GNU General Public License
+    *    along with this program; if not, write to the Free Software
+    *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    *
+    * @license    GNU/GPL, see COPYING.txt
+    *
+    * @author     Jens-Andre Koch <vain@clansuite.com>
+    * @copyright  Copyleft: All rights reserved. Jens-Andre Koch (2005-onwards)
+    *
+    * @link       http://www.clansuite.com
+    * @link       http://gna.org/projects/clansuite
+    * @since      File available since Release 0.2
+    *
+    * @version    SVN: $Id: news.module.php 2248 2008-07-12 01:48:54Z vain $
+    */
+
+// Security Handler
+if (!defined('IN_CS')){ die('Clansuite not loaded. Direct Access forbidden.' );}
+
+/**
+ * Clansuite
+ *
+ * Module:      Menu
+ *
+ */
+class Module_Menu extends ModuleController implements Clansuite_Module_Interface
+{
+    /**
+     * Module_Menu -> Execute
+     */
+    public function execute(httprequest $request, httpresponse $response)
+    {
+        # proceed to the requested action
+        $this->processActionController($request);
+    }
+
+    /**
+     * module menu action_show()
+     *
+     */
+    public function action_show()
+    {
+        // Set Pagetitle and Breadcrumbs
+        trail::addStep( _('Show'), '/index.php?mod=menu&amp;action=show');
+        
+        $this->prepareOutput();
+    }
+}
