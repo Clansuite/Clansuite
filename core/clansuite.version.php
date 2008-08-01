418b53420e653a6f37e429004209fab07c05c6cf
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2008-08-01 15:48:24 +0000

    - version infos now constants - constants are assigned to smarty too - clansuite_revision number fetched from svn
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@2331 34db09b4-2314-0410-a940-aec02a7c051d

--- core/clansuite.version.php
+++ core/clansuite.version.php
@@ -4,8 +4,38 @@ if (!defined('IN_CS')){ die('Clansuite Framework not loaded. Direct Access forbi
 
 # This file contains version info only and is automatically updated. DO NOT EDIT.
 
-$clansuite_version       = '0.2';
-$clansuite_version_state = 'alpha-dev';
-$clansuite_version_name  = 'Trajan';
-$clansuite_revision      = '2100+';
+define('CLANSUITE_VERSION',         '0.2');
+define('CLANSUITE_VERSION_NAME',    'Trajan');
+define('CLANSUITE_VERSION_STATE',   'alpha-dev');
+
+/**
+ * Detect SVN Revision Number from
+ *
+ * Author: Andy Dawson (AD7six) for cakephp.org
+ * URL: http://bakery.cakephp.org/articles/view/using-your-application-svn-revision-number
+ */
+if (!defined('CLANSUITE_REVISION'))
+{
+    # File used: "root/.svn/entries"
+    if (file_exists(ROOT . '.svn' . DS . 'entries'))
+    {
+        $svn = file(ROOT . '.svn' . DS . 'entries');
+        if (is_numeric(trim($svn[3])))
+        {
+            $version = $svn[3];
+        }
+        else # pre 1.4 svn used xml for this file
+        {
+            $version = explode('"', $svn[4]);
+            $version = $version[1];
+        }
+        define ('CLANSUITE_REVISION', trim($version));
+        unset ($svn);
+        unset ($version);
+    }
+    else # default if no svn data avilable
+    {
+        define ('CLANSUITE_REVISION', 0);
+    }
+}
 ?>
