6b25e6fcb3c52dd556bc7cec10df52bec1891d60
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2008-08-01 15:54:44 +0000

    - tabs to ws
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@2333 34db09b4-2314-0410-a940-aec02a7c051d

--- libraries/smarty/clansuite_plugins/block.t.php
+++ libraries/smarty/clansuite_plugins/block.t.php
@@ -20,39 +20,39 @@
  *
  * Installation: simply copy this file to the smarty plugins directory.
  *
- * @package	smarty-gettext
- * @version	$Id$
- * @link	http://smarty-gettext.sourceforge.net/
- * @author	Sagi Bashari <sagi@boom.org.il>
+ * @package smarty-gettext
+ * @version $Id$
+ * @link    http://smarty-gettext.sourceforge.net/
+ * @author  Sagi Bashari <sagi@boom.org.il>
  * @copyright 2004-2005 Sagi Bashari
  */
- 
+
 /**
  * Replaces arguments in a string with their values.
  * Arguments are represented by % followed by their number.
  *
- * @param	string	Source string
- * @param	mixed	Arguments, can be passed in an array or through single variables.
- * @returns	string	Modified string
+ * @param   string  Source string
+ * @param   mixed   Arguments, can be passed in an array or through single variables.
+ * @returns string  Modified string
  */
 function smarty_gettext_strarg($str)
 {
-	$tr = array();
-	$p = 0;
+    $tr = array();
+    $p = 0;
 
-	for ($i=1; $i < func_num_args(); $i++) {
-		$arg = func_get_arg($i);
-		
-		if (is_array($arg)) {
-			foreach ($arg as $aarg) {
-				$tr['%'.++$p] = $aarg;
-			}
-		} else {
-			$tr['%'.++$p] = $arg;
-		}
-	}
-	
-	return strtr($str, $tr);
+    for ($i=1; $i < func_num_args(); $i++) {
+        $arg = func_get_arg($i);
+
+        if (is_array($arg)) {
+            foreach ($arg as $aarg) {
+                $tr['%'.++$p] = $aarg;
+            }
+        } else {
+            $tr['%'.++$p] = $arg;
+        }
+    }
+
+    return strtr($str, $tr);
 }
 
 /**
@@ -60,7 +60,7 @@ function smarty_gettext_strarg($str)
  *
  * The block content is the text that should be translated.
  *
- * Any parameter that is sent to the function will be represented as %n in the translation text, 
+ * Any parameter that is sent to the function will be represented as %n in the translation text,
  * where n is 1 for the first parameter. The following parameters are reserved:
  *   - escape - sets escape mode:
  *       - 'html' for HTML escaping, this is the default.
@@ -74,55 +74,54 @@ function smarty_gettext_strarg($str)
  */
 function smarty_block_t($params, $text, &$smarty)
 {
-	$text = stripslashes($text);
-	
-	// set escape mode
-	if (isset($params['escape'])) {
-		$escape = $params['escape'];
-		unset($params['escape']);
-	}
-	
-	// set plural version
-	if (isset($params['plural'])) {
-		$plural = $params['plural'];
-		unset($params['plural']);
-		
-		// set count
-		if (isset($params['count'])) {
-			$count = $params['count'];
-			unset($params['count']);
-		}
-	}
-	
-	// use plural if required parameters are set
-	if (isset($count) && isset($plural)) {
-		$text = T_ngettext($text, $plural, $count); # vain: prefixed "T_" for usage of php-gettext
-	} else { // use normal
-		$text = T_gettext($text);                   # vain: prefixed "T_" for usage of php-gettext
-	}
+    $text = stripslashes($text);
 
-	// run strarg if there are parameters
-	if (count($params)) {
-		$text = smarty_gettext_strarg($text, $params);
-	}
+    // set escape mode
+    if (isset($params['escape'])) {
+        $escape = $params['escape'];
+        unset($params['escape']);
+    }
 
-	if (!isset($escape) || $escape == 'html') { // html escape, default
-	   $text = nl2br(htmlspecialchars($text));
+    // set plural version
+    if (isset($params['plural'])) {
+        $plural = $params['plural'];
+        unset($params['plural']);
+
+        // set count
+        if (isset($params['count'])) {
+            $count = $params['count'];
+            unset($params['count']);
+        }
+    }
+
+    // use plural if required parameters are set
+    if (isset($count) && isset($plural)) {
+        $text = T_ngettext($text, $plural, $count); # vain: prefixed "T_" for usage of php-gettext
+    } else { // use normal
+        $text = T_gettext($text);                   # vain: prefixed "T_" for usage of php-gettext
+    }
+
+    // run strarg if there are parameters
+    if (count($params)) {
+        $text = smarty_gettext_strarg($text, $params);
+    }
+
+    if (!isset($escape) || $escape == 'html') { // html escape, default
+       $text = nl2br(htmlspecialchars($text));
    } elseif (isset($escape)) {
-		switch ($escape) {
-			case 'javascript':
-			case 'js':
-				// javascript escape
-				$text = str_replace('\'', '\\\'', stripslashes($text));
-				break;
-			case 'url':
-				// url escape
-				$text = urlencode($text);
-				break;
-		}
-	}
-	
-	return $text;
-}
+        switch ($escape) {
+            case 'javascript':
+            case 'js':
+                // javascript escape
+                $text = str_replace('\'', '\\\'', stripslashes($text));
+                break;
+            case 'url':
+                // url escape
+                $text = urlencode($text);
+                break;
+        }
+    }
 
+    return $text;
+}
 ?>
