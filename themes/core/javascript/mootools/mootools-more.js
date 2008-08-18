01c43cd65d79d87d7df0e598250530604857f895
xsign <xsign@34db09b4-2314-0410-a940-aec02a7c051d>
2008-08-18 05:42:55 +0000

    - Block Dropables fixed!!! Sorry to Spam the SVN, but this feature is SOOO DAMN AMAZING !!!! - fixed widths of Xul Menu
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@2446 34db09b4-2314-0410-a940-aec02a7c051d

--- themes/core/javascript/mootools/mootools-more.js
+++ themes/core/javascript/mootools/mootools-more.js
@@ -25,34 +25,225 @@ for(var G in D){var A=F[G]-E[G];if($chk(D[G])){D[G]=($type(D[G])=="number")?D[G]
 this.parent(A);},compute:function(G,H,I){var C={};for(var D in G){var A=G[D],E=H[D],F=C[D]={};for(var B in A){F[B]=this.parent(A[B],E[B],I);}}return C;
 },set:function(B){for(var C in B){var A=B[C];for(var D in A){this.render(this.elements[C],D,A[D],this.options.unit);}}return this;},start:function(C){if(!this.check(arguments.callee,C)){return this;
 }var H={},I={};for(var D in C){var F=C[D],A=H[D]={},G=I[D]={};for(var B in F){var E=this.prepare(this.elements[D],B,F[B]);A[B]=E.from;G[B]=E.to;}}return this.parent(H,I);
-}});var Drag=new Class({Implements:[Events,Options],options:{snap:6,unit:"px",grid:false,style:true,limit:false,handle:false,invert:false,preventDefault:false,modifiers:{x:"left",y:"top"}},initialize:function(){var B=Array.link(arguments,{options:Object.type,element:$defined});
-this.element=$(B.element);this.document=this.element.getDocument();this.setOptions(B.options||{});var A=$type(this.options.handle);this.handles=(A=="array"||A=="collection")?$$(this.options.handle):$(this.options.handle)||this.element;
-this.mouse={now:{},pos:{}};this.value={start:{},now:{}};this.selection=(Browser.Engine.trident)?"selectstart":"mousedown";this.bound={start:this.start.bind(this),check:this.check.bind(this),drag:this.drag.bind(this),stop:this.stop.bind(this),cancel:this.cancel.bind(this),eventStop:$lambda(false)};
-this.attach();},attach:function(){this.handles.addEvent("mousedown",this.bound.start);return this;},detach:function(){this.handles.removeEvent("mousedown",this.bound.start);
-return this;},start:function(C){if(this.options.preventDefault){C.preventDefault();}this.fireEvent("beforeStart",this.element);this.mouse.start=C.page;
-var A=this.options.limit;this.limit={x:[],y:[]};for(var D in this.options.modifiers){if(!this.options.modifiers[D]){continue;}if(this.options.style){this.value.now[D]=this.element.getStyle(this.options.modifiers[D]).toInt();
-}else{this.value.now[D]=this.element[this.options.modifiers[D]];}if(this.options.invert){this.value.now[D]*=-1;}this.mouse.pos[D]=C.page[D]-this.value.now[D];
-if(A&&A[D]){for(var B=2;B--;B){if($chk(A[D][B])){this.limit[D][B]=$lambda(A[D][B])();}}}}if($type(this.options.grid)=="number"){this.options.grid={x:this.options.grid,y:this.options.grid};
-}this.document.addEvents({mousemove:this.bound.check,mouseup:this.bound.cancel});this.document.addEvent(this.selection,this.bound.eventStop);},check:function(A){if(this.options.preventDefault){A.preventDefault();
-}var B=Math.round(Math.sqrt(Math.pow(A.page.x-this.mouse.start.x,2)+Math.pow(A.page.y-this.mouse.start.y,2)));if(B>this.options.snap){this.cancel();this.document.addEvents({mousemove:this.bound.drag,mouseup:this.bound.stop});
-this.fireEvent("start",this.element).fireEvent("snap",this.element);}},drag:function(A){if(this.options.preventDefault){A.preventDefault();}this.mouse.now=A.page;
-for(var B in this.options.modifiers){if(!this.options.modifiers[B]){continue;}this.value.now[B]=this.mouse.now[B]-this.mouse.pos[B];if(this.options.invert){this.value.now[B]*=-1;
-}if(this.options.limit&&this.limit[B]){if($chk(this.limit[B][1])&&(this.value.now[B]>this.limit[B][1])){this.value.now[B]=this.limit[B][1];}else{if($chk(this.limit[B][0])&&(this.value.now[B]<this.limit[B][0])){this.value.now[B]=this.limit[B][0];
-}}}if(this.options.grid[B]){this.value.now[B]-=(this.value.now[B]%this.options.grid[B]);}if(this.options.style){this.element.setStyle(this.options.modifiers[B],this.value.now[B]+this.options.unit);
-}else{this.element[this.options.modifiers[B]]=this.value.now[B];}}this.fireEvent("drag",this.element);},cancel:function(A){this.document.removeEvent("mousemove",this.bound.check);
-this.document.removeEvent("mouseup",this.bound.cancel);if(A){this.document.removeEvent(this.selection,this.bound.eventStop);this.fireEvent("cancel",this.element);
-}},stop:function(A){this.document.removeEvent(this.selection,this.bound.eventStop);this.document.removeEvent("mousemove",this.bound.drag);this.document.removeEvent("mouseup",this.bound.stop);
-if(A){this.fireEvent("complete",this.element);}}});Element.implement({makeResizable:function(A){return new Drag(this,$merge({modifiers:{x:"width",y:"height"}},A));
-}});Drag.Move=new Class({Extends:Drag,options:{droppables:[],container:false},initialize:function(C,B){this.parent(C,B);this.droppables=$$(this.options.droppables);
-this.container=$(this.options.container);if(this.container&&$type(this.container)!="element"){this.container=$(this.container.getDocument().body);}C=this.element;
-var D=C.getStyle("position");var A=(D!="static")?D:"absolute";if(C.getStyle("left")=="auto"||C.getStyle("top")=="auto"){C.position(C.getPosition(C.offsetParent));
-}C.setStyle("position",A);this.addEvent("start",function(){this.checkDroppables();},true);},start:function(B){if(this.container){var D=this.element,J=this.container,E=J.getCoordinates(D.offsetParent),F={},A={};
-["top","right","bottom","left"].each(function(K){F[K]=J.getStyle("padding-"+K).toInt();A[K]=D.getStyle("margin-"+K).toInt();},this);var C=D.offsetWidth+A.left+A.right,I=D.offsetHeight+A.top+A.bottom;
-var H=[E.left+F.left,E.right-F.right-C];var G=[E.top+F.top,E.bottom-F.bottom-I];this.options.limit={x:H,y:G};}this.parent(B);},checkAgainst:function(B){B=B.getCoordinates();
-var A=this.mouse.now;return(A.x>B.left&&A.x<B.right&&A.y<B.bottom&&A.y>B.top);},checkDroppables:function(){var A=this.droppables.filter(this.checkAgainst,this).getLast();
-if(this.overed!=A){if(this.overed){this.fireEvent("leave",[this.element,this.overed]);}if(A){this.overed=A;this.fireEvent("enter",[this.element,A]);}else{this.overed=null;
-}}},drag:function(A){this.parent(A);if(this.droppables.length){this.checkDroppables();}},stop:function(A){this.checkDroppables();this.fireEvent("drop",[this.element,this.overed]);
-this.overed=null;return this.parent(A);}});Element.implement({makeDraggable:function(A){return new Drag.Move(this,A);}});Hash.Cookie=new Class({Extends:Cookie,options:{autoSave:true},initialize:function(B,A){this.parent(B,A);
+}});var Drag = new Class({
+ 
+  Implements: [Events, Options],
+ 
+  options: {/*
+    onBeforeStart: $empty,
+    onStart: $empty,
+    onDrag: $empty,
+    onCancel: $empty,
+    onComplete: $empty,*/
+    snap: 6,
+    unit: 'px',
+    grid: false,
+    style: true,
+    limit: false,
+    handle: false,
+    invert: false,
+    preventDefault: false,
+    modifiers: {x: 'left', y: 'top'}
+  },
+ 
+  initialize: function(){
+    var params = Array.link(arguments, {'options': Object.type, 'element': $defined});
+    this.element = $(params.element);
+    this.document = this.element.getDocument();
+    this.setOptions(params.options || {});
+    var htype = $type(this.options.handle);
+    this.handles = (htype == 'array' || htype == 'collection') ? $$(this.options.handle) : $(this.options.handle) || this.element;
+    this.mouse = {'now': {}, 'pos': {}};
+    this.value = {'start': {}, 'now': {}};
+    
+    this.selection = (Browser.Engine.trident) ? 'selectstart' : 'mousedown';
+    
+    this.bound = {
+      start: this.start.bind(this),
+      check: this.check.bind(this),
+      drag: this.drag.bind(this),
+      stop: this.stop.bind(this),
+      cancel: this.cancel.bind(this),
+      eventStop: $lambda(false)
+    };
+    this.attach();
+  },
+ 
+  attach: function(){
+    this.handles.addEvent('mousedown', this.bound.start);
+    return this;
+  },
+ 
+  detach: function(){
+    this.handles.removeEvent('mousedown', this.bound.start);
+    return this;
+  },
+ 
+  start: function(event){
+    if (this.options.preventDefault) event.preventDefault();
+    this.fireEvent('beforeStart', this.element);
+    this.mouse.start = event.page;
+    var limit = this.options.limit;
+    this.limit = {'x': [], 'y': []};
+    for (var z in this.options.modifiers){
+      if (!this.options.modifiers[z]) continue;
+      if (this.options.style) this.value.now[z] = this.element.getStyle(this.options.modifiers[z]).toInt();
+      else this.value.now[z] = this.element[this.options.modifiers[z]];
+      if (this.options.invert) this.value.now[z] *= -1;
+      this.mouse.pos[z] = event.page[z] - this.value.now[z];
+      if (limit && limit[z]){
+        for (var i = 2; i--; i){
+          if ($chk(limit[z][i])) this.limit[z][i] = $lambda(limit[z][i])();
+        }
+      }
+    }
+    if ($type(this.options.grid) == 'number') this.options.grid = {'x': this.options.grid, 'y': this.options.grid};
+    this.document.addEvents({mousemove: this.bound.check, mouseup: this.bound.cancel});
+    this.document.addEvent(this.selection, this.bound.eventStop);
+  },
+ 
+  check: function(event){
+    if (this.options.preventDefault) event.preventDefault();
+    var distance = Math.round(Math.sqrt(Math.pow(event.page.x - this.mouse.start.x, 2) + Math.pow(event.page.y - this.mouse.start.y, 2)));
+    if (distance > this.options.snap){
+      this.cancel();
+      this.document.addEvents({
+        mousemove: this.bound.drag,
+        mouseup: this.bound.stop
+      });
+      this.fireEvent('start', this.element).fireEvent('snap', this.element);
+    }
+  },
+ 
+  drag: function(event){
+    if (this.options.preventDefault) event.preventDefault();
+    this.mouse.now = event.page;
+    for (var z in this.options.modifiers){
+      if (!this.options.modifiers[z]) continue;
+      this.value.now[z] = this.mouse.now[z] - this.mouse.pos[z];
+      if (this.options.invert) this.value.now[z] *= -1;
+      if (this.options.limit && this.limit[z]){
+        if ($chk(this.limit[z][1]) && (this.value.now[z] > this.limit[z][1])){
+          this.value.now[z] = this.limit[z][1];
+        } else if ($chk(this.limit[z][0]) && (this.value.now[z] < this.limit[z][0])){
+          this.value.now[z] = this.limit[z][0];
+        }
+      }
+      if (this.options.grid[z]) this.value.now[z] -= (this.value.now[z] % this.options.grid[z]);
+      if (this.options.style) this.element.setStyle(this.options.modifiers[z], this.value.now[z] + this.options.unit);
+      else this.element[this.options.modifiers[z]] = this.value.now[z];
+    }
+    this.fireEvent('drag', this.element);
+  },
+ 
+  cancel: function(event){
+    this.document.removeEvent('mousemove', this.bound.check);
+    this.document.removeEvent('mouseup', this.bound.cancel);
+    if (event){
+      this.document.removeEvent(this.selection, this.bound.eventStop);
+      this.fireEvent('cancel', this.element);
+    }
+  },
+ 
+  stop: function(event){
+    this.document.removeEvent(this.selection, this.bound.eventStop);
+    this.document.removeEvent('mousemove', this.bound.drag);
+    this.document.removeEvent('mouseup', this.bound.stop);
+    if (event) this.fireEvent('complete', this.element);
+  }
+ 
+});
+ 
+Element.implement({
+  
+  makeResizable: function(options){
+    return new Drag(this, $merge({modifiers: {'x': 'width', 'y': 'height'}}, options));
+  }
+ 
+});
+Drag.Move = new Class({
+ 
+  Extends: Drag,
+ 
+  options: {/*
+    onEnter: $empty,
+    onLeave: $empty,
+    onDrop: $empty,*/
+    droppables: [],
+    container: false
+  },
+ 
+  initialize: function(element, options){
+    this.parent(element, options);
+    this.droppables = $$(this.options.droppables);
+    this.container = $(this.options.container);
+    if (this.container && $type(this.container) != 'element') this.container = $(this.container.getDocument().body);
+    element = this.element;
+
+    var current = element.getStyle('position');
+    var position = (current != 'static') ? current : 'absolute';
+    //if (element.getStyle('left') == 'auto' || element.getStyle('top') == 'auto') element.position(element.getPosition(element.getOffsetParent));
+    //element.setStyle('position', position);
+    
+    this.addEvent('start', this.checkDroppables, true);
+    
+    this.overed = null;
+  },
+ 
+  start: function(event){
+    if (this.container){
+      var el = this.element, cont = this.container, ccoo = cont.getCoordinates(el.offsetParent), cps = {}, ems = {};
+ 
+      ['top', 'right', 'bottom', 'left'].each(function(pad){
+        cps[pad] = cont.getStyle('padding-' + pad).toInt();
+        ems[pad] = el.getStyle('margin-' + pad).toInt();
+      }, this);
+ 
+      var width = el.offsetWidth + ems.left + ems.right, height = el.offsetHeight + ems.top + ems.bottom;
+      var x = [ccoo.left + cps.left, ccoo.right - cps.right - width];
+      var y = [ccoo.top + cps.top, ccoo.bottom - cps.bottom - height];
+
+      this.options.limit = {x: x, y: y};
+    }
+    this.parent(event);
+  },
+ 
+  checkAgainst: function(el){
+    el = el.getCoordinates();
+    var now = this.mouse.now;
+    return (now.x > el.left && now.x < el.right && now.y < el.bottom && now.y > el.top);
+  },
+ 
+  checkDroppables: function(){
+    var overed = this.droppables.filter(this.checkAgainst, this).getLast();
+    if (this.overed != overed){
+      if (this.overed) this.fireEvent('leave', [this.element, this.overed]);
+      if (overed) this.fireEvent('enter', [this.element, overed]);
+      this.overed = overed;
+    }
+  },
+ 
+  drag: function(event){
+    this.parent(event);
+    if (this.droppables.length) this.checkDroppables();
+  },
+ 
+  stop: function(event){
+    this.checkDroppables();
+    this.fireEvent('drop', [this.element, this.overed]);
+    this.overed = null;
+    return this.parent(event);
+  }
+ 
+});
+ 
+Element.implement({
+ 
+  makeDraggable: function(options){
+    return new Drag.Move(this, options);
+  }
+ 
+});Hash.Cookie=new Class({Extends:Cookie,options:{autoSave:true},initialize:function(B,A){this.parent(B,A);
 this.load();},save:function(){var A=JSON.encode(this.hash);if(!A||A.length>4096){return false;}if(A=="{}"){this.dispose();}else{this.write(A);}return true;
 },load:function(){this.hash=new Hash(JSON.decode(this.read(),true));return this;}});Hash.Cookie.implement((function(){var A={};Hash.each(Hash.prototype,function(C,B){A[B]=function(){var D=C.apply(this.hash,arguments);
 if(this.options.autoSave){this.save();}return D;};});return A;})());var Color=new Native({initialize:function(B,C){if(arguments.length>=3){C="rgb";B=Array.slice(arguments,0,3);
