b6128f1b4ac30dce44d8cc478bc9ac0358fcb9c3
Jens-André Koch <jakoch@web.de>
2008-09-18 16:28:31 +0000

    - added php-debug library (webdebug-console), loaded via prefilter

---
+++ core/filters/php_debug_console.filter.php
@@ -0,0 +1,97 @@
+<?php
+   /**
+    * Clansuite - just an eSports CMS
+    * Jens-Andre Koch © 2005 - onwards
+    * http://www.clansuite.com/
+    *
+    * LICENSE:
+    *
+    *    This program is free software; you can redistribute it and/or modify
+    *    it under the terms of the GNU General Public License as published by
+    *    the Free Software Foundation; either version 2 of the License, or
+    *    (at your option) any later version.
+    *
+    *    This program is distributed in the hope that it will be useful,
+    *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    *    GNU General Public License for more details.
+    *
+    *    You should have received a copy of the GNU General Public License
+    *    along with this program; if not, write to the Free Software
+    *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    *
+    * @license    GNU/GPL, see COPYING.txt
+    *
+    * @author     Jens-Andre Koch <vain@clansuite.com>
+    * @copyright  Jens-Andre Koch (2005 - onwards)
+    *
+    * @link       http://www.clansuite.com
+    * @link       http://gna.org/projects/clansuite
+    * @since      File available since Release 0.2
+    *
+    * @version    SVN: $id$
+    */
+
+// Security Handler
+if (!defined('IN_CS')){ die('Clansuite not loaded. Direct Access forbidden.' );}
+
+/**
+ * Clansuite PHP_Debug Debugging Console
+ *
+ *
+ * @package clansuite
+ * @subpackage filters
+ * @implements FilterInterface
+ */
+class php_debug_console implements Filter_Interface
+{
+    private $config     = null;
+
+    function __construct(Clansuite_Config $config)
+    {
+       $this->config    = $config;
+    }
+
+    public function executeFilter(httprequest $request, httpresponse $response)
+    {
+        // take the initiative, if themeswitching is enabled in CONFIG
+        // or pass through (do nothing)
+        if($this->config['switches']['webdebug'] == 1)
+        {
+            /**
+             *  ================================================
+             * Initialize PHP_Debug Web-Debuggign Console
+             *  ================================================
+             */
+            if(DEBUG)
+            {
+                # Load Library
+                require ROOT_LIBRARIES . 'phpdebug/PHP/Debug.php';
+    
+                # Setup Options
+                $options = array('HTML_DIV_images_path' =>  '/libraries/phpdebug/images',
+                                 'HTML_DIV_css_path' => '/libraries/phpdebug/css',
+                                 'HTML_DIV_js_path' => '/libraries/phpdebug/js');
+
+                # Initialiaze Object
+                $debug = new PHP_Debug($options);
+
+                # Set Title
+                $debug->add('Clansuite DEBUG INFO');
+
+                /* Load JS / CSS */
+                ?>
+                <script type="text/javascript" src="<?php echo $options['HTML_DIV_js_path']; ?>/html_div.js"></script>
+                <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $options['HTML_DIV_css_path']; ?>/html_div.css" />
+                <?php
+
+                # unset $options
+                unset($options);
+
+                # display the console
+                $debug->display();
+            }
+        }// else => bypass
+    }
+}
+?>
