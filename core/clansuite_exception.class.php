41bbbf949991ea765aa1e019dade296201aa76d9
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2008-10-07 03:21:49 +0000

    - clansuite_exception.class.php: reworked exception display - clansuite_cache.class.php: apc stats added - sysinfo module added, added common system infos (show) and apcstats (showapc)
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@2551 34db09b4-2314-0410-a940-aec02a7c051d

--- core/clansuite_exception.class.php
+++ core/clansuite_exception.class.php
@@ -38,14 +38,16 @@ if (!defined('IN_CS')){ die('Clansuite not loaded. Direct Access forbidden.' );}
 class clansuite_exception extends exception
 {
     // redeclare exception, so that it is not optional
-    public function __construct($ErrorObject, $errormessage, $errorcode = 0)
+    public function __construct($ErrorObject, $error_head, $errorcode = 3)
     {
         // assign to parent
-        parent::__construct($errormessage, $errorcode); 
-        
-        $this->ysod($ErrorObject, $errormessage, $errorcode);
+        parent::__construct($ErrorObject, $errorcode);
+
+        $this->ysod($ErrorObject, $error_head, $errorcode);
         #clansuite_xdebug::printR($ErrorObject);
-        exit;
+
+        # Goodbye.
+        exit(1);
     }
 
     /**
@@ -56,52 +58,68 @@ class clansuite_exception extends exception
      * @param string $string contains errorstring
      * @param integer $error_level contains errorlvl
      */
-    public function ysod( $ErrorObject, $error_head = 'Unknown Error', $string = '', $error_level = 3 )
+    public function ysod( $ErrorObject, $error_head, $error_level = 3 )
     {
         # Header
         $errormessage    = '<html><head>';
-        $errormessage   .= '<title>Clansuite Error : '. $error_head .' | Errorcode: '. $error_level .'</title>';
+        $errormessage   .= '<title>Yellow Screen of Death : '. $error_head .' | Errorcode: '. $error_level .'</title>';
         $errormessage   .= '<body>';
         $errormessage   .= '<link rel="stylesheet" href="'. WWW_ROOT_THEMES_CORE .'/css/error.css" type="text/css" />';
         $errormessage   .= '</head>';
+
         # Body
         $errormessage   .= '<body>';
+
         # Fieldset with colours (error_red, error_orange, error_beige)
         if ($error_level == 1)     { echo '<fieldset class="error_red">'; }
         elseif ($error_level == 2) { echo '<fieldset class="error_orange">'; }
         elseif ($error_level == 3) { echo '<fieldset class="error_beige">'; }
+
         # Errorlogo
         $errormessage   .= '<div style="float: left; margin: 5px; margin-right: 25px; border:1px inset #bf0000; padding: 20px;">';
         $errormessage   .= '<img src="'. WWW_ROOT_THEMES_CORE .'/images/Clansuite-Toolbar-Icon-64-error.png" style="border: 2px groove #000000;"/></div>';
+
         # Fieldset Legend
-        $errormessage   .= '<legend>Clansuite Error : '. $error_head .'</legend>';
+        $errormessage   .= '<legend>Clansuite Exception : '. $error_head .'</legend>';
+
         # Error String (passed Error Description)
-        #$errormessage   .= '<p><strong>'.$ErrorObject->message.'</strong>';
+        $errormessage   .= '<p><strong>'.$ErrorObject->message.'</strong>';
+
         # Error Messages from the ErrorObject
         $errormessage   .= '<hr style="width=80%">';
+
+        # Error Object Table
         $errormessage   .= '<table>';
-        $errormessage  .= '<tr><td><h3>Error</h3></td></tr>';
+        $errormessage  .= '<tr><td><h3>Exception Object</h3></td></tr>';
         $errormessage   .= '<tr><td><strong>Errorcode :</strong></td><td>'.$ErrorObject->getCode().'</td></tr>';
         $errormessage   .= '<tr><td><strong>Message :</strong></td><td>'.$ErrorObject->getMessage().'</td></tr>';
         $errormessage   .= '<tr><td><strong>Pfad :</strong></td><td>'. dirname($ErrorObject->getFile()).'</td></tr>';
         $errormessage   .= '<tr><td><strong>Datei :</strong></td><td>'. basename($ErrorObject->getFile()).'</td></tr>';
         $errormessage   .= '<tr><td><strong>Zeile :</strong></td><td>'.$ErrorObject->getLine().'</td></tr>';
+
+        # Environmental Informations at Errortime
+        if ( defined('DEBUG') && DEBUG == 1 )
+        {
         # HR Split
         $errormessage   .= '<tr><td colspan="2"><hr style="width=80%"></td></tr>';
-        # Environmental Informations at Errortime
-        $errormessage  .= '<tr><td><h3>Server Environment</h3></td></tr>';
+
+        $errormessage  .= '<tr><td colspan="2"><h3>Server Environment</h3></td></tr>';
         $errormessage   .= '<tr><td><strong>Date :</strong></td><td>'.date('r').'</td></tr>';
-        $errormessage   .= '<tr><td><strong>Request :</strong></td><td>'.$_SERVER['QUERY_STRING'].'</td></tr>';
+        $errormessage   .= '<tr><td><strong>Request :</strong></td><td>'.htmlentities($_SERVER['QUERY_STRING']).'</td></tr>';
         $errormessage   .= '<tr><td><strong>Server :</strong></td><td>'.$_SERVER['SERVER_SOFTWARE'].'</td></tr>';
         $errormessage   .= '<tr><td><strong>Remote :</strong></td><td>'.$_SERVER['REMOTE_ADDR'].'</td></tr>';
         $errormessage   .= '<tr><td><strong>Agent :</strong></td><td>'.$_SERVER['HTTP_USER_AGENT'].'</td></tr>';
+
         # HR Split
         $errormessage   .= '<tr><td colspan="2"><hr style="width=80%"></td></tr>';
+        }
+
         # Tracing
         #if ( defined('DEBUG') && DEBUG == 1 )
         #{
         #    $errormessage   .= '<tr><td>' . $this->getDebugBacktrace() . '</td></tr>';
         #}
+
         # close all html elements: table, fieldset, body+page
         $errormessage   .= '</table>';
         $errormessage   .= '</fieldset>';
---
+++ modules/sysinfo/sysinfo.config.php
@@ -0,0 +1,7 @@
+; <?php die( 'Access forbidden.' ); /* DO NOT MODIFY THIS LINE! ?>
+; 
+; sysinfo configuration file
+;
+[sysinfo]
+
+; DO NOT REMOVE THIS LINE */ ?>
