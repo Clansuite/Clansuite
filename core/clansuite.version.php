65835bf4e3da7ca84b6f17f6a532f2a566a67719
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2008-10-11 07:44:38 +0000

    - i've drawn the function for the svn revision retrival from files into the if-condition Ticket: 68
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@2553 34db09b4-2314-0410-a940-aec02a7c051d

--- core/clansuite.version.php
+++ core/clansuite.version.php
@@ -41,35 +41,39 @@ define('CLANSUITE_VERSION',         '0.2');
 define('CLANSUITE_VERSION_NAME',    'Trajan');
 define('CLANSUITE_VERSION_STATE',   'alpha-dev');
 
-/**
- * Detect SVN Revision Number from
- *
- * Author: Andy Dawson (AD7six) for cakephp.org
- * URL: http://bakery.cakephp.org/articles/view/using-your-application-svn-revision-number
- */
+# Define Clansuite SVN Revision
 if (!defined('CLANSUITE_REVISION'))
 {
     # File used: "root/.svn/entries"
     if (file_exists(ROOT . '.svn' . DS . 'entries'))
     {
-        $svn = file(ROOT . '.svn' . DS . 'entries');
-        if (is_numeric(trim($svn[3])))
+        /**
+         * Detect SVN Revision Number from File
+         *
+         * Author: Andy Dawson (AD7six) for cakephp.org
+         * URL: http://bakery.cakephp.org/articles/view/using-your-application-svn-revision-number
+         */
+        function getRevisionNumberFromFile()
         {
-            $version = $svn[3];
+            $svn = file(ROOT . '.svn' . DS . 'entries');
+            if (is_numeric(trim($svn[3])))
+            {
+                $version = $svn[3];
+            }
+            else # pre 1.4 svn used xml for this file
+            {
+                $version = explode('"', $svn[4]);
+                $version = $version[1];
+            }
+            unset($svn);
+            return trim($version);
         }
-        else # pre 1.4 svn used xml for this file
-        {
-            $version = explode('"', $svn[4]);
-            $version = $version[1];
-        }
-        define ('CLANSUITE_REVISION', trim($version));
-        unset ($svn);
-        unset ($version);
+        define ('CLANSUITE_REVISION', getRevisionNumberFromFile());
     }
-    else # default if no svn data available
+    else # get revision number from the Subversion Rev-property, if no SVN data available
     {
         /**
-         * Returns revision number
+         * Returns revision number from Subversion Rev-property
          */
         function getRevisionNumber()
         {
