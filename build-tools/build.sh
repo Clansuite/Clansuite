3bf334196bf9f189ae8a0178d22d5c20a9fcf033
Jens-André Koch <jakoch@web.de>
2008-11-19 20:41:11 +0000

    - build tools update

---
+++ build-tools/build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+phing -f /home/clansuite/svn-export/build-tools/build-clansuite.xml rebuild
--- build-tools/svn/clansuite-svn-export.sh
+++ build-tools/svn/clansuite-svn-export.sh
@@ -1,8 +1,8 @@
 # Clansuite - just an eSports CMS
-#
+# 
 # -- SVN Checkout and Export --
-# Author:   Jens-Andre Koch
-# Version:  0.1
+# Author:   Jens-André Koch
+# Version:  0.1 
 # Date:     28.11.2007
 # Licence:  GPL
 #
@@ -30,73 +30,75 @@ SVN_DIR_TRUNK=http://svn.gna.org/svn/clansuite/trunk/
 CHECKOUT_DIR=/home/clansuite/svn
 EXPORT_DIR=/home/clansuite/svn-export
 
-# CHECKOUT
+# CHECKOUT 
 # check if we have an checked out version already, (.svn) dir would exist then
 if [ -d $CHECKOUT_DIR/.svn ]; then
     # SVN UPDATE (incremental)
     svn update $CHECKOUT_DIR
-    echo ">>> svn update [done]"
+    echo "SVN UPDATE -- done --"
 else
     # if directory exists, remove
     rm -rf $CHECKOUT_DIR
-    # SVN CHECKOUT
+    # SVN CHECKOUT 
     svn checkout $SVN_DIR_TRUNK $CHECKOUT_DIR
-    echo ">>> svn checkout [done]"
+    echo "SVN CHECKOUT -- done --"
 fi
 
 # EXPORT
-# export the /trunk from SVN to the export directory
-rm -rf $EXPORT_DIR
-#svn export -rHEAD $SVN_DIR_TRUNK $EXPORT_DIR
-#echo ">>> svn export (from GNA svn) [done]"
-svn export -rHEAD $CHECKOUT_DIR $EXPORT_DIR
-echo ">>> svn export (from local svn) [done]"
+# export the /trunk from SVN to the export directory  
+#rm -rf $EXPORT_DIR
+#svn export $SVN_DIR_TRUNK $EXPORT_DIR
+#echo "SVN EXPORT from GNA -- done --"
+svn export --force -rHEAD $CHECKOUT_DIR $EXPORT_DIR
+echo "SVN EXPORT from local SVN -- done --"
+
+exit; 
 #--------
-
-# FOLDER COPY because of NAMING (/clansuite) inside the Archives
-cp -r /home/clansuite/svn-export/ /home/clansuite/clansuite
-echo ">>> copied SVN-EXPORT to CLANSUITE DIR!"
-
+# CLEANUP + FOLDER COPY because of NAMING (/clansuite) inside the Archives
+rm -rf /home/clansuite/clansuite/
+rm -rf /home/clansuite/downloads/
+# copy svn-export to clansuite (for sake of naming it right)
+cp -r /home/clansuite/svn-export/ /home/clansuite/clansuite/
+# copy readme.txt and additional files for the download dir
+cp /home/clansuite/svn-export/installation/webinstaller/README.txt /home/clansuite/downloads/
+echo "Copy SVN-EXPORT -> CLANSUITE DIR done!"
 #--------
 
 # Remove the Build-Tools Directory from an release version
+rm -rf /home/clansuite/clansuite/doc/manuals/
+rm -rf /home/clansuite/clansuite/installation/webinstaller
 rm -rf /home/clansuite/clansuite/build-tools
 rm -rf /home/clansuite/clansuite/tests
 rm /home/clansuite/clansuite/clansuite.config.php
-echo ">>> remove build-tools and stuff from CLANSUITE DIR!"
+echo ">>> remove DIR and STUFF from BUILD DIR!"
 
-#--------
-
-# CREATE ARCHIVES
-
-# ZIP
+# ARCHIVE 
+# ZIP 
 # a new, mx=compresslevel7, t = zip, r recurse
 7z a -mx7 -tzip /home/clansuite/downloads/clansuite.zip -r /home/clansuite/clansuite/
-echo ">>> ZIP created!"
+echo "ZIP created!"
 
 # TAR.GZ
 # c=create new archive, v=verbose on, f=write to file, z=zip
 tar cvzf /home/clansuite/downloads/clansuite.tar.gz ../clansuite/
-echo ">>> TAR.GZ created!"
+echo "TAR.GZ created!"
 
 #--------
 
-# Put Webinstaller into Downloads
-# fetch it from local SVN EXPORT and place it into /home/clansuite/downloads
-cp /home/clansuite/clansuite/installation/webinstaller/* /home/clansuite/downloads/
-echo ">>> webinstaller was dropped into the local downloads dir!"
+# Fetch Webinstaller into /home/clansuite/downloads
+cp /home/clansuite/svn-export/installation/webinstaller/* /home/clansuite/downloads/
 
 #--------
 
-# CREATE GPG SIGNATURES on the files in the downloads dir
+# CREATE GPG SIGNATURES
 gpg --detach /home/clansuite/downloads/webinstaller.php
 gpg --detach /home/clansuite/downloads/clansuite.zip
 gpg --detach /home/clansuite/downloads/clansuite.tar.gz
-echo ">>> GPG Signatures created! [done]"
+echo "Signatures created!"
 
 #--------
 
-# Transfer the local Downloads Directory to GNA
+rm -rf /home/clansuite/downloads/.svn
 
 # SCP - COPY STUFF TO GNA Downloads
 #scp /home/clansuite/downloads/clansuite.zip* vain@download.gna.org:/upload/clansuite/
@@ -108,6 +110,5 @@ echo ">>> GPG Signatures created! [done]"
 #echo "Content Fetch from GNA!"
 # 2) Put the content on download.gna.org
 rsync --delete -avr --rsh="ssh" /home/clansuite/downloads/ vain@download.gna.org:/upload/clansuite
-echo ">>> rsyncing content to GNA!"
-
-echo ">>> Mission Complete! <<<"
+echo "Content Uploaded to GNA!"
+echo "---> Mission Complete! <---"
