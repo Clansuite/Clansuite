363274271722681ab6483a0b1c7a1c114586729a
Jens-André Koch <jakoch@web.de>
2008-11-20 22:09:34 +0000

    - email unique

--- myrecords/generated/BaseCsUser.php
+++ myrecords/generated/BaseCsUser.php
@@ -8,9 +8,10 @@ abstract class BaseCsUser extends Doctrine_Record
 
   public function setTableDefinition()
   {
-    $this->setTableName('users');
+    $this->setTableName('cs_users');
+
     $this->hasColumn('user_id', 'integer', 4, array('unsigned' => 1, 'primary' => true, 'notnull' => true, 'autoincrement' => true));
-    $this->hasColumn('email', 'string', 150, array('fixed' => false, 'primary' => false, 'notnull' => false, 'autoincrement' => false));
+    $this->hasColumn('email', 'string', 150, array('fixed' => false, 'primary' => false, 'notnull' => false, 'autoincrement' => false, 'unique' => true));
     $this->hasColumn('nick', 'string', 25, array('fixed' => false, 'primary' => false, 'default' => '', 'notnull' => true, 'autoincrement' => false));
     $this->hasColumn('passwordhash', 'string', 40, array('fixed' => false, 'primary' => false, 'default' => '', 'notnull' => true, 'autoincrement' => false));
     $this->hasColumn('new_passwordhash', 'string', 40, array('fixed' => false, 'primary' => false, 'default' => '', 'notnull' => true, 'autoincrement' => false));
@@ -34,30 +35,30 @@ abstract class BaseCsUser extends Doctrine_Record
 
     $this->index('user_id', array('fields' => 'user_id'));
 
-    $this->hasMany('CsGroup', array(   'local' => 'user_id',
-                                        'foreign' => 'group_id',
-                                        'refClass' => 'CsRelUserGroup'
+    $this->hasMany('CsGroup',       array(      'local' => 'user_id',
+                                                'foreign' => 'group_id',
+                                                'refClass' => 'CsRelUserGroup'
                                          ));
 
-    $this->hasMany('CsOption', array(   'local' => 'user_id',
-                                         'foreign' => 'option_id',
-                                         'refClass' => 'CsRelUserOption'
+    $this->hasMany('CsOption',      array(      'local' => 'user_id',
+                                                'foreign' => 'option_id',
+                                                'refClass' => 'CsRelUserOption'
                                          ));
 
-    $this->hasOne('CsProfile', array('local' => 'user_id',
-                                         'foreign' => 'user_id',
+    $this->hasOne('CsProfile',      array(      'local' => 'user_id',
+                                                'foreign' => 'user_id',
                                          ));
 
-    $this->hasMany('CsProfileGuestbook', array('local' => 'user_id',
-                                         'foreign' => 'user_id',
+    $this->hasMany('CsProfileGuestbook', array( 'local' => 'user_id',
+                                                'foreign' => 'user_id',
                                          ));
 
-    $this->hasMany('CsProfileGeneral', array('local' => 'user_id',
-                                         'foreign' => 'user_id',
+    $this->hasMany('CsProfileGeneral', array(   'local' => 'user_id',
+                                                'foreign' => 'user_id',
                                          ));
 
-    $this->hasMany('CsProfileComputer', array('local' => 'user_id',
-                                         'foreign' => 'user_id',
+    $this->hasMany('CsProfileComputer', array(  'local' => 'user_id',
+                                                'foreign' => 'user_id',
                                          ));
     }
 }
