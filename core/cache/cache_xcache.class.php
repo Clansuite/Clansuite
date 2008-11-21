c3ebb04d3a98531270a1b7be77284e874bfb2871
Jens-André Koch <jakoch@web.de>
2008-11-21 16:31:33 +0000

    - added headers and security constant - added function skeletons for file&xcache

---
+++ core/cache/cache_xcache.class.php
@@ -0,0 +1,80 @@
+<?php
+   /**
+    * Clansuite - just an eSports CMS
+    * Jens-André Koch © 2005 - onwards
+    * http://www.clansuite.com/
+    *
+    * This file is part of "Clansuite - just an eSports CMS".
+    *
+    * LICENSE:
+    *
+    *    This program is free software; you can redistribute it and/or modify
+    *    it under the terms of the GNU General Public License as published by
+    *    the Free Software Foundation; either version 2 of the License, or
+    *    (at your option) any later version.
+    *
+    *    This program is distributed in the hope that it will be useful,
+    *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    *    GNU General Public License for more details.
+    *
+    *    You should have received a copy of the GNU General Public License
+    *    along with this program; if not, write to the Free Software
+    *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    *
+    * @license    GNU/GPL v2 or (at your option) any later version, see "/doc/LICENSE".
+    *
+    * @author     Jens-André Koch <vain@clansuite.com>
+    * @copyright  Jens-André Koch (2005-2008)
+    *
+    * @link       http://www.clansuite.com
+    * @link       http://gna.org/projects/clansuite
+    *
+    * @version    SVN: $Id: cache.class.php 1813 2008-03-21 22:46:21Z vain $
+    */
+
+// Security Handler
+if (!defined('IN_CS')){ die('Clansuite not loaded. Direct Access forbidden.'); }
+
+/**
+ * Clansuite Cache Handler for Xcache
+ *
+ * @package clansuite
+ * @subpackage cache
+ * @category caches
+ */
+class Clansuite_Cache_Xcache implements Clansuite_Cache_Interface
+{
+    /**
+     *
+     */
+    function fetch($key)
+    {
+
+    }
+
+    /**
+     *
+     */
+    function store($key, $data, $cache_lifetime)
+    {
+
+    }
+
+    /**
+     *
+     */
+    function delete($key)
+    {
+
+    }
+
+    /**
+     *
+     */
+    function stats()
+    {
+
+    }
+}
+?>
