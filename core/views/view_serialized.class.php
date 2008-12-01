a3f488b9ae1f62365bdd1bbac8c40d887f54e507
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2008-12-01 21:41:58 +0000

    - added comments - corrected header- modified: smarty, better RenderDoc compatibility v1/2
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@2602 34db09b4-2314-0410-a940-aec02a7c051d

--- core/views/view_serialized.class.php
+++ core/views/view_serialized.class.php
@@ -1,7 +1,7 @@
 <?php
    /**
     * Clansuite - just an eSports CMS
-    * Jens-Andre Koch © 2005 - onwards
+    * Jens-André Koch © 2005 - onwards
     * http://www.clansuite.com/
     *
     * This file is part of "Clansuite - just an eSports CMS".
@@ -22,10 +22,10 @@
     *    along with this program; if not, write to the Free Software
     *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
     *
-    * @license    GNU/GPL, see COPYING.txt
+    * @license    GNU/GPL v2 or (at your option) any later version, see "/doc/LICENSE".
     *
-    * @author     Jens-Andre Koch <vain@clansuite.com>
-    * @copyright  Jens-Andre Koch (2005 - onwards)
+    * @author     Jens-André Koch <vain@clansuite.com>
+    * @copyright  Jens-André Koch (2005 - onwards)
     *
     * @link       http://www.clansuite.com
     * @link       http://gna.org/projects/clansuite
@@ -41,8 +41,8 @@ if (!defined('IN_CS')){ die('Clansuite not loaded. Direct Access forbidden.' );}
  *
  * This is a wrapper/adapter for returning serialized PHP data.
  *
- * @author     Jens-Andre Koch <vain@clansuite.com>
- * @copyright  Jens-Andre Koch (2005-onwards)
+ * @author     Jens-André Koch <vain@clansuite.com>
+ * @copyright  Jens-André Koch (2005-onwards)
  *
  * @package     clansuite
  * @category    view
