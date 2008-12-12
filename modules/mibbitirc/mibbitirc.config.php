c43108f03093847d0dc5181030d577cd8ee43726
Jens-André Koch <jakoch@web.de>
2008-12-12 22:00:28 +0000

    - added module mibbit irc

---
+++ modules/mibbitirc/mibbitirc.config.php
@@ -0,0 +1,13 @@
+; <?php die( 'Access forbidden.' ); /* DO NOT MODIFY THIS LINE! ?>
+;
+; sysinfo configuration file
+;
+[mibbitirc]
+mibbit_irc_page_title = "Clansuite - Internet Relay Chat"
+mibbit_irc_nickname_prefix = "cs_"
+mibbit_irc_server = "gameservers.nl.quakenet.org"
+mibbit_irc_channel = "clansuite"
+mibbit_irc_width = "500"
+mibbit_irc_height = "300"
+
+; DO NOT REMOVE THIS LINE */ ?>
