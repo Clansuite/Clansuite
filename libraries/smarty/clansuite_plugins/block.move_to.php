22c46b52e8e7e9c84928e8465b6e010749f76b63
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2009-01-12 01:01:02 +0000

    - added smarty block function and outputfilter: move_to - this fixes #57 Ticket: 57
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@2687 34db09b4-2314-0410-a940-aec02a7c051d

---
+++ libraries/smarty/clansuite_plugins/block.move_to.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Smarty plugin
+ * @package Smarty
+ * @subpackage plugins
+ */
+
+/**
+ * This smarty function is part of "Clansuite - just an eSports CMS"
+ * @link http://www.clansuite.com
+ *
+ * @author Jens-André Koch <jakoch@web.de>
+ * @copyright Copyright (C) 2009 Jens-André Koch
+ * @license GNU General Public License v2 or any later version
+ * @version SVN $Id: $
+ *
+ * Smarty {move_to} block plugin
+ *
+ * Filename: block_move_to.php<br>
+ * Type:     block<br>
+ * Name:     move_to<br>
+ * Date:     Januar 11, 2009<br>
+ * Purpose:  move all content in move_to blocks to the position in the html document which is defined by tag parameter
+ *
+ * Examples:<br>
+ * <pre>
+ * {move_to tag="pre_head_close"}
+ *    <style type="text/css">
+ *       h1{font-family:fantasy;}
+ *    </style>
+ * {/move_to}
+ * </pre>
+ *
+ * @param array
+ * @param string
+ * @param Smarty
+ * @param boolean
+ * @return string
+ */
+function smarty_block_move_to($params, $content, &$smarty, &$repeat)
+{
+    if ( empty($content) )
+    {
+        return;
+    }
+
+    if( isset($params['tag']) )
+    {
+        $tag = strtoupper($params['tag']);
+    }
+    else
+    {
+        $smarty->trigger_error("Parameter 'tag' missing.");
+        return;
+    }
+
+    /**
+     * define possible moveto positions
+     * The x marks the position, the content will be moved to.
+     */
+    $valid_movement_positions = array('PRE_HEAD_CLOSE',  #  x</head>
+                                      'POST_BODY_OPEN',  #  <body>x
+                                      'PRE_BODY_CLOSE'); #  x</body>
+
+    # check if tag is a valid movement position
+    if( !in_array($tag, $valid_movement_positions) )
+    {
+        $smarty->trigger_error("Parameter 'tag' needs one of the following values: pre_head_close, post_body_open, pre_body_close");
+        return;
+    }
+
+    $content = '@@@SMARTY:'.$tag.':BEGIN@@@'.trim($content).'@@@SMARTY:'.$tag.':END@@@';
+
+    return $content;
+}
+?>
---
+++ libraries/smarty/clansuite_plugins/outputfilter.move_to.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Smarty plugin
+ * @package Smarty
+ * @subpackage plugins
+ */
+
+/**
+ * This smarty function is part of "Clansuite - just an eSports CMS"
+ * @link http://www.clansuite.com
+ *
+ * @author Jens-André Koch <jakoch@web.de>
+ * @copyright Copyright (C) 2009 Jens-André Koch
+ * @license GNU General Public License v2 or any later version
+ * @version SVN $Id: $
+ *
+ * Smarty {move_to} outputfilter moves content to several tag positions
+ *
+ * The x marks the position, the content will be moved to.
+ * pre_head_close = x</head>
+ * post_body_open = <body>x
+ * pre_body_close = x</body>
+ *
+ * @param string
+ * @param Smarty
+ * @return string
+ */
+function smarty_outputfilter_move_to($tpl_output, &$smarty)
+{
+    # PRE_HEAD_CLOSE = x</head>
+    $matches = array();
+    preg_match_all('!@@@SMARTY:PRE_HEAD_CLOSE:BEGIN@@@(.*?)@@@SMARTY:PRE_HEAD_CLOSE:END@@@!is', $tpl_output, $matches);
+    $tpl_output = preg_replace("!@@@SMARTY:PRE_HEAD_CLOSE:BEGIN@@@(.*?)@@@SMARTY:PRE_HEAD_CLOSE:END@@@!is", '', $tpl_output);
+    $tpl_output = str_replace('</head>', implode("\n", array_unique($matches[1]))."\n".'</head>', $tpl_output);
+
+    # POST_BODY_OPEN = <body>x
+    $matches = array();
+    preg_match_all('!@@@SMARTY:POST_BODY_OPEN:BEGIN@@@(.*?)@@@SMARTY:POST_BODY_OPEN:END@@@!is', $tpl_output, $matches);
+    $tpl_output = preg_replace("!@@@SMARTY:POST_BODY_OPEN:BEGIN@@@(.*?)@@@SMARTY:POST_BODY_OPEN:END@@@!is", '', $tpl_output);
+    $tpl_output = str_replace('<body>', implode("\n", '<body>'."\n".array_unique($matches[1])), $tpl_output);
+
+    # PRE_BODY_CLOSE = x</body>
+    $matches = array();
+    preg_match_all('!@@@SMARTY:PRE_BODY_CLOSE:BEGIN@@@(.*?)@@@SMARTY:PRE_BODY_CLOSE:END@@@!is', $tpl_output, $matches);
+    $tpl_output = preg_replace("!@@@SMARTY:PRE_BODY_CLOSE:BEGIN@@@(.*?)@@@SMARTY:PRE_BODY_CLOSE:END@@@!is", '', $tpl_output);
+    $tpl_output = str_replace('</body>', implode("\n", array_unique($matches[1]))."\n".'</body>', $tpl_output);
+
+    return $tpl_output;
+}
+?>
