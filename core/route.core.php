05a85e5715a92bc33af8705e7e82a0e2a177efba
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2009-01-20 14:05:58 +0000

    - added Clansuite_Javascripts and Clansuite_Route to /core
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@2736 34db09b4-2314-0410-a940-aec02a7c051d

---
+++ core/route.core.php
@@ -0,0 +1,78 @@
+<?php
+   /**
+    * Clansuite - just an eSports CMS
+    * Jens-André Koch © 2005 - onwards
+    * http://www.clansuite.com/
+    *
+    * This file is part of "Clansuite - just an eSports CMS".
+    *
+    * LICENSE:
+    *
+    *    This program is free software; you can redistribute it and/or modify
+    *    it under the terms of the GNU General Public License as published by
+    *    the Free Software Foundation; either version 2 of the License, or
+    *    (at your option) any later version.
+    *
+    *    This program is distributed in the hope that it will be useful,
+    *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    *    GNU General Public License for more details.
+    *
+    *    You should have received a copy of the GNU General Public License
+    *    along with this program; if not, write to the Free Software
+    *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    *
+    * @license    GNU/GPL v2 or (at your option) any later version, see "/doc/LICENSE".
+    *
+    * @author     Jens-André Koch <vain@clansuite.com>
+    * @copyright  Jens-André Koch (2005 - onwards)
+    *
+    * @link       http://www.clansuite.com
+    * @link       http://gna.org/projects/clansuite
+    *
+    * @version    SVN: $Id: functions.core.php 2686 2009-01-12 01:00:11Z vain $response.class.php 2580 2008-11-20 20:38:03Z vain $
+    */
+
+// Security Handler
+if (!defined('IN_CS')){ die('Clansuite not loaded. Direct Access forbidden.' );}
+
+/**
+ * Clansuite_Route
+ *
+ * Purpose: Clansuite_Route does URL Formating and internal Rewriting.
+ * The URL is segmented and restructured to fit the internal route to a controller.
+ * The internal routes are described in a central routing configuration file (whitelist).
+ * On Installation the routes are added (add_url_route), on deinstallation removed (del_url_route).
+ *
+ * There are two different URL Formatings allowed:
+ * 1. Slashes as Segment Dividers-Style, like so: /mod/sub/action/id
+ * 2. Fake HTML File Request or SMF-Style, like so: /mod.sub.action.id.html
+ *
+ */
+class Clansuite_Route
+{
+    # Route
+    private $route;
+
+    # URI Segments
+    private $module;
+    private $sub;
+    private $action;
+    private $id;
+
+    # Objects: Request, Config
+    private $request;
+    private $config;
+
+    public function construct(Clansuite_HttpRequest $request, Clansuite_Config $config)
+    {
+        $this->request = $request;
+        $this->config  = $config;
+    }
+
+    public function getRoute()
+    {
+        return $this->route;
+    }
+}
+?>
