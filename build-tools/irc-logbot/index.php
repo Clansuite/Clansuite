ed5032ca42d48afe118af882c035adf9810a6430
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2009-02-10 08:59:08 +0000

    - complete bugfix for irc-chanlog display
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@2779 34db09b4-2314-0410-a940-aec02a7c051d

--- build-tools/irc-logbot/index.php
+++ build-tools/irc-logbot/index.php
@@ -1,100 +1,113 @@
 <?php
+setlocale(LC_ALL,'de_DE');
 
-    include("header.inc.php");
+include("header.inc.php");
 
-  # array with name of months
-     $monate = array("01" => "Januar",
-                     "02" => "Februar",
-                     "03" => "M&auml;rz",
-                     "04" => "April",
-                     "05" => "Mai",
-                     "06" => "Juni",
-                     "07" => "Juli",
-                     "08" => "August",
-                     "09" => "September",
-                     "10" => "Oktober",
-                     "11" => "November",
-	             "12" => "Dezember");
-    
-    $back_year = 0;
-    $back_monat = 0;
-  
-  
-    $date = $_GET['date'];
-    if (isset($date) && preg_match("/^\d\d\d\d-\d\d-\d\d$/", $date)) {
+$back_year = 0;
+$back_monat = 0;
+
+/**
+ * View Single Logfile by appeding date to URL, like: "?date=2009-02-09"
+ */
+$date = $_GET['date'];
+if (isset($date) && preg_match("/^\d\d\d\d-\d\d-\d\d$/", $date))
+{
+    $jahr = substr($date, 0, 4);
+    $monat = substr($date, 5, 2);
+    $day = substr($date,8,2);
 ?>
-
     <p>
-     <a href="./">Index</a>
+        <a href="./">Index</a>
     </p>
 
-    <h2>IRC Log for <?php echo($date); ?></h2>
+    <h2>IRC Log for <?php echo strftime("%A, %d. %B %Y", mktime(0, 0, 0, $monat, $day, $jahr)); ?></h2>
+
     <p>
-     Timestamps are in GMT/BST.
+        Timestamps are in GMT/BST.
     </p>
     <p>
-    
-<?php
-        readfile($date . ".log");
-?>
+    <?php
+            readfile($date . ".log");
+    ?>
     </p>
 <?php
+}
+/**
+ * Display Overview of Logfiles available
+ */
+else
+{
+    # Alle Logfiles einlesen
+    $dir = opendir(".");
+    while (false !== ($file = readdir($dir)))
+    {
+        if (strpos($file, ".log") == 10)
+        {
+            $filearray[] = $file;
+        }
     }
-    else {
-        $dir = opendir(".");
-        while (false !== ($file = readdir($dir))) {
-            if (strpos($file, ".log") == 10) {
-                $filearray[] = $file;
+    closedir($dir);
+
+    # Array aufbereiten
+    foreach($filearray as $file)
+    {
+        $file   = substr($file, 0, 10);
+
+    	$jahr   = substr($file, 0, 4);
+    	$month  = substr($file, 5, 2);
+        $day    = substr($file, 8, 2);
+
+    	$years_array["$jahr"]["$month"]["$day"] = $file;
+    }
+    asort($years_array, SORT_STRING);
+
+    /** OUTPUT **/
+    ?>
+    <ul>Year(s):
+    <?php
+    # Display Links for all Years
+    foreach ($years_array as $year => $months)
+    {
+        echo "<a href='#$year'>$year</a> ";
+    }
+
+    foreach ($years_array as $year => $months)
+    {
+        # Year Name + Anchor
+    	echo "<h3><a name='$year'>$year</a></h3>";
+
+        echo "Month(s): ";
+        # Display Links for all Months
+        foreach($months as $month => $days)
+        {
+            $monthname = strftime("%B", mktime(0, 0, 0, $month, '01', $year));
+            echo "<a href='#$year-$month'>$monthname</a> ";
+        }
+
+        echo "<br />";
+
+        foreach ($months as $month => $days)
+        {
+            $monthname = strftime("%B", mktime(0, 0, 0, $month, '01', $year));
+            echo "<h3><a name='$year-$month'>$monthname</a></h3>";
+            echo "<blockquote>";
+            foreach($days as $day => $filename)
+            {
+
+
+                echo "<li>";
+                ?>
+                <a href="<?php echo($_SERVER['PHP_SELF'] . "?date=" . $filename); ?>">
+                <?php
+                echo strftime("%A, %d. %B %Y", mktime(0, 0, 0, $month, $day, $year));
+                echo "</a>";
+                echo "</li>";
             }
+            echo "</blockquote>";
         }
-        closedir($dir);
-        
-        rsort($filearray);
-?>
-    <ul>
-    Year: <a href='#2009'>2009</a> 
-	  <a href='#2008'>2008</a>
-<?php
-        
-        
-        foreach ($filearray as $file) {
-            $file = substr($file, 0, 10);
-
-	$jahr = substr($file, 0, 4);
-	if($jahr != $back_year) {
-			          echo "</blockquote></blockquote><h3><a name='". $jahr."'>".$jahr."</a></h3>";
-				  
-				  echo '<blockquote>Monate:';
-				  foreach($monate as $monats_nr => $monats_name)
-				  {?>
-				      <a href="#<?php echo $monats_nr; ?>"><?php echo $monats_name;?></a>
-				  <?php
-				  }
-				  ?> </blockquote> <?php
-				  $back_year = $jahr; 
-				}
-
-	$monat = substr($file, 5, 2);
-	if($monat != $back_monat)
-	{
-			?>
-			<blockquote><h4><a name="<?php echo $monat;?>"><?php echo $monate[$monat];?></a></h4>
-			<blockquote>
-			<?php
-			$back_monat = $monat;
-	}
-       
-?>		
-        <li><a href="<?php echo($_SERVER['PHP_SELF'] . "?date=" . $file); ?>"><?php echo substr($file,8,2).' '.$monate[$monat].' '.$jahr; ?></a></li>
-	
-<?php
-	if($monat != $back_monat && $jahr != $back_year){ echo '</blockquote>'; }
-        }
-?>
-    </ul>
-<?php
     }
 
-    include("footer.inc.php");
-
+    ?></ul><?php
+}
+include("footer.inc.php");
 ?>
