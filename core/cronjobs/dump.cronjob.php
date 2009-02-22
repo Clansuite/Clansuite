061e6465c372ad076c20aa6ea90f688cef1d8f00
Jens-André Koch <jakoch@web.de>
2009-02-22 13:33:46 +0000

    - added feature: cronjob support. this is basically a reimplementation for php5 of kai blankenhorn's pseudo-cron (bitfolge.de) for php.

---
+++ core/cronjobs/dump.cronjob.php
@@ -0,0 +1,46 @@
+<?php
+   /**
+    * Clansuite - just an eSports CMS
+    * Jens-André Koch © 2005 - onwards
+    * http://www.clansuite.com/
+    *
+    * This file is part of "Clansuite - just an eSports CMS".
+    *
+    * LICENSE:
+    *
+    *    This program is free software; you can redistribute it and/or modify
+    *    it under the terms of the GNU General Public License as published by
+    *    the Free Software Foundation; either version 2 of the License, or
+    *    (at your option) any later version.
+    *
+    *    This program is distributed in the hope that it will be useful,
+    *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    *    GNU General Public License for more details.
+    *
+    *    You should have received a copy of the GNU General Public License
+    *    along with this program; if not, write to the Free Software
+    *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    *
+    * @license    GNU/GPL v2 or (at your option) any later version, see "/doc/LICENSE".
+    *
+    * @author     Jens-André Koch <vain@clansuite.com>
+    * @copyright  Jens-André Koch (2005 - onwards)
+    *
+    * @link       http://www.clansuite.com
+    * @link       http://gna.org/projects/clansuite
+´   *
+    * @version    SVN: $Id: $
+    */
+
+// Security Handler
+if (!defined('IN_CS')){ die('Clansuite not loaded. Direct Access forbidden.'); }
+
+class Clansuite_Cronjob_Dump implements Clansuite_Cronjob
+{
+    public function execute()
+    {
+        echo "It's just me, a dumb example of an cronjump. <br/> My name is 'dump.cronjob.php'.";
+    }
+}
+?>
