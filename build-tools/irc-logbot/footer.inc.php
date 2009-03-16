d37a34f0d26954e3615b4dc4107a9ca21222bb2c
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2009-03-16 13:57:25 +0000

    - moved trunk/build-tools/irc-logbot to /website-clansuite.com/irclogs
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@2826 34db09b4-2314-0410-a940-aec02a7c051d

--- build-tools/irc-logbot/footer.inc.php
+++
@@ -1,8 +0,0 @@
-<p>
- These logs were automatically created by <b><?php echo($nick); ?></b> on
- <a href="irc://<?php echo($server . "/" . substr($channel, 1)); ?>"><?php echo($server); ?></a>
- using the <a href="http://www.jibble.org/logbot/">Java IRC LogBot</a>.
-</p>
-</div><!-- close maincontent -->
-</body>
-</html>
