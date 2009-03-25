410ff881a1a9293a04ca7c6b6210e53459acb178
creep7 <creep7@34db09b4-2314-0410-a940-aec02a7c051d>
2009-03-25 19:10:55 +0000

    * Poll Module Structure * Shoutbox Module Structure * Sponsors Module Structure
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@2857 34db09b4-2314-0410-a940-aec02a7c051d

---
+++ modules/sponsors/sponsors.module.php
@@ -0,0 +1,31 @@
+<?php
+/*======================================================================*\
+|| #################################################################### ||
+|| # ClanSuite v0.2 Alpha                                             # ||
+|| # ---------------------------------------------------------------- # ||
+|| # Copyright ©2005-2009 ClanSuite Developer. All Rights Reserved.   # ||
+|| # This file may not be redistributed in whole or significant part. # ||
+|| # ------------------ CLANSUITE IS FREE SOFTWARE ------------------ # ||
+|| # http://www.clansuite.com | http://www.clansuite.com/license.html # ||
+|| #################################################################### ||
+\*======================================================================*/
+
+// Security Handler
+if (!defined('IN_CS')){die('Clansuite not loaded. Direct Access forbidden.');}
+
+/*======================================================*\
+|| #################################################### ||
+|| #   This is the Clansuite Module Class - Sponsors  # ||
+|| # ------------------------------------------------ # ||
+|| # @author     Niklas Károly <creep7@clansuite.com> # ||
+|| # @copyright  Niklas Károly (2009 - onwards)       # ||
+|| # @since      Class available since 0.1 Alpha      # ||
+|| # ------------------------------------------------ # ||
+|| # @package     clansuite                           # ||
+|| # @category    module                              # ||
+|| # @subpackage  sponsors                            # ||
+|| #################################################### ||
+\*======================================================*/
+
+
+?>
