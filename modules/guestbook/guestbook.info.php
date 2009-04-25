ac6d43825d88632d94087c1dbd9ce4e9af4c8244
thunderm00n <thunderm00n@34db09b4-2314-0410-a940-aec02a7c051d>
2009-04-25 21:13:40 +0000

    Added Basic Structures for Guestbook Module
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@2976 34db09b4-2314-0410-a940-aec02a7c051d

---
+++ modules/guestbook/guestbook.info.php
@@ -0,0 +1,24 @@
+; <?php die( 'Access forbidden.' ); /* DO NOT MODIFY THIS LINE! ?>
+;
+; guestbook Info
+;
+
+[guestbook_info]
+author="Daniel Winterfeldt, Jens-Andre Koch"
+license="GPLv2 or any later"
+link="http://www.clansuite.com"
+name = guestbook
+description = 
+dependencies =
+package = Development
+
+[guestbook_package]
+version = 0.2-0.x-dev
+project = guestbook
+datestamp =
+uniqueid =
+
+[guestbook_widgets]
+
+
+; DO NOT REMOVE THIS LINE */ ?>
