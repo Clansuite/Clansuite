31d28fd75ee136b95eb5cdbdf6d758b6bdee2c9b
Jens-André Koch <jakoch@web.de>
2009-04-25 19:49:34 +0000

    - gallery.config.php

--- modules/gallery/gallery.config.php
+++ modules/gallery/gallery.config.php
@@ -1,9 +1,8 @@
 ; <?php die( 'Access forbidden.' ); /* DO NOT MODIFY THIS LINE! ?>
-; 
-; ClanSuite Gallery configuration file
+;
+; Clansuite Configuration File for Module Gallery
 ;
 [gallery]
 mooflow = true
-
 
 ; DO NOT REMOVE THIS LINE */ ?>
