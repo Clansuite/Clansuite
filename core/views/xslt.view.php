a0e91a7d6ac2826f0826aede45fbafd2b8f2b1b6
Jens-André Koch <jakoch@web.de>
2009-04-27 19:58:29 +0000

    - added renderer for xml/xslt

---
+++ core/views/xslt.view.php
@@ -0,0 +1,133 @@
+<?php
+   /**
+    * Clansuite - just an eSports CMS
+    * Jens-André Koch © 2005 - onwards
+    * http://www.clansuite.com/
+    *
+    * This file is part of "Clansuite - just an eSports CMS".
+    *
+    * LICENSE:
+    *
+    *    This program is free software; you can redistribute it and/or modify
+    *    it under the terms of the GNU General Public License as published by
+    *    the Free Software Foundation; either version 2 of the License, or
+    *    (at your option) any later version.
+    *
+    *    This program is distributed in the hope that it will be useful,
+    *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    *    GNU General Public License for more details.
+    *
+    *    You should have received a copy of the GNU General Public License
+    *    along with this program; if not, write to the Free Software
+    *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    *
+    * @license    GNU/GPL v2 or (at your option) any later version, see "/doc/LICENSE".
+    *
+    * @author     Jens-André Koch <vain@clansuite.com>
+    * @copyright  Jens-André Koch (2005 - onwards)
+    *
+    * @link       http://www.clansuite.com
+    * @link       http://gna.org/projects/clansuite
+    *
+    * @version    SVN: $Id$
+    */
+
+// Security Handler
+if (!defined('IN_CS')){ die('Clansuite not loaded. Direct Access forbidden.' ); }
+
+/**
+ * Clansuite View Class - View for XSLT/XML
+ *
+ * This is a wrapper/adapter for returning XML/XSLT data.
+ *
+ * @author     Jens-André Koch <vain@clansuite.com>
+ * @copyright  Jens-André Koch (2005-onwards)
+ *
+ * @category    Clansuite
+ * @package     Core
+ * @subpackage  View
+ */
+class view_xslt extends Clansuite_Renderer_Base
+{
+    /**
+     * holds instance of Dependency Injector Phemto
+     */
+    protected $injector   = null;
+
+    /**
+	 * holds instance of XSLT Render Engine (object)
+	 * @var object xslt
+	 */
+    protected $xslt = null;
+
+    /**
+     * holds the abs path to the xsl stylesheet
+     * @var string
+     */
+   protected $xslfile = null;
+
+    public function __construct(Phemto $injector = null)
+    {
+      # apply instances to class
+      $this->injector = $injector;
+
+	  # get instances from injector
+      $this->config         = $this->injector->instantiate('Clansuite_Config');
+      $this->response       = $this->injector->instantiate('Clansuite_HttpResponse');
+
+      # instantiate the render engine
+      $this->xslt = new XSLTProcessor;
+
+      # eventlog initalization
+    }
+
+    /**
+     * Returns XSLT RenderEngine Object
+     *
+     * @return xslt_processor
+     */
+    public function getEngine()
+    {
+        return $this->xslt;
+    }
+
+    /**
+     * setXSLStyleSheet
+     *
+     * @param $xslfile The fullpath to the XSL StyleSheet file for later combination with the xml data.
+     */
+    public function setXSLStyleSheet($xslfile)
+    {
+        $this->xslfile = $xslfile;
+    }
+
+    /**
+     * getXSLStyleSheet
+     *
+     * @return $xslfile
+     */
+    public function getXSLStyleSheet()
+    {
+        return $this->xslfile;
+    }
+
+    /**
+     * This renders the xml $data array.
+     *
+     * @param $data XML Data to render
+     */
+    public function render($data)
+    {
+        # $this->response()->setContentType('text/html');
+
+        # import the stylesheet  for later transformation
+        $this->xslt->importStyleSheet( DOMDocument::load($this->getXSLStyleSheet());
+
+        # then import the xml data (or file) into the XSLTProcessor and start the transform
+        $dom =  $this->xslt->transformToXML( DOMDocument::load( $data ) );
+
+        return  $dom;
+    }
+}
+?>
