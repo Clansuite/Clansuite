5f149450abcb82013fa7a5a7b74ee0ad7f8694bb
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2009-05-04 19:27:19 +0000

    - added smarty viewhelper currentmodule
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@3033 34db09b4-2314-0410-a940-aec02a7c051d

---
+++ core/viewhelper/smarty/function.currentmodule.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Smarty plugin
+ * @package Smarty
+ * @subpackage Clansuite Plugins / Smarty View Helper
+ */
+
+/**
+ * Smarty Currentmodule
+ * Displays they name of the current module
+ *
+ * Examples:
+ * <pre>
+ * {pagination}
+ * </pre>
+ *
+ * Type:     function<br>
+ * Name:     currentmodule<br>
+ * Purpose:  display name of current module<br>
+ * @author   Jens-André Koch <vain@clansuite.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GPL 2 / any later version
+ * @param array $params
+ * @param Smarty $smarty
+ * @return string
+ */
+function smarty_function_currentmodule($params, &$smarty)
+{
+    return Clansuite_ModuleController_Resolver::getModuleName();
+}
+?>
