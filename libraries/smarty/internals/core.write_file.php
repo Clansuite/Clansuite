36c174f3e19dd6953a282298e4f9c888e9625a7b
Jens-André Koch <jakoch@web.de>
2009-05-29 01:22:20 +0000

    - updated Smarty to version 2.6.25 and reapplied hacks to the core

--- libraries/smarty/internals/core.write_file.php
+++ libraries/smarty/internals/core.write_file.php
@@ -37,13 +37,8 @@ function smarty_core_write_file($params, &$smarty)
     fwrite($fd, $params['contents']);
     fclose($fd);
 
-    if( file_exists($params['filename']) )
-    {
-        @unlink($params['filename']);
-    }
-
-    if (!@rename($_tmp_file, $params['filename'])) {
-        // On platforms and filesystems that cannot overwrite with rename()
+    if (DIRECTORY_SEPARATOR == '\\' || !@rename($_tmp_file, $params['filename'])) {
+        // On platforms and filesystems that cannot overwrite with rename() 
         // delete the file before renaming it -- because windows always suffers
         // this, it is short-circuited to avoid the initial rename() attempt
         @unlink($params['filename']);
