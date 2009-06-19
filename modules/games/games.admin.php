6ee63f8940f8e9a9462524e96cb78b9a0601b5b1
Pasqual Eusterfeldhaus <thunderm00n@gna.org>
2009-06-19 15:51:05 +0000

    Added Games Module Struktures for Backend

---
+++ modules/games/games.admin.php
@@ -0,0 +1,92 @@
+<?php
+   /**
+    * Clansuite - just an eSports CMS
+    * Jens-André Koch © 2005 - onwards
+    * http://www.clansuite.com/
+    *
+    * This file is part of "Clansuite - just an eSports CMS".
+    *
+    * LICENSE:
+    *
+    *    This program is free software; you can redistribute it and/or modify
+    *    it under the terms of the GNU General Public License as published by
+    *    the Free Software Foundation; either version 2 of the License, or
+    *    (at your option) any later version.
+    *
+    *    This program is distributed in the hope that it will be useful,
+    *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    *    GNU General Public License for more details.
+    *
+    *    You should have received a copy of the GNU General Public License
+    *    along with this program; if not, write to the Free Software
+    *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    *
+    * @license    GNU/GPL v2 or (at your option) any later version, see "/doc/LICENSE".
+    *
+    * @author     Jens-André Koch <vain@clansuite.com>
+    * @copyright  Jens-André Koch (2005 - onwards)
+    *
+    * @link       http://www.clansuite.com
+    * @link       http://gna.org/projects/clansuite
+    *
+
+// Security Handler
+if (!defined('IN_CS')){ die('Clansuite not loaded. Direct Access forbidden.' );}
+
+/**
+ * Clansuite
+ *
+ * Module:      Games
+ * Submodule:   Admin
+ *
+ * @author     Jens-André Koch <vain@clansuite.com>
+ * @copyright  Jens-André Koch (2005 - $Date: 2008-06-12 01:44:20 +0200 (Do, 12 Jun 2008) $)
+ *
+ * @package     clansuite
+ * @category    module
+ * @subpackage  games
+ */
+class Module_Games_Admin extends Clansuite_ModuleController implements Clansuite_Module_Interface
+{
+
+    public function __construct(Phemto $injector=null)
+    {
+        parent::__construct(); # run constructor on controller_base
+    }
+
+    public function execute(Clansuite_HttpRequest $request, Clansuite_HttpResponse $response)
+    {
+        parent::initRecords('games');
+    }
+
+    /**
+     * Module_Games_Admin - action_admin_show
+     *
+     */
+    public function action_admin_show()
+    {
+        # Permission check
+        #$perms::check('cc_view_games');
+
+        # Set Pagetitle and Breadcrumbs
+        Clansuite_Trail::addStep( _('Show'), '/index.php?mod=games&amp;sub=admin&amp;action=show');
+
+        $games = Doctrine_Query::create()
+               ->select('c.*')
+               ->from('CsGames c')
+               ->fetchArray();
+
+        #clansuite_xdebug::printr($games);
+
+        $view = $this->getView();
+        $view->assign('games', $games);
+
+        # Set Layout Template
+        $this->getView()->setLayoutTemplate('admin/index.tpl');
+
+        # Prepare the Output
+        $this->prepareOutput();
+    }
+}
+?>
---
+++ modules/games/games.config.php
@@ -0,0 +1,8 @@
+; <?php die( 'Access forbidden.' ); /* DO NOT MODIFY THIS LINE! ?>
+;
+; Clansuite Configuration File for Games Module
+;
+[games]
+
+
+; DO NOT REMOVE THIS LINE */ ?>
