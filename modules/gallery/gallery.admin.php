c1e3d6321edaa64efedec57f877e5c8ad2ac82e2
Pasqual Eusterfeldhaus <thunderm00n@gna.org>
2009-06-19 23:08:46 +0000

    fixed parent::initRecords('gallery'); in gallery.admin.php

--- modules/gallery/gallery.admin.php
+++ modules/gallery/gallery.admin.php
@@ -53,7 +53,7 @@ class Module_Gallery_Admin extends Clansuite_ModuleController implements Clansui
 
     public function execute(Clansuite_HttpRequest $request, Clansuite_HttpResponse $response)
     {
-
+		parent::initRecords('gallery');
     }
 
     /**
