70a270871ea2d3a275f475e25be6f3d20a6a494f
thunderm00n <thunderm00n@34db09b4-2314-0410-a940-aec02a7c051d>
2009-06-19 15:51:05 +0000

    Added Games Module Struktures for Backend
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@3157 34db09b4-2314-0410-a940-aec02a7c051d

---
+++ modules/games/games.info.php
@@ -0,0 +1,24 @@
+; <?php die( 'Access forbidden.' ); /* DO NOT MODIFY THIS LINE! ?>
+;
+; games Info
+;
+
+[games_info]
+author="Jens-Andre Koch"
+license="GPLv2 or any later"
+link="http://www.clansuite.com"
+name = games
+description = 
+dependencies =
+package = Development
+
+[games_package]
+version = 0.2-0.x-dev
+project = games
+datestamp =
+uniqueid =
+
+[games_widgets]
+
+
+; DO NOT REMOVE THIS LINE */ ?>
