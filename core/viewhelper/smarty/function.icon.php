a23d344537dd6e7cb47ef4aff2e13b4b93cfa3f4
Jens-André Koch <jakoch@web.de>
2009-08-31 20:51:35 +0000

    - updated smarty function {icon}

--- core/viewhelper/smarty/function.icon.php
+++ core/viewhelper/smarty/function.icon.php
@@ -1,4 +1,35 @@
 <?php
+/**
+ * Smarty plugin
+ * @package Smarty
+ * @subpackage plugins
+ */
+
+/**
+ * This smarty function is part of "Clansuite - just an eSports CMS"
+ * @link http://www.clansuite.com
+ *
+ * @author Jens-André Koch <jakoch@web.de>
+ * @copyright Copyright (C) 2009 Jens-André Koch
+ * @license GNU Public License (GPL) v2 or any later version
+ * @version SVN $Id$
+ *
+ * Name:    icon
+ * Type:    function
+ * Purpose: This TAG inserts images/icons.
+ *
+ * Static Function to Call variable Methods from templates via
+ * {icon ...}
+ * Parameters: icondir, src, width, heigth, alt, extra
+ *
+ * Example Usage:
+ * {icon name="rss"}
+ * {icon theme="lullacons" name="calendar"}
+ *
+ * @param array $params as described above
+ * @param Smarty $smarty
+ * @return string
+ */
 
 function smarty_function_icon($params, &$smarty)
 {
@@ -6,17 +37,29 @@ function smarty_function_icon($params, &$smarty)
 
     extract($params);
 
-    /*
-    if (empty($src) xor empty($name) )
+    # set a icondir / iconpack
+    if ( empty($icondir))
     {
-        $smarty->trigger_error('assign_array: the parameter "src" or "name" is missing. Please add src="www_path_to_your_file" or name="imagename" to your template.');
-        return;
-    }*/
+        $icondir = 'icons';
+    }
+    else # icondir var incomming
+    {
+        # check if it is a valid one
+        $icondir_whitelist = array( 'icons', 'lullacons' );
+        if(in_array($icondir, $icondir_whitelist))
+        {
+            # valid
+        }
+        else # fallback to valid
+        {
+            $icondir = 'icons';
+        }
+    }
 
     # transform name into a valid image src
-    if ( empty($src) and (empty($name) == false))
+    if (empty($src) and isset($name))
     {
-        $src = WWW_ROOT_THEMES_CORE . '/images/icons/'.$name.'.png';
+        $src = WWW_ROOT_THEMES_CORE . '/images/'.$icondir.'/'.$name.'.png';
     }
 
     # we got no height, set it to zero
