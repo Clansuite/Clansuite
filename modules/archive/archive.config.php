773338b89cf4e2fbd9ed26fd9232548c85888041
Pasqual Eusterfeldhaus <thunderm00n@gna.org>
2009-08-31 18:25:11 +0000

    added modules/archive for testing proposes

---
+++ modules/archive/archive.config.php
@@ -0,0 +1,8 @@
+; <?php die( 'Access forbidden.' ); /* DO NOT MODIFY THIS LINE! ?>
+;
+; Clansuite Configuration File for Module Archive
+;
+[archive]
+
+
+; DO NOT REMOVE THIS LINE */ ?>
---
+++ modules/archive/archive.info.php
@@ -0,0 +1,25 @@
+; <?php die( 'Access forbidden.' ); /* DO NOT MODIFY THIS LINE! ?>
+;
+; Archive Info
+;
+
+[quotes_info]
+author="Jens-Andre Koch"
+license="GPLv2 or any later"
+link="http://www.clansuite.com"
+name = Archive
+description = 
+dependencies =
+package = Development
+
+[quotes_package]
+version = 0.2-0.x-dev
+project = Archive
+datestamp =
+uniqueid =
+
+[archive_widgets]
+widget_archive
+
+
+; DO NOT REMOVE THIS LINE */ ?>
