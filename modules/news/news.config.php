4080a6ad13fcd24f6ab27640b823a5ba09df43ce
thunderm00n <thunderm00n@34db09b4-2314-0410-a940-aec02a7c051d>
2009-10-03 15:53:20 +0000

    - moved resultsPerPage for the different actions to config
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@3443 34db09b4-2314-0410-a940-aec02a7c051d

--- modules/news/news.config.php
+++ modules/news/news.config.php
@@ -3,7 +3,11 @@
 ; Clansuite Configuration File for Module News
 ;
 [news]
+
+; Limitation for different Actions
 resultsPerPage_show = 3
+resultsPerPage_archiv = 3
+resultsPerPage_fullarchiv = 25
 
 
 ; DO NOT REMOVE THIS LINE */ ?>
