a04c95039f32be9e147362f711ea3a5fdb25e377
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2009-11-02 07:06:49 +0000

    - pagination bugfix
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@3566 34db09b4-2314-0410-a940-aec02a7c051d

--- core/viewhelper/smarty/function.pagination.php
+++ core/viewhelper/smarty/function.pagination.php
@@ -17,6 +17,7 @@
  * Type:     function<br>
  * Name:     pagination<br>
  * Purpose:  display pagination if needed<br>
+ *
  * @author   Jens-André Koch <vain@clansuite.com>
  * @license  http://www.gnu.org/copyleft/gpl.html GPL 2 / any later version
  * @param array $params
@@ -25,26 +26,31 @@
  */
 function smarty_function_pagination($params, &$smarty)
 {
-    if( isset($params['type']) and $params['type']== 'alphabet' and $smarty->template_exists('pagination-alphabet.tpl') )
+    # check if a alphabet pagination is requested
+    if( isset($params['type']) and $params['type'] == 'alphabet' )
     {
-        # load the generic pagination template
-        return $smarty->fetch('pagination-alphabet.tpl');
+        # check if file exists
+        if( $smarty->template_exists('pagination-alphabet.tpl') == false )
+        {
+            echo 'Pagination Template for alphabet not found.';
+        }
+        else # load the generic pagination template
+        {
+            return $smarty->fetch('pagination-alphabet.tpl');
+        }
     }
 
-    # check if file exists true and if it's necessary to paginate
+    # check if file exists
     if( $smarty->template_exists('pagination-generic.tpl') == false )
     {
         echo 'Pagination Template not found.';
     }
-
-    if( $smarty->get_template_vars('pager')->haveToPaginate() )
+    else # load the generic pagination template
     {
-        # load the generic pagination template
         return $smarty->fetch('pagination-generic.tpl');
     }
-    else # there's no need to paginate
-    {
-        #return 'No need to paginate!';
-    }
+
+    # @deprecated: check if it's necessary to paginate
+    # if( $smarty->get_template_vars('pager')->haveToPaginate() )
 }
 ?>
