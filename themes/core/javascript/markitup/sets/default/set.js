5bb63377e5930ba193bc0b3e3c46d3e51df84955
thunderm00n <thunderm00n@34db09b4-2314-0410-a940-aec02a7c051d>
2009-11-04 23:26:12 +0000

    - changed set of markItUp to better fit our needs
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@3612 34db09b4-2314-0410-a940-aec02a7c051d

--- themes/core/javascript/markitup/sets/default/set.js
+++ themes/core/javascript/markitup/sets/default/set.js
@@ -17,9 +17,36 @@ mySettings = {
 		{name:'Bold', key:'B', openWith:'(!(<strong>|!|<b>)!)', closeWith:'(!(</strong>|!|</b>)!)' },
 		{name:'Italic', key:'I', openWith:'(!(<em>|!|<i>)!)', closeWith:'(!(</em>|!|</i>)!)'  },
 		{name:'Stroke through', key:'S', openWith:'<del>', closeWith:'</del>' },
+		{name:'Underline', key:'U', openWith:'[u]', closeWith:'[/u]'},
+		{name:'Size', key:'S', openWith:'[size=[![Text size]!]]', closeWith:'[/size]',
+		dropMenu :[
+			{name:'Big', openWith:'[size=200]', closeWith:'[/size]' },
+			{name:'Normal', openWith:'[size=100]', closeWith:'[/size]' },
+			{name:'Small', openWith:'[size=50]', closeWith:'[/size]' }
+		]},
+		{separator:'---------------' },
+		{name:'Bulleted list', openWith:'(!(* |!|*)!)'},
+		{name:'Numeric list', openWith:'(!(# |!|#)!)'}, 
+		{name:'Text indent', className:'indent', openWith:'text-indent:', placeHolder:'5px', closeWith:';' },
+		{name:'Alignments', className:'alignments', dropMenu:[
+			{name:'Left', className:'left', replaceWith:'text-align:left;'},
+			{name:'Center', className:'center', replaceWith:'text-align:center;'},
+			{name:'Right', className:'right', replaceWith:'text-align:right;'},
+			{name:'Justify', className:'justify', replaceWith:'text-align:justify;'}
+			]
+		},
+		{name:'Padding/Margin', className:'padding', dropMenu:[
+				{name:'Top', className:'top', openWith:'(!(padding|!|margin)!)-top:', placeHolder:'5px', closeWith:';' },
+				{name:'Left', className:'left', openWith:'(!(padding|!|margin)!)-left:', placeHolder:'5px', closeWith:';' },
+				{name:'Right', className:'right', openWith:'(!(padding|!|margin)!)-right:', placeHolder:'5px', closeWith:';' },
+				{name:'Bottom', className:'bottom', openWith:'(!(padding|!|margin)!)-bottom:', placeHolder:'5px', closeWith:';' }
+			]
+		},
 		{separator:'---------------' },
 		{name:'Picture', key:'P', replaceWith:'<img src="[![Source:!:http://]!]" alt="[![Alternative text]!]" />' },
 		{name:'Link', key:'L', openWith:'<a href="[![Link:!:http://]!]"(!( title="[![Title]!]")!)>', closeWith:'</a>', placeHolder:'Your text to link...' },
+		{name:'Quotes', openWith:'bq(!(([![Class]!]))!). '},
+		{name:'Code', openWith:'@', closeWith:'@'},
 		{separator:'---------------' },
 		{name:'Clean', className:'clean', replaceWith:function(markitup) { return markitup.selection.replace(/<(.*?)>/g, "") } },		
 		{name:'Preview', className:'preview',  call:'preview'}
