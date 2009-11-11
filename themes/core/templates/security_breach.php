049d04d02daafce5f59307215d4796723a790e4a
Jens-André Koch <jakoch@web.de>
2009-11-11 21:36:51 +0000

    - removed short tags

--- themes/core/templates/security_breach.php
+++ themes/core/templates/security_breach.php
@@ -45,14 +45,14 @@ if (!defined('IN_CS')){ die('Clansuite not loaded. Direct Access forbidden.' );}
     <p>&nbsp;</p>
     <p>&nbsp;</p>
     <div align="center">
-        <img src="<?=WWW_ROOT_THEMES_CORE?>/images/security_breach.jpg" border="0">
+        <img src="<php echo WWW_ROOT_THEMES_CORE?>/images/security_breach.jpg" border="0">
         <br>
         <font face="Verdana" size="2" color="red">
-            <strong><?=_('Warning: Intrusion Detection');?></strong>
+            <strong><?php echo _('Warning: Intrusion Detection'); ?></strong>
             <br>
-            <?=_('Sorry! But your latest action got classified as an possible hacking attempt or there is an unusual failure in progress.');?>
+            <php echo _('Sorry! But your latest action got classified as an possible hacking attempt or there is an unusual failure in progress.'); ?>
             <br>
-            <?=_('User actions were logged and reported to the webmaster.');?>
+            <php echo _('User actions were logged and reported to the webmaster.'); ?>
         </font>
     </div>
 </body>
