b7db4961a379f4fd59a497ecf636925b7315cc2b
Jens-André Koch <jakoch@web.de>
2010-01-26 19:21:17 +0000

    - readded indes module

---
+++ modules/index/index.info.php
@@ -0,0 +1,24 @@
+; <?php die( 'Access forbidden.' ); /* DO NOT MODIFY THIS LINE! ?>
+;
+; index Info
+;
+
+[index_info]
+author="Jens-Andre Koch"
+license="GPLv2 or any later"
+link="http://www.clansuite.com"
+name = index
+description = 
+dependencies =
+package = Development
+
+[index_package]
+version = 0.2-0.x-dev
+project = index
+datestamp =
+uniqueid =
+
+[index_widgets]
+
+
+; DO NOT REMOVE THIS LINE */ ?>
