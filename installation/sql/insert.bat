20412563a791aa7a5d06f249d26c0ce0e685c2dc
xsign <xsign@34db09b4-2314-0410-a940-aec02a7c051d>
2010-02-06 01:31:39 +0000

    db is exported - %dbname% not needed anymore
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@4052 34db09b4-2314-0410-a940-aec02a7c051d

--- installation/sql/insert.bat
+++ installation/sql/insert.bat
@@ -24,6 +24,6 @@ FOR /F "tokens=1,2 delims=/: " %%a in ('time/T') do set CTIME=%%a-%%b
 @ECHO Creating backup of %dbname% as \sql\%dbname%_%CDATE%_%CTIME%_backup.sql ...
 %mysqldir%/mysqldump -u %mysqluser% -p%mysqlpassword% --skip-add-locks --add-drop-table --databases %dbname% >  %basedir%\sql\backup\%dbname%_%CDATE%_%CTIME%_backup.sql
 @ECHO Beginning insert of \sql\%dbname%.sql ...
-%mysqldir%/mysql -u %mysqluser% -p%mysqlpassword% %dbname% < %basedir%\sql\%dbname%.sql
+%mysqldir%/mysql -u %mysqluser% -p%mysqlpassword% < %basedir%\sql\%dbname%.sql
 
 @ECHO Finished insert!
