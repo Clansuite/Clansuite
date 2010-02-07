913702c3bccacd24447a03ec618f2bbf10d97c04
xsign <xsign@34db09b4-2314-0410-a940-aec02a7c051d>
2010-02-07 21:38:02 +0000

    minor hotfixes
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@4073 34db09b4-2314-0410-a940-aec02a7c051d

--- modules/news/news.config.php
+++ modules/news/news.config.php
@@ -1,8 +1,13 @@
 ; <?php die( 'Access forbidden.' ); /* DO NOT MODIFY THIS LINE! ?>
 ; 
 ; Clansuite Configuration File : 
-; D:\xampplite\htdocs\work\clansuite\trunk\modules\news\news.config.php 
+; K:\Clansuite\webserver\htdocs\clansuite\modules\news\news.config.php 
 ;
+
+
+;----------------------------------------
+; news
+;----------------------------------------
 [news]
 resultsPerPage_show = 3
 items_newswidget = 5
@@ -11,12 +16,18 @@ resultsPerPage_archive = 3
 feed_format = "RSS2.0"
 feed_items = 20
 resultsPerPage_adminshow = 10
-
 feed_title = "ClanSuite Newsfeed"
 feed_description = "ClanSuite | A new fast & flexible CMS for Clans."
 feed_image = "/uploads/noimage.png"
 feed_imagetitle = "Logo"
 feed_imagedescription = "Klick mich"
 feed_imageurl = "http://www.clansuite-dev.com"
+resultsPerPage_show = 3
+items_newswidget = 5
+resultsPerPage_fullarchive = 3
+resultsPerPage_adminshow = 10
+resultsPerPage_archive = 3
+feed_format = "RSS2.0"
+feed_items = 20
 
 ; DO NOT REMOVE THIS LINE */ ?>
