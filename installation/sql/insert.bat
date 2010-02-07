94dcf5004cfb28ead1e9106ecefb209965473628
xsign <xsign@34db09b4-2314-0410-a940-aec02a7c051d>
2010-02-07 16:09:01 +0000

    updated insert script for better usage and visibility also checks for database existence
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@4067 34db09b4-2314-0410-a940-aec02a7c051d

--- installation/sql/insert.bat
+++ installation/sql/insert.bat
@@ -8,22 +8,34 @@
 
 @ECHO OFF
 
-@REM Verzeichnisse 
+SetLocal
+
+REM Verzeichnisse 
 SET basedir=%CLANSUITE%\installation
 SET mysqldir=%XAMPP%\mysql\bin
 SET mysqluser=root
 SET mysqlpassword=toop
 SET dbname=clansuite
 
-@REM Get timestamp
+REM Get timestamp
 FOR /F "tokens=1,2,3,4 delims=/. " %%a in ('date/T') do set CDATE=%%a-%%b-%%c
 FOR /F "tokens=1,2 delims=/: " %%a in ('time/T') do set CTIME=%%a-%%b
 
-@REM Clansuite DB insert
+REM Clansuite DB insert
 
-@ECHO Creating backup of %dbname% as \sql\%dbname%_%CDATE%_%CTIME%_backup.sql ...
-%mysqldir%/mysqldump -u %mysqluser% -p%mysqlpassword% --skip-add-locks --add-drop-table --databases %dbname% >  %basedir%\sql\backup\%dbname%_%CDATE%_%CTIME%_backup.sql
-@ECHO Beginning insert of \sql\%dbname%.sql ...
-%mysqldir%/mysql -u %mysqluser% -p%mysqlpassword% < %basedir%\sql\%dbname%.sql
+REM Check if db exists
+ECHO select 1; exit; | %mysqldir%\mysqltest.exe -uroot -ptoop -Dclansuite -s >nul 2>&1
+if "%ERRORLEVEL%"=="1" GOTO INSERT
+ECHO  Creating backup of database %dbname%
+ECHO  File: %~dp0backup\%dbname%_%CDATE%_%CTIME%_backup.sql
+%mysqldir%\mysqldump -u %mysqluser% -p%mysqlpassword% --skip-add-locks --add-drop-table --databases %dbname% >  %basedir%\sql\backup\%dbname%_%CDATE%_%CTIME%_backup.sql
 
-@ECHO Finished insert!
+REM Insert SQL Code
+:INSERT
+ECHO  Beginning insert of SQL
+ECHO  File: %~dp0%dbname%.sql
+%mysqldir%\mysql -u %mysqluser% -p%mysqlpassword% < %basedir%\sql\%dbname%.sql
+ECHO  Finished SQL insert!
+
+:END
+EndLocal
