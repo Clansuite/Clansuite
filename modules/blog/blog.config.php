05904dbb99f6d7507cf6daa4cd45a532eda454e9
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2010-03-23 00:12:47 +0000

    - removed bloat
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@4294 34db09b4-2314-0410-a940-aec02a7c051d

--- modules/blog/blog.config.php
+++
@@ -1,9 +0,0 @@
-; <?php die( 'Access forbidden.' ); /* DO NOT MODIFY THIS LINE! ?>
-;
-; Clansuite Configuration File for Module Blog
-;
-[blog]
-
-items_resultsPerPage = 5
-
-; DO NOT REMOVE THIS LINE */ ?>
