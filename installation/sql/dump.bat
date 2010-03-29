bb77e083b61c071012fb1457b30499c7c7f39ba2
Jens-André Koch <jakoch@web.de>
2010-03-29 16:50:00 +0000

    - reverted to old revision

--- installation/sql/dump.bat
+++ installation/sql/dump.bat
@@ -1,7 +1,6 @@
 @REM   ,------------------------------------.
-@REM   | QUICK MYSQL DB INSERT via BATCH    |
+@REM   | QUICK MYSQL DB DUMP via BATCH      |
 @REM   | by Jens-Andre Koch (jakoch@web.de) |
-@REM   | by F. Wolf (xsign.dll@gmail.com)   |
 @REM   | for clansuite.com                  |
 @REM   | LICENCE: BSD                    <3 |
 @REM   `------------------------------------'
@@ -9,19 +8,16 @@
 @ECHO OFF
 
 @REM Verzeichnisse 
-SetLocal
-
-SET basedir="%CLANSUITE%\installation"
-SET mysqldir="%XAMPP%\mysql\bin"
+SET basedir=D:\xampplite\htdocs\work\clansuite\trunk\installation
+SET mysqldir=D:\xampplite\mysql\bin
 SET mysqluser=root
-SET mysqlpassword=toop
+SET mysqlpassword=
 SET dbname=clansuite
 
-@REM Dump clansuite
+@REM Clansuite DB dumpen
+
 @ECHO Beginning backup of %dbname%...
-%mysqldir%\mysqldump -u %mysqluser% -p%mysqlpassword% --skip-add-locks --add-drop-table --databases %dbname% > %basedir%\sql\%dbname%.sql
+%mysqldir%/mysqldump -u %mysqluser% -p%mysqlpassword% --skip-add-locks --add-drop-table --databases %dbname% > %basedir%\sql\%dbname%.sql
 
 @ECHO Finished backup!  - Press any Key -
 pause
-
-EndLocal
