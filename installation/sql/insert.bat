72c22aa3115b579b7cc520171ea150a439c06551
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2010-03-29 16:50:00 +0000

    - reverted to old revision
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@4338 34db09b4-2314-0410-a940-aec02a7c051d

--- installation/sql/insert.bat
+++ installation/sql/insert.bat
@@ -1,41 +1,28 @@
 @REM   ,------------------------------------.
 @REM   | QUICK MYSQL DB INSERT via BATCH    |
 @REM   | by Jens-Andre Koch (jakoch@web.de) |
-@REM   | by F. Wolf (xsign.dll@gmail.com)   |
 @REM   | for clansuite.com                  |
 @REM   | LICENCE: BSD                    <3 |
 @REM   `------------------------------------'
 
 @ECHO OFF
 
-SetLocal
-
-REM Verzeichnisse 
-SET basedir=%CLANSUITE%\installation
-SET mysqldir=%XAMPP%\mysql\bin
-SET mysqluser=root
+@REM Verzeichnisse 
+SET basedir=D:\xampplite\htdocs\work\clansuite\trunk\installation
+SET mysqldir=D:\xampplite\mysql\bin
+SET mysqluser=clansuite
 SET mysqlpassword=toop
 SET dbname=clansuite
 
-REM Get timestamp
+@REM Get timestamp
 FOR /F "tokens=1,2,3,4 delims=/. " %%a in ('date/T') do set CDATE=%%a-%%b-%%c
 FOR /F "tokens=1,2 delims=/: " %%a in ('time/T') do set CTIME=%%a-%%b
 
-REM Clansuite DB insert
+@REM Clansuite DB insert
 
-REM Check if db exists
-ECHO select 1; exit; | %mysqldir%\mysqltest.exe -u%mysqluser% -p%mysqlpassword% -D%dbname% -s >nul 2>&1
-if "%ERRORLEVEL%"=="1" GOTO INSERT
-ECHO  Creating backup of database %dbname%
-ECHO  File: %~dp0backup\%dbname%_%CDATE%_%CTIME%_backup.sql
-%mysqldir%\mysqldump -u %mysqluser% -p%mysqlpassword% --skip-add-locks --add-drop-table --databases %dbname% >  %basedir%\sql\backup\%dbname%_%CDATE%_%CTIME%_backup.sql
+@ECHO Creating backup of %dbname% as \sql\%dbname%_%CDATE%_%CTIME%_backup.sql ...
+%mysqldir%/mysqldump -u %mysqluser% -p%mysqlpassword% --skip-add-locks --add-drop-table --databases %dbname% >  %basedir%\sql\backup\%dbname%_%CDATE%_%CTIME%_backup.sql
+@ECHO Beginning insert of \sql\%dbname%.sql ...
+%mysqldir%/mysql -u %mysqluser% -p%mysqlpassword% %dbname% < %basedir%\sql\%dbname%.sql
 
-REM Insert SQL Code
-:INSERT
-ECHO  Beginning insert of SQL
-ECHO  File: %~dp0%dbname%.sql
-%mysqldir%\mysql -u %mysqluser% -p%mysqlpassword% < %basedir%\sql\%dbname%.sql
-ECHO  Finished SQL insert!
-
-:END
-EndLocal
+@ECHO Finished insert!
