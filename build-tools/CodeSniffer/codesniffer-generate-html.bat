35dcb439dece57d84022d86281af8ccf648d1dd8
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2010-04-17 13:09:43 +0000

    - clean ups + removed shell scripts for trac's ci bitten
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@4346 34db09b4-2314-0410-a940-aec02a7c051d

--- build-tools/CodeSniffer/codesniffer-generate-html.bat
+++
@@ -1,9 +0,0 @@
-@REM   ,------------------------------------.
-@REM   | Clansuite Build-Scripts        w32 |
-@REM   | by Jens-André Koch (jakoch@web.de) |
-@REM   | http://www.clansuite.com           |
-@REM   | LICENCE: GPLv2 any later version   |
-@REM   `------------------------------------'
-@REM SVN: $Id$
-
-START /MAX D:\xampplite\php\phpcs.bat --generator=html >> d:\xampplite\htdocs\work\clansuite\trunk\build-tools\codesniffer-report.html
