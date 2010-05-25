171dba03c22cb0b5e286dc2b7259c77859a186d1
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2010-05-25 17:27:56 +0000

    - added missing parameter default value to redirect()
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@4462 34db09b4-2314-0410-a940-aec02a7c051d

--- core/httpresponse.core.php
+++ core/httpresponse.core.php
@@ -376,7 +376,7 @@ class Clansuite_HttpResponse implements Clansuite_Response_Interface
      * @param text   text of redirect message
      * @param string redirect mode LOCATION, REFRESH, JS, HTML
      */
-    public static function redirect($url, $time = 0, $statusCode = 302, $message = '', $mode = null )
+    public static function redirect($url, $time = 0, $statusCode = 302, $message = null, $mode = null )
     {
         $linenum = '';
         $redirect_html = '';
@@ -410,7 +410,7 @@ class Clansuite_HttpResponse implements Clansuite_Response_Interface
                     break;
             }
 
-            if($mode == 'JS' or $mode == 'HTML')
+            if(empty($redirect_html) === false)
             {
                 # self::addHeader('Location', $url);
                 self::setContent($redirect_html, $time, htmlspecialchars($url, ENT_QUOTES, 'UTF-8'));
@@ -419,7 +419,7 @@ class Clansuite_HttpResponse implements Clansuite_Response_Interface
             /**
              * Set flashmessage on redirect
              */
-            if(isset($message))
+            if(empty($message) === false )
             {
                 # @todo handle type when $message is "type:message text"
                 #$array = explode(':', $message);
