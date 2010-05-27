906aec61c6680d9601199e987994a595d1744a9d
Jens-André Koch <jakoch@web.de>
2010-05-27 18:01:44 +0000

    - added smarty viewhelper for google analytics (implemented two loading strategies: the new asynchronous call by GA itself and a cached jquery ajax loading strategy)

---
+++ core/viewhelper/smarty/function.googleanalytics.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Smarty plugin
+ * @package Smarty
+ * @subpackage plugins
+ */
+
+/**
+ * Google Analytics Plugin
+ * Generate XHTML 1.1 valid Google Analytics code
+ *
+ * Name:     google_analytics<br>
+ * Date:     26.Mai 2010.<br>
+ *
+ * Examples:
+ * <pre>
+ * {googleanalytics code=UA-xxxxxx-x type=jquery-load}
+ * </pre>
+ *
+ * @link http://code.google.com/intl/de-DE/apis/analytics/docs/tracking/asyncMigrationExamples.html
+ *
+ * @param array $params code parameter required
+ * @param Smarty $smarty
+ * @return string
+ */
+function smarty_function_googleanalytics($params, &$smarty)
+{
+    # get the google analytics code to insert it later on into the script
+    if(empty($params['code']))
+    {
+        # fallback to config, if nothing was given
+        $config = Clansuite_CMS::getInjector('Clansuite_Config');
+        $google_id = $config->getConfigValue('googleanalytics_id');
+
+        if(empty($google_id) == false)
+        {
+
+        }
+        else # no code provided via smarty function nor config
+        {
+            $smarty->trigger_error("google_analytics: the parameter 'code' is missing. please specifiy your GA urchin id.");
+            return;
+        }
+    }
+    else
+    {
+        $google_id = $params['code'];
+    }
+
+    /**
+     * Determine the type of script to include
+     * a) asynchronous script snippet
+     * b) jquery cache snippet
+     */
+    if(empty($params['type']))
+    {
+        $smarty->trigger_error("google_analytics: the parameter 'type' is missing. please specifiy your 'async-load' or 'jquery-load'.");
+        return;
+    }
+
+    if('async' == $params['type'])
+    {
+        # asynchronous ga script
+        $return = '
+            <script type="text/javascript">
+            // <![CDATA[
+            <!-- asynchronous push -->
+            var _gaq = _gaq || [];
+            _gaq.push([\'_setAccount\', \''.$google_id.'\']);
+            _gaq.push([\'_trackPageview\']);
+            <!-- ga object call by inserting it into the page -->
+            (function() {
+              var ga = document.createElement(\'script\');
+                ga.type = \'text/javascript\';
+                ga.async = true;
+                ga.src = (\'https:\'== document.location.protocol ? \'https://ssl\':\'http://www\') + \'.google-analytics.com/ga.js\';
+                var s = document.getElementsByTagName(\'script\')[0];
+                s.parentNode.insertBefore(ga, s);
+            })();
+            // ]]>
+            </script>';
+      }
+
+      if('jquery-load' == $params['type'])
+      {
+          # asynchronous and cached loading via jquery ajax
+          $return = '
+              <script type="text/javascript">
+              // <![CDATA[
+              $(document).ready(function(){$.ajax({
+                      type: \'GET\',
+                      url: \'http://www.google-analytics.com/ga.js\',
+                      dataType: \'script\',
+                      cache: true,
+                      success: function() {
+                          var pageTracker = _gat._getTracker(\''.$google_id.'\');
+                          pageTracker._trackPageview();
+                        }
+                    });
+            });
+            // ]]>
+            </script>';
+      }
+
+      return $return;
+}
+?>
