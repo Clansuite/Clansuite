b8bcbd2f78552ef0ab0560b4b87ea8c2ace496cf
Jens-André Koch <jakoch@web.de>
2010-07-05 15:28:48 +0000

    - updated gettext to 1.0.10

--- libraries/php-gettext/streams.php
+++ libraries/php-gettext/streams.php
@@ -103,13 +103,12 @@ class FileReader {
   }
 
   function read($bytes) {
-
     if ($bytes) {
       fseek($this->_fd, $this->_pos);
 
       // PHP 5.1.1 does not read more than 8192 bytes in one fread()
       // the discussions at PHP Bugs suggest it's the intended behaviour
-      $data = null;
+      $data = '';
       while ($bytes > 0) {
         $chunk  = fread($this->_fd, $bytes);
         $data  .= $chunk;
