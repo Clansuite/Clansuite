ed2cbdbe30d59a6b98084700d4f1c8321d388349
Jens-André Koch <jakoch@web.de>
2010-07-10 12:03:20 +0000

    - doctrine models are convertable to yaml <-> sql <-> db and vice versa

--- modules/doctrine/controller/doctrine.admin.php
+++ modules/doctrine/controller/doctrine.admin.php
@@ -71,58 +71,214 @@ if(defined('IN_CS') == false)
  */
 class Clansuite_Module_Doctrine_Admin extends Clansuite_Module_Controller implements Clansuite_Module_Interface
 {
-    protected $doctrine_cli;
+    /**
+     * @var array Doctrine Paths to models, yaml, sql, fixtures, migration data
+     */
+    private static $options;
 
     /**
      * Module_Doctrine_Admin -> Execute
      */
     public function initializeModule(Clansuite_HttpRequest $request, Clansuite_HttpResponse $response)
     {
-        # Setup the path constants
-        # They are not configured via clansuite configuration, because access is only needed in here
-        define('MODELS_PATH',        ROOT . DS . 'models');
-        define('YAML_SCHEMA_PATH',   ROOT . DS . 'doctrine' . DS . 'schema');
-        define('SQL_PATH',           ROOT . DS . 'doctrine' . DS . 'sql');
-        define('DATA_FIXTURES_PATH', ROOT . DS . 'doctrine' . DS . 'fixtures');
-        define('MIGRATIONS_PATH',    ROOT . DS . 'doctrine' . DS . 'migrations');
+        Doctrine_Core::setPath( ROOT_LIBRARIES . 'doctrine/' );
 
-        # Setup Configuration Array for the Doctrine Command Line Interface (CLI)
+        /**
+         * Setup Paths depending on a specific module or the core
+         */
+        $bool = $request->issetParameter('modulename');
+        if(isset($bool))
+        {
+            self::$options = self::setupDoctrinePaths($request->getParameterFromGet('modulename'));
+        }
+        else
+        {
+            self::$options = self::setupDoctrinePaths();
+        }
+
+        self::$options += self::getDoctrineOptions();
+    }
+
+    /**
+     * Returns the doctrine paths array for models, yaml, sql, fixtures, migration data.
+     *
+     * You can select between paths for the core or for a module via parameter.
+     * The core path is basically ROOT.'doctrine', while the module path is ROOT_MOD.
+     * $modulename followed by the specific data dir.
+     *
+     * This is also useable as configuration array for the Doctrine Command Line Interface (CLI).
+     *
+     * @param string $modulename The modulename to build the paths for. Defaults to ROOT.doctrine
+     * @return array Doctrine Config Array with path definitions.
+     */
+    public static function setupDoctrinePaths($modulename = null)
+    {
+        # core = default path
+        $path = ROOT . 'doctrine' . DS;
+
+        # module
+        if($modulename != null)
+        {
+            $path = ROOT_MOD . $modulename . DS . 'model' . DS;
+        }
+
+        # compose the paths accordingly
         $config = array(
-                        'models_path'         =>  MODELS_PATH,
-                        'yaml_schema_path'    =>  YAML_SCHEMA_PATH,
-                        'sql_path'            =>  SQL_PATH,
-                        'data_fixtures_path'  =>  DATA_FIXTURES_PATH,
-                        'migrations_path'     =>  MIGRATIONS_PATH
+                        'models_path'        => $path . 'models',
+                        'yaml_schema_path'   => $path,
+                        'sql_path'           => $path . 'sql',
+                        'data_fixtures_path' => $path . 'fixtures',
+                        'migrations_path'    => $path . 'migrations'
                        );
 
+        # declare path constants
+        define('DOCTRINE_MODELS_PATH',        $config['models_path']);
+        define('DOCTRINE_YAML_SCHEMA_PATH',   $config['yaml_schema_path']);
+        define('DOCTRINE_SQL_PATH',           $config['sql_path']);
+        define('DOCTRINE_DATA_FIXTURES_PATH', $config['data_fixtures_path']);
+        define('DOCTRINE_MIGRATIONS_PATH',    $config['migrations_path']);
+
+        unset($path);
+
+        return $config;
+    }
+
+    /**
+     * Doctrine Configuration Array for the Generation of Models.
+     *
+     * This is also useable as configuration array for the Doctrine Command Line Interface (CLI).
+     */
+    public static function getDoctrineOptions()
+    {
         # Setup Configuration Array for the Generation of Models
         $config['generate_models_options'] = array(
-                        # PHP Documentor Comment Settings
+                        /**
+                         *  PHP Documentor Comment Settings
+                         */
                         'phpDocPackage'         => 'Doctrine',
                         'phpDocSubpackage'      => 'Records',
-                        'phpDocName'            => 'Jens-André Koch',
+                        'phpDocName'            => 'Jens-Andre Koch',
                         'phpDocEmail'           => 'vain@clansuite.com',
-                        # Classnames and Filenames
-                        'generateBaseClasses'   => true,                        # generate BaseClasses too
+                        /**
+                         * Classname and Filename Settings
+                         */
+                        # generate BaseClasses too
+                        'generateBaseClasses'   => true,
                         #'baseClassPrefix'       => 'Clansuite_ModelsBase_',
                         'baseClassPrefix'       => 'Base_',
                        #'baseClassName'         => '',
                         'baseClassesDirectory'  => 'records',
-                        'generateTableClasses'  => true,                        # generate Tables and Records
+                        'generateTableClasses'  => true,              # generate Tables and Records
                         'baseTableClassName'    => '',
-                        'classPrefix'           => 'Modules_Models_',           # determines the pear style nameing convention for autoload
+                        'classPrefix'           => 'Modules_Models_', # determines the pear style nameing convention for autoload
                         'classPrefixFiles'      => true,
                         'pearStyle'             => true,
                         'suffix'                => '.model.php'
-                );
+                    );
 
-        # Apply settings to the Doctrine_CLI object on instantiation and set it to class for later usage
-        $this->doctrine_cli = new Doctrine_Cli($config);
+        return $config;
     }
 
     public function action_admin_show()
     {
-        $this->display();
+        $this->prepareOutput();
+    }
+
+    /**
+     * db2models
+     *
+     * This generates Models from Database.
+     *
+     * @see Doctrine_Core::generateModelsFromDb
+     */
+    public function action_admin_db2models()
+    {
+        Doctrine_Core::generateModelsFromDb( DOCTRINE_MODELS_PATH , array(), self::$options);
+        $message  = '<b>The MODEL files have been generated from DATABASE.</b>';
+        $message .= '<br /><br />Destination Folder: '. DOCTRINE_MODELS_PATH;
+        $this->setFlashmessage('success', $message);
+        $this->redirectToReferer();
+    }
+
+    /**
+     * db2yaml
+     *
+     * This generates YAML schema files from Database.
+     */
+    public function action_admin_db2yaml()
+    {
+        # load sfYAML (it's somehow not loaded by doctrine autoload)
+        require_once ROOT_LIBRARIES .'doctrine/Doctrine/Parser/sfYaml/SfYaml.php';
+        Doctrine_Core::generateYamlFromDb( DOCTRINE_YAML_SCHEMA_PATH . 'schema.yml', array(), self::$options);
+        $message  = '<b>The YAML schema file has been successfully generated from DATABASE.</b>';
+        $message .= '<br /><br />Destination File: ' . DOCTRINE_YAML_SCHEMA_PATH . 'schema.yml';
+        $this->setFlashmessage('success', $message);
+        $this->redirectToReferer();
+    }
+
+    /**
+     * model2sql
+     * This generates Sql From Models.
+     *
+     * @see Doctrine_Core::generateSqlFromModels()
+     */
+    public function action_admin_models2sql()
+    {
+        $sql = Doctrine_Core::generateSqlFromModels();
+        $sqlSchemaFile = DOCTRINE_SQL_PATH . DS . DB_PREFIX . '_sqlschema_backup_'.date('dmY_His').'.sql';
+        file_put_contents($sqlSchemaFile, $sql);
+        $message  = 'Successfully written SQL for the current schema to disc.';
+        $message .= 'Destination File: '.$sqlSchemaFile;
+        unset($sql);
+        unset($sqlSchemaFile);
+        $this->setFlashmessage('success', $message);
+        $this->redirectToReferer();
+    }
+
+    /**
+     * model2yaml
+     *
+     * This generates YAML schema files from MODELS
+     * This generates TABLES from MODELS.
+     *
+     * @see Doctrine_Core::generateSqlFromYaml()
+     */
+    public function action_admin_models2yaml()
+    {
+        # load sfYAML (it's somehow not loaded by doctrine autoload)
+        require_once ROOT_LIBRARIES .'doctrine/Doctrine/Parser/sfYaml/SfYaml.php';
+        #Doctrine_Core::dropDatabases();
+        #Doctrine_Core::createDatabases();
+        Doctrine_Core::generateYamlFromModels( DOCTRINE_YAML_SCHEMA_PATH . DS . 'schema.yml', DOCTRINE_MODELS_PATH );
+        #Doctrine_Core::createTablesFromModels( DOCTRINE_MODELS_PATH );
+        $message  = '<b>The YAML schema file has been successfully generated from MODELS.</b>';
+        $message .= '<br /><br />Destination File: ' . DOCTRINE_YAML_SCHEMA_PATH . 'schema.yml';
+        #$message .= '<br /><br />The DATABASE TABLES have been successfully generated from MODELS.';
+        $this->setFlashmessage('success', $message);
+        $this->redirectToReferer();
+    }
+
+    /**
+     * yaml2models
+     *
+     * This generates MODELS from YAML schema files.
+     * This generates TABLES from MODELS.
+     *
+     * @see Doctrine_Core::generateModelsFromYaml
+     */
+    public function action_admin_yaml2models()
+    {
+        # load sfYAML (it's somehow not loaded by doctrine autoload)
+        require_once ROOT_LIBRARIES .'doctrine/Doctrine/Parser/sfYaml/SfYaml.php';
+        #Doctrine_Core::dropDatabases();
+        #Doctrine_Core::createDatabases();
+        Doctrine_Core::generateModelsFromYaml( DOCTRINE_YAML_SCHEMA_PATH . DS . 'schema.yml', DOCTRINE_MODELS_PATH, self::$options );
+        #Doctrine_Core::createTablesFromModels( DOCTRINE_MODELS_PATH );
+        $message  = '<b>The Models have been successfully generated from the YAML schema file.</b>';
+        $message .= '<br /><br />Destination Folder ' . DOCTRINE_MODELS_PATH;
+        #$message .= '<br /><br />The DATABASE TABLES have been successfully generated from MODELS.';
+        $this->setFlashmessage('success', $message);
+        $this->redirectToReferer();
     }
 }
 ?>
--- modules/doctrine/view/action_admin_show.tpl
+++ modules/doctrine/view/action_admin_show.tpl
@@ -1,18 +1,28 @@
-<!-- [-Start-] Included Template c:andreclansuitetrunkmodulestemplatemanagertemplatescreate_new_template.tpl -->
-<!-- Start of Template - modules/doctrine/view/action_admin_show.tpl -->
-
-
-{* 
-    This is the DEBUG OUTPUT of assigned variables or arrays:
-    {$variablename|@var_dump}
-*}
-
 {modulenavigation}
+<div class="ModuleHeading">{t}Doctrine - Overview{/t}</div>
+<div class="ModuleHeadingSmall">{t}You can execute Doctrine commands to manage (import, export, backup) your records.{/t}</div>
 
->>> PLEASE INSERT YOUR TEMPLATE CONTENT HERE
+For the understanding of this module 3 things a very important.
+1) The schema files are configuration files describing your database structure.
+2) The models are php files. They represent object implementing the active record pattern.
+   Your data is interacting with these objects.
+3) The database itself stored via the mysql server daemon.
+If you update one of these things manually, you have to update the other two
+things to keep them synchronized.
 
-A good start point is to modify your action to assign $variablename to the view.
-For Smarty the command is $smarty->assign('variablename', $variablecontent);
+<h3>{t}Tasks{/t}</h3>
 
-<!-- End of Template - modules/doctrine/view/action_admin_show.tpl -->
-<!-- [-End-] Included Template c:andreclansuitetrunkmodulestemplatemanagertemplatescreate_new_template.tpl  -->
+<h4>1. DB &raquo; Models</h4>
+This will generate Models files from your Database.
+
+<h4>2. DB &raquo; YAML</h4>
+This will generate YAML schema files from your Database.
+
+<h4>3. Models &raquo; SQL</h4>
+This will generate a SQL schema file from your Database.
+
+<h4>4. Models &raquo; YAML</h4>
+This will generate YAML schema files from your Models.
+
+<h4>5. YAML &raquo; Models</h4>
+This will generate YAML from Models.
