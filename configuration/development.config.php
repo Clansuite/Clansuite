740c9de51f83cde84f6d680bebae9c3ee3d313e4
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2010-09-17 18:38:43 +0000

    - moved [login] section from clansuite main config to account module config
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@4644 34db09b4-2314-0410-a940-aec02a7c051d

--- configuration/development.config.php
+++ configuration/development.config.php
@@ -75,19 +75,6 @@ timezone = "Europe/Berlin"
 gmtoffset = 3600
 
 ;----------------------------------------
-; login
-;----------------------------------------
-[login]
-login_method = "nick"
-remember_me_time = 90
-max_login_attempts = 5
-login_ban_minutes = 30
-min_pass_length = 6
-hash_algorithm = "sha1"
-salt = "1-3-5-8-4-1-7-2-4-1-4-1"
-registration_term = ""
-
-;----------------------------------------
 ; openid
 ;----------------------------------------
 [openid]
--- modules/account/account.config.php
+++ modules/account/account.config.php
@@ -6,4 +6,17 @@
 [account]
 captchatype = recaptcha
 
+;----------------------------------------
+; login
+;----------------------------------------
+[login]
+login_method = "nick"
+remember_me_time = 90
+max_login_attempts = 5
+login_ban_minutes = 30
+min_pass_length = 6
+hash_algorithm = "sha1"
+salt = "1-3-5-8-4-1-7-2-4-1-4-1"
+registration_term = ""
+
 ; DO NOT REMOVE THIS LINE */ ?>
