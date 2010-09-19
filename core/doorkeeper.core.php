2e4c7ceec643b9b3cd30d0b499c33889ca55d1c8
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2010-09-19 11:40:48 +0000

    - create php_ids cache file if not existant
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@4657 34db09b4-2314-0410-a940-aec02a7c051d

--- core/doorkeeper.core.php
+++ core/doorkeeper.core.php
@@ -80,7 +80,20 @@ class Clansuite_DoorKeeper
             define( 'IDS_FILTER_PATH',  ROOT_LIBRARIES . 'IDS/default_filter.xml');
             define( 'IDS_TMP_PATH',     ROOT_CACHE);
             define( 'IDS_LOG_PATH',     ROOT . 'logs/phpids_log.txt');
-            define( 'IDS_CACHE_PATH',   ROOT_CACHE . 'default_filter.cache');
+            define( 'IDS_CACHE_PATH',   ROOT_CACHE . 'phpids_defaultfilter.cache');
+
+            # the following lines have to remain, till PHP_IDS team fixes their lib
+            # in order to create the cache file automatically
+            if(false === is_file(IDS_CACHE_PATH))
+            {
+                $filehandle = @fopen(IDS_CACHE_PATH, 'w+');
+                if(false === $filehandle)
+                {
+                    throw new Clansuite_Exception('PHP IDS Cache file couldn\'t be created.', 11);
+                }
+                fclose($filehandle);
+            }
+
 
             # Initialize the System with the configuration values
             $init = IDS_Init::init( ROOT_CONFIG . 'phpids_config.ini');
