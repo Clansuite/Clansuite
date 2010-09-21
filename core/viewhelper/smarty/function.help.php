4819430a4e8a21ce1403f8ed1cc33349938e54c9
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2010-09-21 14:44:23 +0000

    - fixed paths on smarty viewhelper help - added magic methods __unset() __isset() to smarty.renderer.php
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@4704 34db09b4-2314-0410-a940-aec02a7c051d

--- core/viewhelper/smarty/function.help.php
+++ core/viewhelper/smarty/function.help.php
@@ -29,15 +29,15 @@ function smarty_function_help($params, $smarty)
     $modulename = $smarty->getTemplateVars('template_of_module');
     
     # check if file exists
-    if( $smarty->templateExists( $modulename. '/view/help.tpl') )
+    if( $smarty->templateExists( $modulename. '/view/smarty/help.tpl') )
     {
         # load the help template from modulepath ->  modulename/view/help.tpl
-        return $smarty->fetch( $modulename. '/view/help.tpl');
+        return $smarty->fetch( $modulename. '/view/smarty/help.tpl');
     }
-    elseif( DEBUG == true and DEVELOPMENT == true )
+    /*elseif( DEBUG == true and DEVELOPMENT == true )
     {
         return $smarty->fetch( ROOT_THEMES . 'core/view/help_not_found.tpl');
-    }
+    }*/
     else
     {
         return 'Help Template not found.';
