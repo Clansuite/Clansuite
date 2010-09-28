6e6a76299ccb0b96bed73580d7b8b5080d8e590e
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2010-09-28 11:52:57 +0000

    - added smarty viewhelper a
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@4745 34db09b4-2314-0410-a940-aec02a7c051d

---
+++ core/viewhelper/smarty/function.a.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Smarty plugin
+ * @package Smarty
+ * @subpackage Clansuite Plugins / Smarty View Helper
+ */
+
+/**
+ * URL Generator for Smarty Templates
+ *
+ * Examples:
+ * {a href="/news/show"}
+ *
+ * Type:     function<br>
+ * Name:     a<br>
+ * Purpose:  Generates the proper URL from the href parameter given<br>
+ *
+ * @author   Jens-André Koch <vain@clansuite.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GPL 2 / any later version
+ * @param array $params
+ * @param Smarty $smarty
+ * @return string
+ */
+function smarty_function_a($params, $smarty)
+{
+    # method parameter "href"
+    if(empty($params['href']))
+    {
+        $errormessage  = 'You are using the <font color="#FF0033">{a}</font> command, but the <font color="#FF0033">Parameter "href" is missing.</font>';
+        $errormessage .= ' Try to append the parameter in the following way: <font color="#66CC00">href="/news/show"</font>.';
+        $smarty->trigger_error($errormessage);
+        return;
+    }
+    else
+    {
+        # convert from internal slashed format to URL
+        return Clansuite_Router::buildURL($params['href']);
+    }
+}
+?>
