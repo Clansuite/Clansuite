a59403bf1da3282475db266f3d6c2c38cfd20d97
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2010-10-04 18:28:31 +0000

    - changes to gettext extractor - added structure for languages module
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@4760 34db09b4-2314-0410-a940-aec02a7c051d

---
+++ modules/languages/languages.info.php
@@ -0,0 +1,10 @@
+; <?php die('Access forbidden.'); /* DO NOT MODIFY THIS LINE! ?>
+; 
+; Clansuite Configuration File : 
+; D:\xampplite\htdocs\work\clansuite\trunk\modules\languages\languages.info.php
+; 
+; This file was generated on 22-09-2010 16:44
+;
+
+
+; DO NOT REMOVE THIS LINE */ ?>
