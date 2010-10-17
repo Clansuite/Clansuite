4014364ea14a2e20569ad2772f114aaadda2bec5
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2010-10-17 15:20:04 +0000

    - adjustments and cleanup of codemirror config - note: line-wrapping is enabled now
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@4805 34db09b4-2314-0410-a940-aec02a7c051d

--- themes/core/javascript/codemirror_config.js
+++ themes/core/javascript/codemirror_config.js
@@ -2,8 +2,8 @@
  * @file       /themes/core/javascript/codemirror_config.js
  * @desc       CodeMirror Configuration File for "Clansuite - just an eSports CMS"
  * @license    GNU/GPL v2 or (at your option) any later version, see "/doc/LICENSE".
- * @author     Jens-AndrÃ© Koch <vain@clansuite.com>
- * @copyright  Copyleft: All rights reserved. Jens-AndrÃ© Koch (2005 - onwards)
+ * @author     Jens-André Koch <vain@clansuite.com>
+ * @copyright  Copyleft: All rights reserved. Jens-André Koch (2005 - onwards)
  * @link       http://www.clansuite.com
  * @version    SVN: $Id$
  */
@@ -17,9 +17,6 @@ $(document).ready(function() {
      */
 
     var editor = CodeMirror.fromTextArea("codecontent", {
-    //var textarea = document.getElementById('codecontent');
-    //var editor = new MirrorFrame(CodeMirror.replace(textarea), {
-      //content: textarea.value,
 
       // The path parameter specifies the location where the script will find the parser and tokenizers files.
       path: "libraries/codemirror/js/",
@@ -33,17 +30,16 @@ $(document).ready(function() {
                    "tokenizephp.js", "parsephp.js",
                    "parsephphtmlmixed.js"],
 
-      //parserfile: ["parsexml.js", "parsecss.js", "tokenizejavascript.js", "parsejavascript.js", "parsehtmlmixed.js"],
-
       // The stylesheet parameter defines the CSS to pull in for each corresponding parser.
       stylesheet: ["libraries/codemirror/css/xmlcolors.css",
                    "libraries/codemirror/css/jscolors.css",
                    "libraries/codemirror/css/csscolors.css"],
 
       autoMatchParens: true,
-      width: '100%',
-      height: '480px',
-      textWrapping: false,
+      width: '98%',
+      height: 'dynamic',
+      disableSpellcheck: true,
+      textWrapping: true,
       lineNumbers: true,
       tabMode: 'spaces',
       iframeClass: 'ifc',
