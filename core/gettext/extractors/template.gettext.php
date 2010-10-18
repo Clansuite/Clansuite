d85397b5335d7c70a6721bf5a648179a489859ae
Jens-André Koch <jakoch@web.de>
2010-10-18 00:39:23 +0000

    - added scanning for translation-tags in smarty templates

--- core/gettext/extractors/template.gettext.php
+++ core/gettext/extractors/template.gettext.php
@@ -50,12 +50,17 @@ if(defined('IN_CS') === false)
  */
 class Clansuite_Gettext_Extractor_Template extends Clansuite_Gettext_Extractor_Base implements Clansuite_Gettext_Extractor_Interface
 {
-    /**
-     * @const regex to match the curly bracket syntax
-     */
-    #const REGEX = '#{(__PLACEHOLDERS__)("[^"\\\\]*(?:\\\\.[^"\\\\]*)*"|\'[^\'\\\\]*(?:\\\\.[^\'\\\\]*)*\')+(\|[a-z]+(:[a-z0-9]+)*)*}#u';
-    const REGEX = '#{(__PLACEHOLDERS__)(.*?)+(\|[a-z]+(:[a-z0-9]+)*)*}#u';
+    const L_DELIMITER = '{';
+    const R_DELIMITER = '}';
 
+    /**
+     * This modified regexp is based on "tsmarty2c.php" by
+     * @author Sagi Bashari <sagi@boom.org.il>
+     * @license LGPL v2.1+
+     *
+     * @const regexp to match the smarty curly bracket syntax
+     */
+    const REGEXP = "/__LD__\s*(__TAGS__)\s*([^__RD__]*)__RD__([^__LD__]*)__LD__\/\\1__RD__/";
 
     /**
      * The function tags to extract translation strings from
@@ -73,21 +78,41 @@ class Clansuite_Gettext_Extractor_Template extends Clansuite_Gettext_Extractor_B
      */
     public function extract($file)
     {
-        $pathinfo = pathinfo($file);
+        # load file
+        $filecontent = file($file);
 
+        # ensure we got the filecontent
+        if(empty($filecontent))
+        {
+            return;
+        }
+
+        # ensure we got defined some tags to scan for
         if(false === count($this->tags_to_scan))
         {
             return;
         }
 
+        # init vars
+        $pathinfo = pathinfo($file);
         $data = array();
-        $filecontent = file($file);
 
-        # construct the regular expression pattern
-        $placeholders = join('|', $this->tags_to_scan);
-        $pattern = str_replace('__PLACEHOLDERS__', $placeholders, self::REGEX);
+        /**
+         *  construct the regular expression pattern
+         */
+        # join placeholders for multi-tag scan
+        #$tags = $this->tags_to_scan[0];
+        $tags = join('|', $this->tags_to_scan);
 
-        Clansuite_Debug::firebug($pattern);
+        # setup search/replace arrays
+        $search  = array('__TAGS__', '__LD__', '__RD__');
+        $replace = array($tags, self::L_DELIMITER, self::R_DELIMITER);
+
+        # replace tags and delimiters in regexp pattern
+        $pattern = str_replace($search, $replace, self::REGEXP);
+
+        # debug display pattern
+        # Clansuite_Debug::firebug($pattern);
 
         # parse file by lines
         foreach($filecontent as $line => $line_content)
@@ -95,8 +120,8 @@ class Clansuite_Gettext_Extractor_Template extends Clansuite_Gettext_Extractor_B
             # match all {t ... } or {_ ... } tags if prefixes are "t" and "_"
             preg_match_all($pattern, $line_content, $matches);
 
-            Clansuite_Debug::firebug($line_content);
-            Clansuite_Debug::firebug($matches);
+            #Clansuite_Debug::firebug($line_content);
+            #Clansuite_Debug::firebug($matches);
 
             if(empty($matches))
             {
@@ -108,12 +133,15 @@ class Clansuite_Gettext_Extractor_Template extends Clansuite_Gettext_Extractor_B
                 continue;
             }
 
+            # correct line number, because file[line1] = array[0]
+            $calc_line = 1 + $line;
+
             foreach($matches[2] as $match)
             {
                 $match_arrayname = substr($match, 1, -1);
 
                 # strips trailing apostrophes or double quotes
-                $data[$match_arrayname][] = $pathinfo['basename'] . ':' . $line;
+                $data[$match_arrayname][] = $pathinfo['basename'] . ':' . $calc_line;
 
                 unset($match_arrayname);
             }
