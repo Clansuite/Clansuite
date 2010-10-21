1237d51b6242703a8e53512b0362a6585de4a78c
Jens-André Koch <jakoch@web.de>
2010-10-21 21:33:43 +0000

    - array_key_exists to isset

--- core/httprequest.core.php
+++ core/httprequest.core.php
@@ -593,16 +593,16 @@ class Clansuite_HttpRequest implements Clansuite_Request_Interface, ArrayAccess
      */
     public static function getRemoteAddress()
     {
-        if (array_key_exists('HTTP_CLIENT_IP', $_SERVER) and self::validateIP($_SERVER['HTTP_CLIENT_IP']))
+        if(isset($_SERVER['HTTP_CLIENT_IP']) and self::validateIP($_SERVER['HTTP_CLIENT_IP']))
         {
             return $_SERVER['HTTP_CLIENT_IP'];
         }
 
-        if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER))
+        if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
         {
-            foreach (explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']) as $ip)
+            foreach(explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']) as $ip)
             {
-                if (self::validateIP(trim($ip)))
+                if(self::validateIP(trim($ip)))
                 {
                     return $ip;
                 }
@@ -610,22 +610,22 @@ class Clansuite_HttpRequest implements Clansuite_Request_Interface, ArrayAccess
         }
 
         # NGINX - with natural russian config passes the IP as REAL_IP ;)
-        if (array_key_exists('HTTP_X_REAL_IP', $_SERVER) and self::validateIP($_SERVER['HTTP_X_REAL_IP']))
+        if(isset($_SERVER['HTTP_X_REAL_IP']) and self::validateIP($_SERVER['HTTP_X_REAL_IP']))
         {
             return $_SERVER['HTTP_X_REAL_IP'];
         }
 
-        if (array_key_exists('HTTP_FORWARDED_FOR', $_SERVER) and self::validateIP($_SERVER['HTTP_FORWARDED_FOR']))
+        if(isset($_SERVER['HTTP_FORWARDED_FOR']) and self::validateIP($_SERVER['HTTP_FORWARDED_FOR']))
         {
             return $_SERVER['HTTP_FORWARDED_FOR'];
         }
 
-        if (array_key_exists('HTTP_FORWARDED', $_SERVER) and self::validateIP($_SERVER['HTTP_FORWARDED']))
+        if(isset($_SERVER['HTTP_FORWARDED']) and self::validateIP($_SERVER['HTTP_FORWARDED']))
         {
             return $_SERVER['HTTP_FORWARDED'];
         }
 
-        if (array_key_exists('HTTP_X_FORWARDED', $_SERVER) and self::validateIP($_SERVER['HTTP_X_FORWARDED']))
+        if(isset($_SERVER['HTTP_X_FORWARDED']) and self::validateIP($_SERVER['HTTP_X_FORWARDED']))
         {
             return $_SERVER['HTTP_X_FORWARDED'];
         }
