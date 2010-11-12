42007022d2477f0e2681bd940fe6f2891c2eb514
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2010-11-12 15:20:30 +0000

    - Updated Smarty to v3.0.1
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@4899 34db09b4-2314-0410-a940-aec02a7c051d

--- libraries/smarty/Smarty.class.php
+++ libraries/smarty/Smarty.class.php
@@ -28,7 +28,7 @@
  * @author Monte Ohrt <monte at ohrt dot com> 
  * @author Uwe Tews 
  * @package Smarty
- * @version 3.0.0
+ * @version 3.0.1
  */
 
 /**
@@ -87,7 +87,7 @@ class Smarty extends Smarty_Internal_Data {
 	* constant definitions
 	*/
     // smarty version
-    const SMARTY_VERSION = 'Smarty-3.0.0'; 
+    const SMARTY_VERSION = 'Smarty-3.0.1'; 
   	//define variable scopes
 	const SCOPE_LOCAL = 0;
 	const SCOPE_PARENT = 1;
@@ -318,6 +318,9 @@ class Smarty extends Smarty_Internal_Data {
         // create template object if necessary
         ($template instanceof $this->template_class)? $_template = $template :
         $_template = $this->createTemplate ($template, $cache_id, $compile_id, $parent);
+        if (isset($this->error_reporting)) {
+        	$_smarty_old_error_level = error_reporting($this->error_reporting);
+    	}
         // obtain data for cache modified check
         if ($this->cache_modified_check && $this->caching && $display) {
             $_isCached = $_template->isCached() && !$_template->has_nocache_code;
@@ -334,6 +337,9 @@ class Smarty extends Smarty_Internal_Data {
             $_output = $_template->getRenderedTemplate();
         } 
         $_template->rendered_content = null;
+        if (isset($this->error_reporting)) {
+        	error_reporting($_smarty_old_error_level);
+        } 
         // display or fetch
         if ($display) {
             if ($this->caching && $this->cache_modified_check) {
