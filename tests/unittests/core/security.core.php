dd99b15776985a32d6d0462ce15c4b266d244bb1
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2011-01-03 01:02:17 +0000

    - bugfix for generate_salt() (inconsistent length) and build_salted_hash() (static calls) - added unittests/core/security.core.php
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@4994 34db09b4-2314-0410-a940-aec02a7c051d

---
+++ tests/unittests/core/security.core.php
@@ -0,0 +1,59 @@
+<?php
+
+# Simpletest
+require_once 'simpletest/autorun.php';
+
+# Test Subject - Clansuite_Security
+require_once dirname(__FILE__) . '/../../../core/security.core.php';
+
+class Security_Test extends UnitTestCase {
+
+    /**
+     * testMethod_generate_salt()
+     */
+    public function testMethod_generate_salt()
+    {
+        # generate a salt with length
+        $salt = Clansuite_Security::generate_salt(12);
+        
+        # ensure $salt is a string
+        $this->assertTrue(is_string($salt), true);
+                
+        # ensure $salt has correct length
+        $this->assertEqual(strlen($salt), 12);
+    }
+    
+    public function testMethod_generate_hash()
+    {
+        $hash_md5 = Clansuite_Security::generate_hash('md5', 'admin');
+        
+        $this->assertIdentical('21232f297a57a5a743894a0e4a801fc3', $hash_md5);
+      
+        $hash_sha1 = Clansuite_Security::generate_hash('sha1', 'admin');
+        
+        $this->assertIdentical('d033e22ae348aeb5660fc2140aec35850c4da997', $hash_sha1);
+    }
+    
+    public function testMethod_build_salted_hash()
+    {
+        $salted_hash = Clansuite_Security::build_salted_hash('admin', 'md5');
+        
+        $this->assertTrue(is_array($salted_hash), true);
+     }
+    
+    public function testMethod_check_salted_hash()
+    {
+        # md5('admin'); from form input
+        $passwordhash = '21232f297a57a5a743894a0e4a801fc3';
+        # expected, from db
+        $databasehash = '7ff3adfa18a8ad7f115e90ce2c44a0ec';
+        # from db
+        $salt = 'Sko5ie'; 
+        $hash_algorithm = 'md5';
+    
+        $bool = Clansuite_Security::check_salted_hash( $passwordhash, $databasehash, $salt, $hash_algorithm );
+    
+        $this->assertTrue($bool, true);
+    }
+}
+?>
