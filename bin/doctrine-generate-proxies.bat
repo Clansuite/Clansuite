aa9d18ec04f711a4bd5b6d1da3f32bf3911b3b8f
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2011-01-18 19:31:27 +0000

    - path fix for entities and proxies - added generate proxies bat
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@5043 34db09b4-2314-0410-a940-aec02a7c051d

---
+++ bin/doctrine-generate-proxies.bat
@@ -0,0 +1,11 @@
+@echo off
+
+REM if "%PHPBIN%" == "" set PHPBIN=@php_bin@
+if "%PHPBIN%" == "" set PHPBIN=C:\Programme\Zend\ZendServer\bin\php.exe
+if not exist "%PHPBIN%" if "%PHP_PEAR_PHP_BIN%" neq "" goto USE_PEAR_PATH
+GOTO RUN
+:USE_PEAR_PATH
+set PHPBIN=%PHP_PEAR_PHP_BIN%
+:RUN
+"%PHPBIN%" "C:\Programme\Zend\Apache2\htdocs\clansuite\trunk\bin\doctrine.php" orm:generate-proxies
+PAUSE
