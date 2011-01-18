daa2bb23d6b1633277c68f10e555d04ba232dfc1
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2011-01-18 17:12:50 +0000

    - added doctrine2 cli
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@5036 34db09b4-2314-0410-a940-aec02a7c051d

---
+++ bin/cli-config.php
@@ -0,0 +1,30 @@
+<?php
+$classLoader = new \Doctrine\Common\ClassLoader('Entities', realpath('/../doctrine'));
+$classLoader->register();
+
+$classLoader = new \Doctrine\Common\ClassLoader('Proxies', realpath('/../doctrine'));
+$classLoader->register();
+
+$config = new \Doctrine\ORM\Configuration();
+$config->setMetadataCacheImpl(new \Doctrine\Common\Cache\ArrayCache);
+$driverImpl = $config->newDefaultAnnotationDriver(array(realpath('/../doctrine/entities')));
+$config->setMetadataDriverImpl($driverImpl);
+$config->setProxyDir('/../doctrine/proxies');
+$config->setProxyNamespace('Proxies');
+
+$connectionOptions = array(
+            'driver'    => 'pdo_mysql',
+            'user'      => "root",
+            'password'  => "toop",
+            'dbname'    => "clansuite",
+            'host'      => "localhost"
+        );
+
+$em = \Doctrine\ORM\EntityManager::create($connectionOptions, $config);
+
+$helperSet = new \Symfony\Component\Console\Helper\HelperSet(array(
+    'db' => new \Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper($em->getConnection()),
+    'em' => new \Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper($em)
+));
+
+?>
