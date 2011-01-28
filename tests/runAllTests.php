0b0036258ccbe9fce63cb27dfc8aedfbeecfaa44
vain <vain@34db09b4-2314-0410-a940-aec02a7c051d>
2011-01-28 11:17:49 +0000

    - testsuite update - added pass/fail reporting with dots, messages, classnames) - adjusted paths in bootstrap and tests
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@5100 34db09b4-2314-0410-a940-aec02a7c051d

--- tests/runAllTests.php
+++ tests/runAllTests.php
@@ -30,112 +30,40 @@
     * @version    SVN: $Id$
     */
 
-# setup simpletest
+# setup env(error/date/paths)
 require_once 'bootstrap.php';
-require_once 'simpletest/autorun.php';
+# setup simpletest
 require_once 'simpletest/unit_tester.php';
+require_once 'simpletest/test_case.php';
 require_once 'simpletest/reporter.php';
-require_once 'simpletest/extensions/colortext_reporter.php';
-require_once 'simpletest/extensions/junit_xml_reporter.php';
+# setup our testsuite and reporter
+require_once 'reporter.php';
+require_once 'testsuite.php';
 
-class ClansuiteTestsuite extends TestSuite
-{
-
-    private $files = array();
-
-    function __construct()
-    {
-        # add a headline to know where we are ,)
-        $this->TestSuite('Testsuite for "Clansuite - just an eSports CMS"');
-
-        # walk through dir /unittests and grab all tests
-        $this->scanDirForTests(dirname(__FILE__) . '/unittests');
-
-        # @todo check array structure of $tests and add Grouping by directory
-        # $test = new GroupTest('GroupTest for /core of Clansuite');
-
-        # Debug array with test files
-        #var_export($this->files);
-
-        if(count($this->files) > 0)
-        {
-            foreach($this->files as $testfile)
-            {
-                #echo '<p>File '. $testfile.' was added to the tests.</p>';
-
-                $this->addFile($testfile);
-            }
-        }
-        else
-        {
-            echo 'No UnitTests found.';
-        }
-    }
-
-    public function scanDirForTests($dir)
-    {
-        if(is_dir($dir))
-        {
-            $sourcedir = opendir($dir);
-            while(false !== ( $file = readdir($sourcedir) ))
-            {
-                # fix slashes
-                $source_file = strtr($dir . '/' . $file, '\\', '/');
-
-                if(is_dir($source_file))
-                {
-                    # exlude some dirs
-                    if($file == '.' || $file == '..' || $file == '.svn')
-                    {
-                        continue;
-                    }
-
-                    # WATCH IT ! RECURSION !
-                    $this->scanDirForTests($source_file);
-                }
-                else
-                {
-                    if(is_file($source_file) && $this->isPHPfile($file))
-                    {
-                        # add file to array
-                        $this->files[] = $source_file;
-
-                        #echo "<p>File {$source_file} was added to the tests array.</p>\n";
-                    }
-                }
-            }
-            closedir($sourcedir);
-        }
-    }
-
-    protected function isPHPFile($entry)
-    {
-        return preg_match('/\w+\.php$/', $entry);
-    }
-
-}
-
-# instantiate ClansuiteTestsuite
-$testsuite = new ClansuiteTestsuite;
+# instantiate Clansuite Testsuite
+$testsuite = new ClansuiteTestsuite();
+$success = false;
 
 # determine, if we are in commandline mode, then output pure text
 if(TextReporter::inCli())
 {
+    #require_once 'simpletest/extensions/colortext_reporter.php';
+    require_once 'simpletest/extensions/junit_xml_reporter.php';
     ob_start();
-    #$success = $testsuite->run(new TextReporter());
-    #$success = $testsuite->run(new ColorTextReporter());
+    #$reporter = new TextReporter();
+    #$reporter = new ColorTextReporter();
     $reporter = new JUnit_Xml_Reporter();
-    $result = $testsuite->run($reporter);    
+    $success = $testsuite->run($reporter);
     file_put_contents('simpletest.xml', ob_get_contents());
     ob_end_clean();
     $success = true;
 }
 else # else display nice html report
 {
-    $success = $testsuite->run(new HtmlReporter());
+    $success = $testsuite->run(new Reporter);
 }
 
-if(false == $success)
+if(false === $success)
 {
     # Exit with error code to let the build fail, when the test is unsuccessfull.
     exit(1);
---
+++ tests/testsuite.php
@@ -0,0 +1,105 @@
+<?php
+   /**
+    * Clansuite - just an eSports CMS
+    * Jens-André Koch © 2005 - onwards
+    * http://www.clansuite.com/
+    *
+    * This file is part of "Clansuite - just an eSports CMS".
+    *
+    * LICENSE:
+    *
+    *    This program is free software; you can redistribute it and/or modify
+    *    it under the terms of the GNU General Public License as published by
+    *    the Free Software Foundation; either version 2 of the License, or
+    *    (at your option) any later version.
+    *
+    *    This program is distributed in the hope that it will be useful,
+    *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    *    GNU General Public License for more details.
+    *
+    *    You should have received a copy of the GNU General Public License
+    *    along with this program; if not, write to the Free Software
+    *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    *
+    * @license    GNU/GPL v2 or (at your option) any later version, see "/doc/LICENSE".
+    * @author     Jens-André Koch <vain@clansuite.com>
+    * @copyright  Jens-André Koch (2005 - onwards)
+    * @link       http://www.clansuite.com
+    *
+    * @version    SVN: $Id$
+    */
+
+class ClansuiteTestsuite extends TestSuite
+{
+    private $files = array();
+
+    function __construct()
+    {
+        # add a headline to know where we are ,)
+        parent::__construct('Testsuite for "Clansuite - just an eSports CMS"');
+
+        # walk through dir /unittests and grab all tests
+        $this->scanDirForTests(dirname(__FILE__) . '/unittests');
+
+        # Debug array with test files
+        #var_export($this->files);
+
+        if(count($this->files) > 0)
+        {
+            foreach($this->files as $test_file)
+            {
+                #echo '<p>File '. $test_file.' was added to the tests.</p>';
+
+                $this->addFile($test_file);
+            }
+        }
+        else
+        {
+            echo 'No UnitTests found.';
+        }
+    }
+
+    public function scanDirForTests($dir)
+    {
+        if(is_dir($dir))
+        {
+            $sourcedir = opendir($dir);
+            while(false !== ( $file = readdir($sourcedir) ))
+            {
+                # fix slashes
+                $source_file = strtr($dir . '/' . $file, '\\', '/');
+
+                if(is_dir($source_file))
+                {
+                    # exlude some dirs
+                    if($file == '.' || $file == '..' || $file == '.svn')
+                    {
+                        continue;
+                    }
+
+                    # WATCH IT ! RECURSION !
+                    $this->scanDirForTests($source_file);
+                }
+                else
+                {
+                    if(is_file($source_file) && $this->isPHPfile($file))
+                    {
+                        # add file to array
+                        $this->files[] = realpath($source_file);
+
+                        #echo "<p>File {$source_file} was added to the tests array.</p>\n";
+                    }
+                }
+            }
+            closedir($sourcedir);
+        }
+    }
+
+    protected function isPHPFile($entry)
+    {
+        return preg_match('/\w+\.php$/', $entry);
+    }
+
+}
+?>
