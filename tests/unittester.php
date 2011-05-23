aef779211387a2818e17476134043227384242c6
Jens-André Koch <jakoch@web.de>
2011-05-23 21:35:41 +0000

    - added method markTestSkipped() and comments

--- tests/unittester.php
+++ tests/unittester.php
@@ -4,7 +4,26 @@
  */
 class Clansuite_UnitTestCase extends UnitTestCase
 {
+    /**
+     * Mark a test as incomplete.
+     *
+     * Usage:
+     * // Remove the following lines when you implement this test.
+     *  $this->markTestIncomplete('This test has not been implemented yet.');
+     */
     public function markTestIncomplete($msg)
+    {
+        $this->assertTrue(false, $msg);
+    }
+    
+    /**
+     * Mark a test as skipped.
+     *
+     * Usage:
+     *  if (!extension_loaded('abc'))
+     *  $this->markTestSkipped('Extension abc not available.');
+     */
+    public function markTestSkipped($msg)
     {
         $this->assertTrue(false, $msg);
     }
