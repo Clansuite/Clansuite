62cd0832efd1fefee61f9ce820aca443ec2e4b2a
vain <vain@dd235d61-e4b4-4fb3-b1fb-b81c2defcbc9>
2011-06-09 12:33:57 +0000

    - added headless + mem JAVA_OPTS
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@5366 dd235d61-e4b4-4fb3-b1fb-b81c2defcbc9

--- build-tools/statsvn/run_statsvn.sh
+++ build-tools/statsvn/run_statsvn.sh
@@ -2,15 +2,15 @@
 #
 # Generate the StatSVN web pages
 #
- 
-# Update local Subversion repository by cron
- 
+
+JAVA_OPTS="-Djava.awt.headless=true -Xms512M -Xmx2048M -XX:MaxPermSize=256M"
+
 # Generate Subversion log file
 svn log -v --xml /home/clansuite/svn/ > /home/clansuite/statsvn/statsvn.log
- 
+
 # Generate web content
 java -jar /home/clansuite/statsvn/statsvn.jar -no-developer root -no-developer creep7 -trac http://trac.clansuite.com/ -cache-dir /home/clansuite/statsvn/cache/ /home/clansuite/statsvn/statsvn.log /home/clansuite/svn -output-dir /var/www/webs/clansuite/statsvn -title "Clansuite - just an eSports CMS"
- 
+
 # chown / chmod the statsvn webserver dir
 chown clansuite:clansuite /var/www/webs/clansuite/statsvn
 chmod -R 705 /var/www/webs/clansuite/statsvn
