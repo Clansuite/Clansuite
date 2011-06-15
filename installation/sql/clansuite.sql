d673e5f41b87d713cdb096b1e3214d18c473528e
Jens-André Koch <jakoch@web.de>
2011-06-15 17:18:04 +0000

    - reintegrated cs_news_comments table into main sql

--- installation/sql/clansuite.sql
+++ installation/sql/clansuite.sql
@@ -1340,6 +1340,32 @@ INSERT INTO `cs_news` VALUES(100, 'german specialchars', 'ä 	ä\r\nÄ 	Ä\r\nö
 -- --------------------------------------------------------
 
 --
+-- Tabellenstruktur für Tabelle `cs_news_comments`
+--
+
+CREATE TABLE `cs_news_comments` (
+  `comment_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `news_id` int(10) unsigned NOT NULL,
+  `user_id` int(11) unsigned NOT NULL DEFAULT '0',
+  `email` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `body` text COLLATE utf8_unicode_ci NOT NULL,
+  `added` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `pseudo` varchar(25) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `ip` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `host` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`comment_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Daten für Tabelle `cs_news_comments`
+--
+
+INSERT INTO `cs_news_comments` VALUES(1, 21, 1, '', '123', '2005-07-29 13:04:07', '', '127.0.0.1', 'localhost');
+INSERT INTO `cs_news_comments` VALUES(2, 22, 2, '', '1234567', '2005-07-29 16:50:08', 'blub', '127.0.0.1', 'localhost');
+
+-- --------------------------------------------------------
+
+--
 -- Tabellenstruktur für Tabelle `cs_news_index`
 --
 -- Erzeugt am: 20. November 2010 um 12:33
