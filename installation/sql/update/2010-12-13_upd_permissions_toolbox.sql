6dcc8f2e2616462698780c37c2c818f20b0e6480
vain <vain@dd235d61-e4b4-4fb3-b1fb-b81c2defcbc9>
2011-06-15 17:21:34 +0000

    - hmm.. absolutely no clue why this resides here, dropped
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@5389 dd235d61-e4b4-4fb3-b1fb-b81c2defcbc9

--- installation/sql/update/2010-12-13_upd_permissions_toolbox.sql
+++
@@ -1,6 +0,0 @@
-
-UPDATE `cs_acl_actions` SET `action` = 'widget_toolbox' WHERE `action_id` =31 LIMIT 1;
-INSERT INTO `cs_acl_rules` VALUES(134, 2, 31, 1);
-INSERT INTO `cs_acl_rules` VALUES(135, 3, 31, 1);
-INSERT INTO `cs_acl_rules` VALUES(136, 4, 31, 1);
-
