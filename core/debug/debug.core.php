0803ce6a2934d1d794020c3fe9c05cc5cd950b8e
vain <vain@dd235d61-e4b4-4fb3-b1fb-b81c2defcbc9>
2011-07-02 11:24:17 +0000

    - added getWrappers() method (lists all available stream wrappers for debugging purposes)
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@5481 dd235d61-e4b4-4fb3-b1fb-b81c2defcbc9

--- core/debug/debug.core.php
+++ core/debug/debug.core.php
@@ -238,5 +238,17 @@ class Clansuite_Debug
     {
         self::printR(parse_ini_file(get_cfg_var('cfg_file_path'), true));
     }
+    
+    /**
+     * Lists all available wrappers
+     */
+    public static function getWrappers()
+    {
+        $w = stream_get_wrappers();
+        echo 'openssl: ',  extension_loaded  ('openssl') ? 'yes':'no', "\n";
+        echo 'http wrapper: ', in_array('http', $w) ? 'yes':'no', "\n";
+        echo 'https wrapper: ', in_array('https', $w) ? 'yes':'no', "\n";
+        echo 'wrappers: ', self::printR($w);
+    }
 }
 ?>
