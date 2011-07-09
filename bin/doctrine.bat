5c64f2f9e0b6a94734d57aaa96ad3a802eed30a1
Jens-André Koch <jakoch@web.de>
2011-07-09 09:02:25 +0000

    - adjusted paths for xampp and xampplite (@todo path detection)

--- bin/doctrine.bat
+++ bin/doctrine.bat
@@ -1,8 +1,10 @@
 @echo off
 
 REM if "%PHPBIN%" == "" set PHPBIN=@php_bin@
-if "%PHPBIN%" == "" set PHPBIN=C:\andre\nginx-serverpack\php\php.exe
+REM if "%PHPBIN%" == "" set PHPBIN=C:\nginx-serverpack\php\php.exe
 REM if "%PHPBIN%" == "" set PHPBIN=C:\Programme\Zend\ZendServer\bin\php.exe
+if "%PHPBIN%" == "" set PHPBIN=d:\xampp\php\php.exe
+REM if "%PHPBIN%" == "" set PHPBIN=C:\xampplite\php\php.exe
 if not exist "%PHPBIN%" if "%PHP_PEAR_PHP_BIN%" neq "" goto USE_PEAR_PATH
 GOTO RUN
 :USE_PEAR_PATH
