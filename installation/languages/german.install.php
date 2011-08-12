153de98ae1428144ac77617ee6d644cc8fc74617
Jens-André Koch <jakoch@web.de>
2011-08-12 15:37:20 +0000

    - copyright update

--- installation/languages/german.install.php
+++ installation/languages/german.install.php
@@ -1,19 +1,44 @@
 <?php
+   /**
+    * Clansuite - just an eSports CMS
+    * Jens-André Koch © 2005 - onwards
+    * http://www.clansuite.com/
+    *
+    * This file is part of "Clansuite - just an eSports CMS".
+    *
+    * LICENSE:
+    *
+    *    This program is free software; you can redistribute it and/or modify
+    *    it under the terms of the GNU General Public License as published by
+    *    the Free Software Foundation; either version 2 of the License, or
+    *    (at your option) any later version.
+    *
+    *    This program is distributed in the hope that it will be useful,
+    *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    *    GNU General Public License for more details.
+    *
+    *    You should have received a copy of the GNU General Public License
+    *    along with this program; if not, write to the Free Software
+    *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    *
+    * @license    GNU/GPL v2 or (at your option) any later version, see "/doc/LICENSE".
+    * @author     Jens-André Koch <vain@clansuite.com>
+    * @copyright  Jens-André Koch (2005 - onwards)
+    * @link       http://www.clansuite.com
+    *
+    * @version    SVN: $Id$
+    */
+
 /**
+ * German Language for Installation
+ * 
+ * Contributors: vain, vyper
+ * 
  * @category        Clansuite
  * @package         Installation
  * @subpackage      Languages
- * @author          Jens-André Koch <vain@clansuite.com>
- * @copyright       Jens-André Koch & Clansuite Development Team
- * @license         http://www.gnu.org/copyleft/gpl.html GNU/GPL Public Licence
- * @description     German Installation Language
- * @version         SVN: $Id$
- * @implements      ArrayAccess
- *
- * Encoding: UTF-8
- * Contributors: vain, vyper
  */
-
 class Language implements ArrayAccess
 {
     private $language = array();
