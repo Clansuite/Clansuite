ad3c662e1d6774e0b2845288aaa446405d3c679b
vain <vain@dd235d61-e4b4-4fb3-b1fb-b81c2defcbc9>
2011-12-08 02:09:42 +0000

    - had to add a check for the content of the block tag, to ensure that content is passed to the block - the block tag behaves a bit different as of Smarty 3.1.6 than in version before: the content is rendered twice! one time each on the opening and closing tag. Uwe what's on your mind? - replaced newlines with carriage returns (CR)
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@5842 dd235d61-e4b4-4fb3-b1fb-b81c2defcbc9

--- core/viewhelper/smarty/block.tabpage.php
+++ core/viewhelper/smarty/block.tabpage.php
@@ -35,19 +35,23 @@ function smarty_block_tabpage($params, $content, $smarty, &$repeat)
     }
 
     # Start TAB Page
-    $start_tabpage  = '<!-- START - TABPAGE "'.$name.'" -->';
-    $start_tabpage .= "\n";
-    $start_tabpage .= '<div class="tab-page">';
-    $start_tabpage .= "\n";
-    $start_tabpage .= '<h2 class="tab">'.$name.'</h2>';
-    $start_tabpage .= "\n";
+    $start_tabpage  = '<!-- START - TABPAGE "'.$name.'" -->' . CR;
+    $start_tabpage .= '<div class="tab-page">' . CR;
+    $start_tabpage .= '<h2 class="tab">'.$name.'</h2>' . CR;
 
     # End TAB Page
-    $end_tabpage = '</div><!-- END - TABPAGE "'.$name.'" -->';
+    $end_tabpage = '</div><!-- END - TABPAGE "'.$name.'" -->' . CR;
 
-    # Construct content for whole BLOCK
-    $content = $start_tabpage . $content . $end_tabpage;
-
-    return $content;
+    /**
+     * As of Smarty v3.1.6 the block tag is rendered at the opening AND closing tag
+     * This results in a duplication of content.
+     * To prevent this, we need to check that the content is oCRy rendered when the inner block (content)
+     * is present.
+     */
+    if(isset($content))
+    {
+        # Construct content for whole BLOCK
+        return $start_tabpage . $content . $end_tabpage;
+    }
 }
 ?>
--- core/viewhelper/smarty/block.tabpanel.php
+++ core/viewhelper/smarty/block.tabpanel.php
@@ -32,44 +32,44 @@ function smarty_block_tabpanel($params, $content, $smarty, &$repeat)
     # Initialize
     # @todo addJavascript('header', 'once', $js)
     # @todo addCss('once', $css);
-    $start_tabpane  = "<!-- Tabs with jQuery + YAML Accessible Tabs Plugin -->\n ";
-    $start_tabpane .= "\n";
-    $start_tabpane .= '<link rel="stylesheet" type="text/css" href="' . WWW_ROOT_THEMES_CORE . 'css/tabs.css" />';
-    $start_tabpane .= "\n";
-    $start_tabpane .= '<script type="text/javascript" src="' . WWW_ROOT_THEMES_CORE . 'javascript/jquery/jquery.tabs.js"></script>';
-    $start_tabpane .= "\n";
+    $start_tabpane  = '<!-- Tabs with jQuery + YAML Accessible Tabs Plugin -->' . CR;
+    $start_tabpane .= '<link rel="stylesheet" type="text/css" href="' . WWW_ROOT_THEMES_CORE . 'css/tabs.css" />' . CR;
+    $start_tabpane .= '<script type="text/javascript" src="' . WWW_ROOT_THEMES_CORE . 'javascript/jquery/jquery.tabs.js"></script>' . CR;
 
-    $html = <<< EOF
+    $js = <<< EOF
 <script type="text/javascript">
     $(document).ready(function(){
         $(".tabs").accessibleTabs({
-                                    fx:"fadeIn",
-                                    tabbody:'.tab-page',
-                                    tabhead: '.tab',
-                                    currentInfoText: '&raquo; ',
-                                    currentInfoPosition: 'prepend',
-                                    currentInfoClass: 'current-info'
-                                  });
+            fx: "fadeIn",
+            tabbody: '.tab-page',
+            tabhead: '.tab',
+            currentInfoText: '&raquo; ',
+            currentInfoPosition: 'prepend',
+            currentInfoClass: 'current-info'
+          });
     });
 </script>
 EOF;
 
-    $start_tabpane .= $html;
-    $start_tabpane .= "\n";
+    $start_tabpane .= $js . CR;
 
     # Start TAB Pane
-    $start_tabpane .= '<!-- START - TAB PANEL "'.$name.'" -->';
-    $start_tabpane .= "\n";
-    $start_tabpane .= '<div class="tabs" id="tab-panel-'.$name.'"> ';
-    $start_tabpane .= "\n";
+    $start_tabpane .= '<!-- START - TAB PANEL "'.$name.'" -->' . CR;
+    $start_tabpane .= '<div class="tabs" id="tab-panel-'.$name.'"> ' . CR;
 
     # End TAB Pane
-    $end_tabpane = '</div><!-- END - TAB PANEL "'.$name.'" -->';
-    $end_tabpane .= "\n";
+    $end_tabpane = '</div><!-- END - TAB PANEL "'.$name.'" -->' . CR;
 
     # Construct content for whole BLOCK
-    $content = $start_tabpane . $content . $end_tabpane;
-
-    return $content;
+    /**
+     * As of Smarty v3.1.6 the block tag is rendered at the opening AND closing tag
+     * This results in a duplication of content.
+     * To prevent this, we need to check that the content is oCRy rendered when the inner block (content)
+     * is present.
+     */
+    if(isset($content))
+    {
+        return $start_tabpane . $content . $end_tabpane;
+    }
 }
 ?>
