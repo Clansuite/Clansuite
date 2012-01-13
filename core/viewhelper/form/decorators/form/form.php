b2e173473bb9ab2f21a150ea3a7207632be9d661
vain <vain@dd235d61-e4b4-4fb3-b1fb-b81c2defcbc9>
2012-01-13 15:30:23 +0000

    - readded decorators (that mass-renamed-commit failed completely)
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@5933 dd235d61-e4b4-4fb3-b1fb-b81c2defcbc9

---
+++ core/viewhelper/form/decorators/form/form.php
@@ -0,0 +1,173 @@
+<?php
+   /**
+    * Clansuite - just an eSports CMS
+    * Jens-André Koch © 2005 - onwards
+    * http://www.clansuite.com/
+    *
+    * This file is part of "Clansuite - just an eSports CMS".
+    *
+    * LICENSE:
+    *
+    *    This program is free software; you can redistribute it and/or modify
+    *    it under the terms of the GNU General Public License as published by
+    *    the Free Software Foundation; either version 2 of the License, or
+    *    (at your option) any later version.
+    *
+    *    This program is distributed in the hope that it will be useful,
+    *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    *    GNU General Public License for more details.
+    *
+    *    You should have received a copy of the GNU General Public License
+    *    along with this program; if not, write to the Free Software
+    *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    *
+    * @license    GNU/GPL v2 or (at your option) any later version, see "/doc/LICENSE".
+    * @author     Jens-André Koch <vain@clansuite.com>
+    * @copyright  Jens-André Koch (2005-onwards)
+    * @link       http://www.clansuite.com
+    *
+    * @version    SVN: $Id$
+    */
+
+# Security Handler
+if (defined('IN_CS') === false)
+{
+    die('Clansuite not loaded. Direct Access forbidden.');
+}
+
+class Clansuite_Form_Decorator_Form extends Clansuite_Form_Decorator
+{
+    /**
+     * Name of this decorator
+     *
+     * @var string
+     */
+    public $name = 'form';
+
+    public function openOpenFormTag()
+    {
+        # init var
+        $html_form = '';
+
+        # init html form with an comment
+        $html_form = '<!-- Start of Form "'. $this->getName() .'" -->' . CR;
+
+        # open the opening form tag
+        $html_form .= '<form ';
+
+        return $html_form;
+    }
+
+    /**
+     * @return string returns html of attributes inside the opening form tag
+     */
+    public function getFormTagAttributesAsHTML()
+    {
+        # init var
+        $html_form = '';
+
+        if( mb_strlen($this->getID()) > 0 )
+        {
+            $html_form .= 'id="'.$this->getID().'" ';
+        }
+
+        if( mb_strlen($this->getAction()) > 0 )
+        {
+            $html_form .= 'action="'.$this->getAction().'" ';
+        }
+
+        if( mb_strlen($this->getMethod()) > 0 )
+        {
+            $html_form .= 'method="'.$this->getMethod().'" ';
+        }
+
+        if( mb_strlen($this->getEncoding()) > 0 )
+        {
+            $html_form .= 'enctype="'.$this->getEncoding().'" ';
+        }
+
+        if( mb_strlen($this->getTarget()) > 0 )
+        {
+            $html_form .= 'target="'.$this->getTarget().'" ';
+        }
+
+        if( mb_strlen($this->getName()) > 0 )
+        {
+             $html_form .= 'name="'.$this->getName().'" ';
+        }
+
+        if( mb_strlen($this->getAcceptCharset()) > 0 )
+        {
+             $html_form .= 'accept-charset="'.$this->getAcceptCharset().'" ';
+        }
+
+        if( $this->getAcceptCharset() === true )
+        {
+             $html_form .= ' autocomplete ';
+        }
+
+        if( $this->getNoValidation() === true )
+        {
+             $html_form .= ' novalidation ';
+        }
+
+        $html_form .= 'class="form '.$this->getClass().'"';
+
+        # return the attributes inside the opening form tag
+        return $html_form;
+    }
+
+    public function closeOpenFormTag()
+    {
+        # close the opened form tag
+        return '>' . CR;
+    }
+
+    public function addHeading()
+    {
+        $html_form = '';
+
+        # add heading
+        if( mb_strlen($this->getHeading()) > 0 )
+        {
+             $html_form = '<h2>'.$this->getHeading().'</h2>' . CR;
+        }
+
+        return $html_form;
+    }
+
+    public function addDescription()
+    {
+         $html_form = '';
+
+        # add description
+        if( mb_strlen($this->getDescription()) > 0 )
+        {
+             $html_form = '<p>'.$this->getDescription().'</p>' . CR;
+        }
+
+        return $html_form;
+    }
+
+    public function closeFormTag()
+    {
+        # close form
+        return CR . '</form>' . CR . '<!--- End of Form "'. $this->getName() .'" -->' . CR;
+    }
+
+    public function render($html_form_content)
+    {
+        # put all the pieces of html together
+        $html_form_content = $this->openOpenFormTag().              # <form
+                             $this->getFormTagAttributesAsHTML().   #  id/method/action/...
+                             $this->closeOpenFormTag().             # >
+                             $this->addHeading().                   # heading
+                             $this->addDescription().               # description
+                             $html_form_content.                    # formelements
+                             $this->closeFormTag();                 # </form>
+
+        return $html_form_content;
+    }
+}
+?>
---
+++ core/viewhelper/form/decorators/formelement/description.php
@@ -0,0 +1,73 @@
+<?php
+   /**
+    * Clansuite - just an eSports CMS
+    * Jens-André Koch © 2005 - onwards
+    * http://www.clansuite.com/
+    *
+    * This file is part of "Clansuite - just an eSports CMS".
+    *
+    * LICENSE:
+    *
+    *    This program is free software; you can redistribute it and/or modify
+    *    it under the terms of the GNU General Public License as published by
+    *    the Free Software Foundation; either version 2 of the License, or
+    *    (at your option) any later version.
+    *
+    *    This program is distributed in the hope that it will be useful,
+    *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    *    GNU General Public License for more details.
+    *
+    *    You should have received a copy of the GNU General Public License
+    *    along with this program; if not, write to the Free Software
+    *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    *
+    * @license    GNU/GPL v2 or (at your option) any later version, see "/doc/LICENSE".
+    * @author     Jens-André Koch <vain@clansuite.com>
+    * @copyright  Jens-André Koch (2005-onwards)
+    * @link       http://www.clansuite.com
+    * 
+    * @version    SVN: $Id$
+    */
+
+# Security Handler
+if (defined('IN_CS') === false)
+{
+    die('Clansuite not loaded. Direct Access forbidden.');
+}
+
+/**
+ * Clansuite_Formelement_Decorator_Description
+ *
+ * Adds a <span> element containing the formelement description after html_fromelement_content.
+ *
+ * @category Clansuite
+ * @package Clansuite_Form
+ * @subpackage Clansuite_Form_Decorator
+ */
+class Clansuite_Formelement_Decorator_Description extends Clansuite_Formelement_Decorator
+{
+    /**
+     * Name of this decorator
+     *
+     * @var string
+     */
+    public $name = 'description';
+
+    /**
+     * renders description AFTER formelement
+     *
+     * @todo if required form field add (*)
+     */
+    public function render($html_formelement)
+    {
+        # add description
+        if ( isset($this->formelement->description) == true)
+        {
+            $html_formelement .= '<br />'. CR;
+            $html_formelement .= '<span class="formdescription">' . $this->formelement->getDescription() . '</span>' . CR;
+        }
+        return $html_formelement;
+    }
+}
+?>
