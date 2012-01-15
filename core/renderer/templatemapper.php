2a66880caf7e5018d342fe960834969de74ce9f0
vain <vain@dd235d61-e4b4-4fb3-b1fb-b81c2defcbc9>
2012-01-15 18:45:50 +0000

    - part 1 for ticket #256 (classActionToTemplateMapper)
    git-svn-id: http://svn.gna.org/svn/clansuite/trunk@5941 dd235d61-e4b4-4fb3-b1fb-b81c2defcbc9

---
+++ core/renderer/templatemapper.php
@@ -0,0 +1,50 @@
+<?php
+   /**
+    * Clansuite - just an eSports CMS
+    * Jens-André Koch © 2005 - onwards
+    * http://www.clansuite.com/
+    *
+    * This file is part of "Clansuite - just an eSports CMS".
+    *
+    * LICENSE:
+    *
+    *    This program is free software; you can redistribute it and/or modify
+    *    it under the terms of the GNU General Public License as published by
+    *    the Free Software Foundation; either version 2 of the License, or
+    *    (at your option) any later version.
+    *
+    *    This program is distributed in the hope that it will be useful,
+    *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    *    GNU General Public License for more details.
+    *
+    *    You should have received a copy of the GNU General Public License
+    *    along with this program; if not, write to the Free Software
+    *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    *
+    * @license    GNU/GPL v2 or (at your option) any later version, see "/doc/LICENSE".
+    * @author     Jens-André Koch <vain@clansuite.com>
+    * @copyright  Jens-André Koch (2005 - onwards)
+    * @link       http://www.clansuite.com
+    *
+    * @version    SVN: $Id$
+    */
+
+# Security Handler
+if(defined('IN_CS') === false)
+{
+    die('Clansuite not loaded. Direct Access forbidden.');
+}
+
+/**
+ * Clansuite_View_Mapper
+ *
+ * Clansuite_View_Mapper is a "class action" to "template" mapper.
+ * This has nothing to do with rendering, but with template selection for the view.
+ * If no template was set manually, this class will help determining the template,
+ * by mapping the requested class and action to a template.
+ */
+class Clansuite_View_Mapper
+{
+
+}
