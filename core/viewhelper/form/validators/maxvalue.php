424f0200ff01b8d3b9a9ad438d490930ef8f185c
Jens-André Koch <jakoch@web.de>
2012-02-01 18:04:21 +0000

    - added validators for maxvalue and minvalue

---
+++ core/viewhelper/form/validators/maxvalue.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Clansuite - just an eSports CMS
+ * Jens-André Koch © 2005 - onwards
+ * http://www.clansuite.com/
+ *
+ * This file is part of "Clansuite - just an eSports CMS".
+ *
+ * LICENSE:
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @license    GNU/GPL v2 or (at your option) any later version, see "/doc/LICENSE".
+ * @author     Jens-André Koch <vain@clansuite.com>
+ * @copyright  Jens-André Koch (2005-onwards)
+ * @link       http://www.clansuite.com
+ *
+ * @version    SVN: $Id$
+ */
+# Security Handler
+if(defined('IN_CS') === false)
+{
+    die('Clansuite not loaded. Direct Access forbidden.');
+}
+
+/**
+ * Validates the value of an integer with maxvalue given.
+ */
+class Clansuite_Formelement_Validator_Maxvalue extends Clansuite_Formelement_Validator
+{
+    public $maxvalue;
+
+    public function getMaxvalue()
+    {
+        return $this->maxvalue;
+    }
+
+    /**
+     * Setter for the maximum length of the string.
+     *
+     * @param integer $maxvalue
+     */
+    public function setMaxvalue($maxvalue)
+    {
+        $this->maxvalue = (int) $maxvalue;
+    }
+
+    public function getErrorMessage()
+    {
+        return _('The value exceeds the maxvalue of ' . $this->getMaxvalue() . ' chars.');
+    }
+
+    protected function processValidationLogic($value)
+    {
+        if($value > $this->getMaxvalue())
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+}
+
+?>
---
+++ core/viewhelper/form/validators/minvalue.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Clansuite - just an eSports CMS
+ * Jens-André Koch © 2005 - onwards
+ * http://www.clansuite.com/
+ *
+ * This file is part of "Clansuite - just an eSports CMS".
+ *
+ * LICENSE:
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @license    GNU/GPL v2 or (at your option) any later version, see "/doc/LICENSE".
+ * @author     Jens-André Koch <vain@clansuite.com>
+ * @copyright  Jens-André Koch (2005-onwards)
+ * @link       http://www.clansuite.com
+ *
+ * @version    SVN: $Id$
+ */
+# Security Handler
+if(defined('IN_CS') === false)
+{
+    die('Clansuite not loaded. Direct Access forbidden.');
+}
+
+/**
+ * Validates the value of an integer with minvalue given.
+ */
+class Clansuite_Formelement_Validator_Minvalue extends Clansuite_Formelement_Validator
+{
+    public $minvalue;
+
+    public function getMinvalue()
+    {
+        return $this->minvalue;
+    }
+
+    /**
+     * Setter for the minimum length of the string.
+     *
+     * @param integer $minvalue
+     */
+    public function setMinvalue($minvalue)
+    {
+        $this->minvalue = (int) $minvalue;
+    }
+
+    public function getErrorMessage()
+    {
+        return _('The value deceeds (is less than) the minvalue of ' . $this->getMinvalue() . ' chars.');
+    }
+
+    protected function processValidationLogic($value)
+    {
+        if($value <= $this->getMinvalue())
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+}
+
+?>
