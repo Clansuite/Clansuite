c675fee40c2f9b4a913cc1cffc94f3a130fdd261
Jens-André Koch <jakoch@web.de>
2012-03-09 21:45:16 +0000

    - eventdispatcher to dispatcher - one class per file: extracted loader from dispatcher file

--- core/event/dispatcher.php
+++ core/event/dispatcher.php
@@ -67,7 +67,7 @@ interface Event
  * @package     Core
  * @subpackage  Eventdispatcher
  */
-class Eventdispatcher
+class Dispatcher
 {
     /**
      * @var object Instance of Koch_Eventdispatcher (singleton)
@@ -402,105 +402,6 @@ class Event implements ArrayAccess
     public function offsetUnset($name)
     {
         unset($this->context[$name]);
-    }
-}
-
-/**
- * Koch_Eventloader
- *
- * Purpose:
- * Eventloader handles the loading and registering of events by using event configuration files.
- *
- * @category    Koch
- * @package     Core
- * @subpackage  Eventloader
- */
-class Eventloader
-{
-    /**
-     * Loads and registers all events of the core and all activated modules.
-     */
-    public static function autoloadEvents()
-    {
-        #self::loadAllModuleEvents();
-        self::loadCoreEvents();
-    }
-
-    /**
-     * Registers multiple Events by Name
-     *
-     * @param array $events_array eventname => filename
-     * @param array $event_objects eventname => object
-     */
-    public static function loadEventHandlers($events)
-    {
-        if(empty($events) or is_array($events) === false)
-        {
-            return;
-        }
-        else # ok, we got an array with some event names
-        {
-            foreach($events as $event)
-            {
-                # array[0] filename
-                $filename = $array[0];
-
-                # array[1] classname
-                $classname = Koch_Functions::ensurePrefixedWith($array[1], 'Koch_Event_');
-
-                # load eventhandler
-                Koch_Loader::requireFile($filename, $classname);
-
-                # instantiate eventhandler
-                $event_object = new $classname();
-
-                # add the eventhandler to the dispatcher
-                $eventdispatcher = Koch_Eventdispatcher::instantiate();
-                $eventdispatcher->addEventHandler($event, $event_object);
-            }
-        }
-    }
-
-    /**
-     * Loads and registers the core eventhandlers according to the event configuration file.
-     * The event configuration for the core is file is /configuration/events.config.php.
-     */
-    public static function loadCoreEvents()
-    {
-        $events = array();
-        $events = include ROOT . 'configuration/events.config.php';
-
-        self::loadEventHandlers($events);
-    }
-
-    /**
-     * Loads and registers the eventhandlers for a module according to the module event configuration file.
-     * The event configuration files for a module resides in /module/module.events.php (abstract).
-     * For instance the eventcfg filename for module news is /modules/news/news.events.php.
-     *
-     * @param string $modulename Name of a module.
-     */
-    public static function loadModuleEvents($modulename)
-    {
-        $events = array();
-        $events = include ROOT_MOD . $modulename . DS . $modulename . '.events.php';
-
-        self::loadEventHandlers($events);
-    }
-
-    /**
-     * Loads and registers the eventhandlers for all activated modules.
-     */
-    public static function loadAllModuleEvents()
-    {
-        # fetch all activated modules
-        $modules = Koch_ModuleInfoController::getAllActivatedModules();
-
-        # load eventhandlers for each module
-        foreach($modules as $module)
-        {
-            self::loadModuleEvents($module);
-        }
     }
 }
 ?>
