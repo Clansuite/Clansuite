5aa4da5de7d6912778db0fb5196a2f200b90ef50
Jens-André Koch <jakoch@web.de>
2012-03-10 23:24:20 +0000

    - added class skeleton for cache compositum

---
+++ core/cache/compositum.php
@@ -0,0 +1,57 @@
+<?php
+   /**
+    * Koch Framework
+    * Jens-André Koch © 2005 - onwards
+    * http://www.clansuite.com/
+    *
+    * This file is part of "Koch Framework".
+    *
+    * LICENSE:
+    *
+    *    This program is free software; you can redistribute it and/or modify
+    *    it under the terms of the GNU General Public License as published by
+    *    the Free Software Foundation; either version 2 of the License, or
+    *    (at your option) any later version.
+    *
+    *    This program is distributed in the hope that it will be useful,
+    *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    *    GNU General Public License for more details.
+    *
+    *    You should have received a copy of the GNU General Public License
+    *    along with this program; if not, write to the Free Software
+    *    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    *
+    * @license    GNU/GPL v2 or (at your option) any later version, see "/doc/LICENSE".
+    * @author     Jens-André Koch <vain@clansuite.com>
+    * @copyright  Jens-André Koch (2005 - onwards)
+    * @link       http://www.clansuite.com
+    *
+    * @version    SVN: $Id$
+    */
+
+namespace Koch\Cache;
+
+# Security Handler
+if(defined('IN_CS') === false)
+{
+    exit('Koch Framework not loaded. Direct Access forbidden.');
+}
+
+/**
+ * Koch_Logger
+ *
+ * This class represents a compositum for all registered cache adapters.
+ * Another name for this class would be MultiCache.
+ * A new cache object is added with addCache(), removed with removeCache().
+ * The composition is fired via method cache().
+ *
+ * @category    Koch
+ * @package     Core
+ * @subpackage  Cache
+ */
+class Compositum
+{
+// @todo
+}
+?>
