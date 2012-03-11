60638ec28b93a486ef793d536226a57a67c74217
Jens-André Koch <jakoch@web.de>
2012-03-11 13:57:09 +0000

    - added unittest for cache adapter apc

---
+++ tests/unittests/core/cache/apc.php
@@ -0,0 +1,38 @@
+<?php
+#namespace Koch\Test\Cache;
+
+use Koch\Cache\Adapter\Apc;
+
+class ApcTest extends Clansuite_UnitTestCase
+{
+    private $apc_not_loaded;
+
+    public function setUp()
+    {
+        if(extension_loaded('apc') === false)
+        {
+            $this->apc_not_loaded = true;
+            $this->markTestSkipped('The PHP extension APC (Alternative PHP Cache) is not loaded.!');
+        }
+    }
+
+    public function testCache()
+    {
+        $this->skipIf($this->apc_not_loaded, 'APC extension not loaded!');
+
+        $cache = new Apc();
+
+        $this->assertFalse($cache->contains('key1'));
+
+        $cache->store('key1', 'value1');
+
+        $this->assertEquals('value1', $cache->fetch('key1'));
+
+        $this->assertTrue($cache->contains('key1'));
+
+        $cache->delete('key1');
+
+        $this->assertFalse($cache->contains('key1'));
+    }
+}
+?>
