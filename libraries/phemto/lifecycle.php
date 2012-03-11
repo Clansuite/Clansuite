33082b381609656d97960ae52bd9611b97ce32b5
Jens-André Koch <jakoch@web.de>
2012-03-11 16:39:38 +0000

    - some premature optimizations and code styling for phemto

--- libraries/phemto/lifecycle.php
+++ libraries/phemto/lifecycle.php
@@ -1,72 +1,92 @@
 <?php
-abstract class Lifecycle {
+
+abstract class Lifecycle
+{
     public $class;
 
-    function __construct($class) {
+    function __construct($class)
+    {
         $this->class = $class;
         $this->triggerAutoload($class);
     }
-    
-    private function triggerAutoload($class) {
+
+    private function triggerAutoload($class)
+    {
         class_exists($class);
     }
 
-    function isOneOf($candidates) {
+    function isOneOf($candidates)
+    {
         return in_array($this->class, $candidates);
     }
 
     abstract function instantiate($dependencies);
 }
 
-class Value extends Lifecycle {
+class Value extends Lifecycle
+{
     private $instance;
 
-    function __construct($instance) {
+    function __construct($instance)
+    {
         $this->instance = $instance;
     }
 
-    function instantiate($dependencies) {
+    function instantiate($dependencies)
+    {
         return $this->instance;
     }
+
 }
 
-class Factory extends Lifecycle {
-    function instantiate($dependencies) {
+class Factory extends Lifecycle
+{
+
+    function instantiate($dependencies)
+    {
         return call_user_func_array(
-                array(new ReflectionClass($this->class), 'newInstance'),
-                $dependencies);
+                        array(new ReflectionClass($this->class), 'newInstance'), $dependencies);
     }
+
 }
 
-class Reused extends Lifecycle {
+class Reused extends Lifecycle
+{
     private $instance;
 
-    function instantiate($dependencies) {
-        if (! isset($this->instance)) {
+    function instantiate($dependencies)
+    {
+        if(false === isset($this->instance))
+        {
             $this->instance = call_user_func_array(
-                    array(new ReflectionClass($this->class), 'newInstance'),
-                    $dependencies);
+                    array(new ReflectionClass($this->class), 'newInstance'), $dependencies);
         }
         return $this->instance;
     }
+
 }
 
-class Sessionable extends Lifecycle {
+class Sessionable extends Lifecycle
+{
     private $slot;
 
-    function __construct($class, $slot = false) {
+    function __construct($class, $slot = false)
+    {
         parent::__construct($class);
         $this->slot = $slot ? $slot : $class;
     }
 
-    function instantiate($dependencies) {
+    function instantiate($dependencies)
+    {
         @session_start();
-        if (! isset($_SESSION[$this->slot])) {
+        if(false === isset($_SESSION[$this->slot]))
+        {
             $_SESSION[$this->slot] = call_user_func_array(
-                    array(new ReflectionClass($this->class), 'newInstance'),
-                    $dependencies);
+                    array(new ReflectionClass($this->class), 'newInstance'), $dependencies);
         }
         return $_SESSION[$this->slot];
     }
+
 }
+
 ?>
--- libraries/phemto/phemto.php
+++ libraries/phemto/phemto.php
@@ -1,53 +1,78 @@
 <?php
-require_once(dirname(__FILE__) . '/lifecycle.php');
-require_once(dirname(__FILE__) . '/repository.php');
+require_once __DIR__ . '/lifecycle.php';
+require_once __DIR__ . '/repository.php';
 
-class CannotFindImplementation extends Exception { }
-class CannotDetermineImplementation extends Exception { }
-class SetterDoesNotExist extends Exception { }
-class MissingDependency extends Exception { }
+class CannotFindImplementation extends Exception
+{
 
-class Phemto {
+}
+
+class CannotDetermineImplementation extends Exception
+{
+
+}
+
+class SetterDoesNotExist extends Exception
+{
+
+}
+
+class MissingDependency extends Exception
+{
+
+}
+
+class Phemto
+{
     private $top;
     private $named_parameters = array();
     private $unnamed_parameters = array();
 
-    function __construct() {
+    function __construct()
+    {
         $this->top = new Context($this);
     }
 
-    function willUse($preference) {
+    function willUse($preference)
+    {
         $this->top->willUse($preference);
     }
 
-    function register($preference) {
+    function register($preference)
+    {
         $this->top->willUse($preference);
     }
 
-    function forVariable($name) {
+    function forVariable($name)
+    {
         return $this->top->forVariable($name);
     }
 
-    function whenCreating($type) {
+    function whenCreating($type)
+    {
         return $this->top->whenCreating($type);
     }
 
-    function forType($type) {
+    function forType($type)
+    {
         return $this->top->forType($type);
     }
 
-    function fill() {
+    function fill()
+    {
         $names = func_get_args();
         return new IncomingParameters($names, $this);
     }
 
-    function with() {
+    function with()
+    {
         $values = func_get_args();
         $this->unnamed_parameters = array_merge($this->unnamed_parameters, $values);
         return $this;
     }
 
-    function create() {
+    function create()
+    {
         $values = func_get_args();
         $type = array_shift($values);
         $this->unnamed_parameters = array_merge($this->unnamed_parameters, $values);
@@ -57,7 +82,8 @@ class Phemto {
         return $object;
     }
 
-    function instantiate() {
+    function instantiate()
+    {
         $values = func_get_args();
         $type = array_shift($values);
         $this->unnamed_parameters = array_merge($this->unnamed_parameters, $values);
@@ -67,53 +93,71 @@ class Phemto {
         return $object;
     }
 
-    function pickFactory($type, $candidates) {
+    function pickFactory($type, $candidates)
+    {
         throw new CannotDetermineImplementation($type);
     }
 
-    function settersFor($class) {
+    function settersFor($class)
+    {
         return array();
     }
 
-    function wrappersFor($type) {
+    function wrappersFor($type)
+    {
         return array();
     }
 
-    function useParameters($parameters) {
+    function useParameters($parameters)
+    {
         $this->named_parameters = array_merge($this->named_parameters, $parameters);
     }
 
-    function instantiateParameter($parameter, $nesting) {
-        if (isset($this->named_parameters[$parameter->getName()])) {
+    function instantiateParameter($parameter, $nesting)
+    {
+        if(true === isset($this->named_parameters[$parameter->getName()]))
+        {
             return $this->named_parameters[$parameter->getName()];
         }
-        if ($value = array_shift($this->unnamed_parameters)) {
+
+        $value = array();
+        $value = array_shift($this->unnamed_parameters);
+        if($value)
+        {
             return $value;
         }
+
         throw new MissingDependency($parameter->getName());
     }
 
-    function repository() {
+    function repository()
+    {
         return $this->repository;
     }
+
 }
 
-class IncomingParameters {
+class IncomingParameters
+{
     private $injector;
 
-    function __construct($names, $injector) {
+    function __construct($names, $injector)
+    {
         $this->names = $names;
         $this->injector = $injector;
     }
 
-    function with() {
+    function with()
+    {
         $values = func_get_args();
         $this->injector->useParameters(array_combine($this->names, $values));
         return $this->injector;
     }
+
 }
 
-class Context {
+class Context
+{
     private $parent;
     private $repository;
     private $registry = array();
@@ -122,103 +166,139 @@ class Context {
     private $types = array();
     private $wrappers = array();
 
-    function __construct($parent) {
+    function __construct($parent)
+    {
         $this->parent = $parent;
     }
 
-    function willUse($preference) {
-        if ($preference instanceof Lifecycle) {
+    function willUse($preference)
+    {
+        if($preference instanceof Lifecycle)
+        {
             $lifecycle = $preference;
-        } elseif (is_object($preference)) {
+        }
+        elseif(true === is_object($preference))
+        {
             $lifecycle = new Value($preference);
-        } else {
+        }
+        else
+        {
             $lifecycle = new Factory($preference);
         }
         array_unshift($this->registry, $lifecycle);
     }
 
-    function forVariable($name) {
+    function forVariable($name)
+    {
         return $this->variables[$name] = new Variable($this);
     }
 
-    function whenCreating($type) {
-        if (! isset($this->contexts[$type])) {
+    function whenCreating($type)
+    {
+        if(false === isset($this->contexts[$type]))
+        {
             $this->contexts[$type] = new Context($this);
         }
         return $this->contexts[$type];
     }
 
-    function forType($type) {
-        if (! isset($this->types[$type])) {
+    function forType($type)
+    {
+        if(false === isset($this->types[$type]))
+        {
             $this->types[$type] = new Type();
         }
         return $this->types[$type];
     }
 
-    function wrapWith($type) {
+    function wrapWith($type)
+    {
         array_push($this->wrappers, $type);
     }
 
-    function create($type, $nesting = array()) {
+    function create($type, $nesting = array())
+    {
         $lifecycle = $this->pickFactory($type, $this->repository()->candidatesFor($type));
         $context = $this->determineContext($lifecycle->class);
-        if ($wrapper = $context->hasWrapper($type, $nesting)) {
+        $wrapper = $context->hasWrapper($type, $nesting);
+        if($wrapper)
+        {
             return $this->create($wrapper, $this->cons($wrapper, $nesting));
         }
         $instance = $lifecycle->instantiate($context->createDependencies(
-                        $this->repository()->getConstructorParameters($lifecycle->class),
-                        $this->cons($lifecycle->class, $nesting)));
+                        $this->repository()->getConstructorParameters($lifecycle->class), $this->cons($lifecycle->class, $nesting)));
         $this->invokeSetters($context, $nesting, $lifecycle->class, $instance);
         return $instance;
     }
 
-    function pickFactory($type, $candidates) {
-        if (count($candidates) == 0) {
+    function pickFactory($type, $candidates)
+    {
+        if(count($candidates) == 0)
+        {
             throw new CannotFindImplementation($type);
-        } elseif ($preference = $this->preferFrom($candidates)) {
-            return $preference;
-        } elseif (count($candidates) == 1) {
-            return new Factory($candidates[0]);
-        } else {
-            return $this->parent->pickFactory($type, $candidates);
         }
+
+        $preference = $this->preferFrom($candidates);
+        if($preference)
+        {
+            return $preference;
+        }
+
+        if(count($candidates) == 1)
+        {
+            return new Factory($candidates[0]);
+        }
+
+        return $this->parent->pickFactory($type, $candidates);
     }
 
-    function hasWrapper($type, $already_applied) {
-        foreach ($this->wrappersFor($type) as $wrapper) {
-            if (! in_array($wrapper, $already_applied)) {
+    function hasWrapper($type, $already_applied)
+    {
+        foreach($this->wrappersFor($type) as $wrapper)
+        {
+            if(false === in_array($wrapper, $already_applied))
+            {
                 return $wrapper;
             }
         }
         return false;
     }
 
-    private function invokeSetters($context, $nesting, $class, $instance) {
-        foreach ($context->settersFor($class) as $setter) {
+    private function invokeSetters($context, $nesting, $class, $instance)
+    {
+        foreach($context->settersFor($class) as $setter)
+        {
             $context->invoke($instance, $setter, $context->createDependencies(
-                                $this->repository()->getParameters($class, $setter),
-                                $this->cons($class, $nesting)));
+                            $this->repository()->getParameters($class, $setter), $this->cons($class, $nesting)));
         }
     }
 
-    private function settersFor($class) {
+    private function settersFor($class)
+    {
         $setters = isset($this->types[$class]) ? $this->types[$class]->setters : array();
         return array_values(array_keys(array_flip(array_merge(
-                    $setters, $this->parent->settersFor($class)))));
+                                                $setters, $this->parent->settersFor($class)))));
     }
 
-    function wrappersFor($type) {
+    function wrappersFor($type)
+    {
         return array_values(array_merge(
-                    $this->wrappers, $this->parent->wrappersFor($type)));
+                                $this->wrappers, $this->parent->wrappersFor($type)));
     }
 
-    function createDependencies($parameters, $nesting) {
+    function createDependencies($parameters, $nesting)
+    {
         $values = array();
-        foreach ($parameters as $parameter) {
-            try {
+        foreach($parameters as $parameter)
+        {
+            try
+            {
                 $values[] = $this->instantiateParameter($parameter, $nesting);
-            } catch (Exception $e) {
-                if ($parameter->isOptional()) {
+            }
+            catch(Exception $e)
+            {
+                if($parameter->isOptional())
+                {
                     break;
                 }
                 throw $e;
@@ -227,76 +307,107 @@ class Context {
         return $values;
     }
 
-    private function instantiateParameter($parameter, $nesting) {
-        if ($hint = $parameter->getClass()) {
+    private function instantiateParameter($parameter, $nesting)
+    {
+        $hint = $parameter->getClass();
+        if($hint)
+        {
             return $this->create($hint->getName(), $nesting);
-        } elseif (isset($this->variables[$parameter->getName()])) {
-            if ($this->variables[$parameter->getName()]->preference instanceof Lifecycle) {
+        }
+
+        if(true === isset($this->variables[$parameter->getName()]))
+        {
+            if($this->variables[$parameter->getName()]->preference instanceof Lifecycle)
+            {
                 return $this->variables[$parameter->getName()]->preference->instantiate(array());
-            } elseif (! is_string($this->variables[$parameter->getName()]->preference)) {
+            }
+
+            if(false === is_string($this->variables[$parameter->getName()]->preference))
+            {
                 return $this->variables[$parameter->getName()]->preference;
             }
+            
             return $this->create($this->variables[$parameter->getName()]->preference, $nesting);
         }
+
         return $this->parent->instantiateParameter($parameter, $nesting);
     }
 
-    private function determineContext($class) {
-        foreach ($this->contexts as $type => $context) {
-            if ($this->repository()->isSupertype($class, $type)) {
+    private function determineContext($class)
+    {
+        foreach($this->contexts as $type => $context)
+        {
+            if(true === $this->repository()->isSupertype($class, $type))
+            {
                 return $context;
             }
         }
         return $this;
     }
 
-    private function invoke($instance, $method, $arguments) {
+    private function invoke($instance, $method, $arguments)
+    {
         call_user_func_array(array($instance, $method), $arguments);
     }
 
-    private function preferFrom($candidates) {
-        foreach ($this->registry as $preference) {
-            if ($preference->isOneOf($candidates)) {
+    private function preferFrom($candidates)
+    {
+        foreach($this->registry as $preference)
+        {
+            if(true === $preference->isOneOf($candidates))
+            {
                 return $preference;
             }
         }
         return false;
     }
 
-    private function cons($head, $tail) {
+    private function cons($head, $tail)
+    {
         array_unshift($tail, $head);
         return $tail;
     }
 
-    function repository() {
+    function repository()
+    {
         return $this->parent->repository();
     }
+
 }
 
-class Variable {
+class Variable
+{
     public $preference;
     private $context;
 
-    function __construct($context) {
+    function __construct($context)
+    {
         $this->context = $context;
     }
 
-    function willUse($preference) {
+    function willUse($preference)
+    {
         $this->preference = $preference;
         return $this->context;
     }
 
-    function useString($string) {
+    function useString($string)
+    {
         $this->preference = new Value($string);
         return $this->context;
     }
+
 }
 
-class Type {
+class Type
+{
     public $setters = array();
 
-    function call($method) {
+    function call($method)
+    {
         array_unshift($this->setters, $method);
     }
+
 }
+
 ?>
