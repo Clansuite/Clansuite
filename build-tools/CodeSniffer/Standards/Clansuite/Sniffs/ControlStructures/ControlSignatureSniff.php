557f61a7d391e1b44b5e8b28a9a112d902620e69
jakoch <jakoch@web.de>
2012-08-01 22:42:17 +0200

    added kernighan&ritchie style for control signatures

--- build-tools/CodeSniffer/Standards/Clansuite/Sniffs/ControlStructures/ControlSignatureSniff.php
+++ build-tools/CodeSniffer/Standards/Clansuite/Sniffs/ControlStructures/ControlSignatureSniff.php
@@ -27,7 +27,39 @@ class Clansuite_Sniffs_ControlStructures_ControlSignatureSniff extends PHP_CodeS
      *
      * @return array(string)
      */
-    protected function getPatterns()
+    protected function getPattern()
+    {
+        return self::getPatternsInKRStyle();
+    }
+
+    /**
+     * Returns the patterns that this test wishes to verify.
+     * Enforcing brackets in Kernighan and Ritchie (C,C++,C#) Style.
+     *
+     * @return array(string)
+     */
+    private function getPatternsInKRStyle()
+    {
+        return array(
+            'do {EOL...} while (...);EOL',
+            'while (...) {EOL',
+            'for (...) {EOL',
+            'if (...) {EOL',
+            'foreach (...) {EOL',
+            '}EOL else if (...) {EOL',
+            '}EOL elseif (...) {EOL',
+            '}EOL else {EOL',
+            'do {EOL',
+         );
+    }
+
+    /**
+     * Returns the patterns that this test wishes to verify.
+     * Enforcing brackets on new line (Allman-Style).
+     *
+     * @return array(string)
+     */
+    private function getPatternsInAllmanStyle()
     {
         return array(
             'tryEOL...{EOL...}EOL...catch (...)EOL...{EOL',
