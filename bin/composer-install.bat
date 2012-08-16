bb68fe7217f6acc0a1f4e94c74c97691ae93f821
jakoch <jakoch@web.de>
2012-08-16 15:07:02 +0200

    added check for GIT_EXEC_PATH environment variable

--- bin/composer-install.bat
+++ bin/composer-install.bat
@@ -7,12 +7,14 @@
 :: The dependencies are defined in "..\composer.json".
 ::
 
+if "%GIT_EXEC_PATH%" == "" call::no_gitcommand_found
+
 if "%PHPBIN%" == "" call::no_phpcommand_found
 
 :: leave "/bin" folder, for composer to find "composer.json"
 cd..
 
-"%PHPBIN%" "bin\composer\composer.phar" "install" > composer-install.report.txt
+"%PHPBIN%" "bin\composer\composer.phar" "install"
 
 :no_phpcommand_found - displayes a hint for the user to setup env var PHPBIN
 echo ---------------------------------------------------------------------------
@@ -21,6 +23,15 @@ echo           Hint: "set PHPBIN=C:\PHP\php.exe"
 echo ---------------------------------------------------------------------------
 :: fallback to hardcoded path
 set PHPBIN=i:\wpnxm\bin\php\php.exe
+goto:eof
+
+:no_gitcommand_found - displayes a hint for the user to setup env var GIT_EXEC_PATH
+echo --------------------------------------------------------------------------------
+echo  WARNING  Set environment variable GIT_EXEC_PATH to the location of your git.exe
+echo           Hint: "set GIT_EXEC_PATH=C:\Programs\GIT\bin"
+echo --------------------------------------------------------------------------------
+:: fallback to hardcoded path
+set GIT_EXEC_PATH=C:\Programme\Git\bin
 goto:eof
 
 pause
