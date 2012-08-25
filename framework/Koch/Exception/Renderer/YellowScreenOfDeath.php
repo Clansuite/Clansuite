8ad31b258803534cb9a2f87a478c740ccb1e3b07
jakoch <jakoch@web.de>
2012-08-25 21:51:00 +0200

    render errorname

--- framework/Koch/Exception/Renderer/YellowScreenOfDeath.php
+++ framework/Koch/Exception/Renderer/YellowScreenOfDeath.php
@@ -221,7 +221,7 @@ class YellowScreenOfDeath
 
         // Panel 1 - Errormessage
         $html .= '<div id="panel1" class="panel">';
-        $html .= '<h3>Error</h3>';
+        $html .= '<h3>Error - '.$errorname.'</h3> ';
         $html .= '<p style="font-weight: bold;">' . $errstr . ' (' . $errno . ')</p>';
         $html .= '<p>in file "<span style="font-weight: bold;">' . $errfile . '</span>"';
         $html .= ' on line #<span style="font-weight: bold;">' . $errline.'.</span></p>';
