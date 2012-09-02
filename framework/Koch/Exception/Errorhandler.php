c87ac68607012e84bec9ba7cde258d7fa610fc9a
jakoch <jakoch@web.de>
2012-09-02 19:21:49 +0200

    check if highlight_file is enabled

--- framework/Koch/Exception/Errorhandler.php
+++ framework/Koch/Exception/Errorhandler.php
@@ -375,8 +375,13 @@ class Errorhandler
                 $lines_html = implode($lines_array, ' ');
 
                 // get ALL LINES syntax highlighted source-code of the file and explode it into an array
-                $array_content = explode('<br />', highlight_file($file, true));
-
+                // the if check is needed to workaround "highlight_file() has been disabled for security reasons"
+                if(function_exists('highlight_file') === true) { 
+                   $array_content = explode('<br />', highlight_file($file, true));
+                } else {
+                   $array_content = explode('<br />', $file);
+                }
+                
                 // get the ERROR SURROUNDING LINES from ALL LINES
                 $array_content_sliced = array_slice($array_content, $errorcontext_starting_line-1, $scope, true);
 
