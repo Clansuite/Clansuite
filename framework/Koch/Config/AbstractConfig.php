e091a5460a5fbcb156b3c627b7a16a5ed3ba5036
jakoch <jakoch@web.de>
2012-09-24 22:42:30 +0200

    QA

--- framework/Koch/Config/AbstractConfig.php
+++ framework/Koch/Config/AbstractConfig.php
@@ -184,11 +184,11 @@ abstract class AbstractConfig /*extends ArrayObject*/ implements \ArrayAccess
         if (true === isset($this->config[$offset])) {
             return $this->config[$offset];
         } else {
-            if(DEBUG === true) {
+            if (DEBUG === true) {
                 throw new \InvalidArgumentException(sprintf('Array Key "%s" is not defined.', $offset));
             } else {
                 return false;
-            }            
+            }
         }
     }
 
