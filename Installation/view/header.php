edd22df82e5192809a25f9c34e08374e60975829
jakoch <jakoch@web.de>
2012-11-03 12:04:44 +0100

    fixing the header for IE support

--- Installation/view/header.php
+++ Installation/view/header.php
@@ -1,7 +1,12 @@
 <!doctype html>
-<html lang="de" dir="ltr">
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]> <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en"> <!--<![endif]-->
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="robots" content="index,follow" />
     <link rel="shortcut icon" href="assets/images/Clansuite-Favicon-16.ico" />
